LeSpocky/ptxdist

commit 58c48f349901988fb9bc63b7f2f9bbb30c6ef60d

u-boot: Add u-boot-dtb.imx install option
Signed-off-by: Lars Pedersen <lapeddk@gmail.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/u-boot.make
@@ -135,6 +135,9 @@ endif
 ifdef PTXCONF_U_BOOT_INSTALL_U_BOOT_IMX
 	@install -v -D -m644 $(U_BOOT_DIR)/u-boot.imx $(IMAGEDIR)/u-boot.imx
 endif
+ifdef PTXCONF_U_BOOT_INSTALL_U_BOOT_DTB_IMX
+	@install -v -D -m644 $(U_BOOT_DIR)/u-boot-dtb.imx $(IMAGEDIR)/u-boot-dtb.imx
+endif
 ifdef PTXCONF_U_BOOT_INSTALL_U_BOOT_DTB
 	@install -v -D -m644 $(U_BOOT_DIR)/u-boot-dtb.bin \
 		$(IMAGEDIR)/u-boot-dtb.bin

platforms/u-boot.in
@@ -137,6 +137,14 @@ config U_BOOT_INSTALL_U_BOOT_IMX
 	  image directory. Say yes if you are building for freescale i.MX SOCs
 	  and are not using SPL.
 
+config U_BOOT_INSTALL_U_BOOT_DTB_IMX
+        prompt "install u-boot-dtb.imx"
+        bool
+        help
+          Installing the U-Boot image with device tree support and imx header (u-boot-dtb.imx)
+          into platform image directory. Say yes if you are building for freescale i.MX SOCs
+          and are not using SPL.
+
 config U_BOOT_INSTALL_U_BOOT_DTB
 	prompt "install u-boot-dtb.bin"
 	bool

commit a6ff01cc91ad335cd63b7efd295b58564397c15b

u-boot: bugfix: missing imx dtb image clean
Fixes: 58c48f349901 ("u-boot: Add u-boot-dtb.imx install option")

Cc: Denis OSTERLAND <denis.osterland@diehl.com>
Cc: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Bruno Thomsen <bruno.thomsen@gmail.com>
Acked-by: Denis Osterland <Denis.Osterland@diehl.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/u-boot.make
@@ -195,7 +195,7 @@ $(STATEDIR)/u-boot.clean:
 	@$(call clean_pkg, U_BOOT)
 	@rm -vf $(IMAGEDIR)/u-boot.bin $(IMAGEDIR)/u-boot.srec $(IMAGEDIR)/u-boot.elf
 	@rm -vf $(IMAGEDIR)/u-boot.img $(IMAGEDIR)/SPL $(IMAGEDIR)/MLO
-	@rm -vf $(IMAGEDIR)/u-boot.imx
+	@rm -vf $(IMAGEDIR)/u-boot.imx $(IMAGEDIR)/u-boot-dtb.imx
 	@rm -vf $(IMAGEDIR)/u-boot-env.img
 	@rm -vf	$(IMAGEDIR)/u-boot-dtb.bin $(IMAGEDIR)/u-boot-with-spl-pbl.bin
 