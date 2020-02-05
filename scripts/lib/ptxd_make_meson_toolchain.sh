2523af9c557f43fc92555c09d9eed866e81b795f
Michael Olbrich <m.olbrich@pengutronix.de>
2020-02-05 08:45:53 +0100

    ptxd_make_meson_cross_file: set corect meson architecture for x86, aarch64 and riscv64
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/lib/ptxd_make_meson_toolchain.sh
+++ scripts/lib/ptxd_make_meson_toolchain.sh
@@ -16,6 +16,11 @@ export PTXDIST_MESON_CROSS_FILE
 #
 ptxd_make_meson_cross_file() {
     local ptxd_reply
+    case "${PTXCONF_ARCH_STRING}" in
+	i386) PTXCONF_ARCH_STRING=x86 ;;
+	arm64) PTXCONF_ARCH_STRING=aarch64 ;;
+	riscv) PTXCONF_ARCH_STRING=riscv64 ;;
+    esac
     ptxd_get_alternative config meson/cross-file.meson.in &&
     CPU="$(ptxd_cross_cc_v | sed -n -e "s/.*'-march=\([^']*\).*/\1/p" -e "/-march=/q")" \
 	ptxd_replace_magic "${ptxd_reply}" > "${1}"
