LeSpocky/ptxdist

commit 189159b2c34bdebb33941a0ae435731b18612447

u-boot: use ptx/cfghash-file instead of manual tricks to rebuild the package
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/u-boot.make
@@ -40,13 +40,13 @@ ifdef PTXCONF_U_BOOT_BOOT_SCRIPT
 U_BOOT_BOOT_SCRIPT_TXT := $(call ptx/in-platformconfigdir, uboot.scr)
 U_BOOT_BOOT_SCRIPT_BIN := $(call remove_quotes, \
 	$(PTXCONF_U_BOOT_BOOT_SCRIPT_ROOTFS_PATH))
-$(STATEDIR)/u-boot.compile: $(U_BOOT_BOOT_SCRIPT_TXT)
+$(call ptx/cfghash-file, OS_RELEU_BOOT, $(U_BOOT_BOOT_SCRIPT_TXT))
 endif
 
 ifdef PTXCONF_U_BOOT_ENV_IMAGE_CUSTOM
 U_BOOT_ENV_IMAGE_CUSTOM_SRC := $(call ptx/in-platformconfigdir, \
 	$(call remove_quotes, $(PTXCONF_U_BOOT_ENV_IMAGE_CUSTOM_SOURCE)))
-$(STATEDIR)/u-boot.compile: $(U_BOOT_ENV_IMAGE_CUSTOM_SRC)
+$(call ptx/cfghash-file, OS_RELEU_BOOT, $(U_BOOT_ENV_IMAGE_CUSTOM_SRC))
 endif
 
 U_BOOT_WRAPPER_BLACKLIST := \

commit 73194fbb24660cbee8ee9e440f5d91d28c810f40

u-boot: Fix typos in cfghash calls
Fixes: 189159b2c34b ("u-boot: use ptx/cfghash-file instead of manual tricks to rebuild the package")
Signed-off-by: Alexander Dahl <ada@thorsis.com>

rules/u-boot.make
@@ -40,13 +40,13 @@ ifdef PTXCONF_U_BOOT_BOOT_SCRIPT
 U_BOOT_BOOT_SCRIPT_TXT := $(call ptx/in-platformconfigdir, uboot.scr)
 U_BOOT_BOOT_SCRIPT_BIN := $(call remove_quotes, \
 	$(PTXCONF_U_BOOT_BOOT_SCRIPT_ROOTFS_PATH))
-$(call ptx/cfghash-file, OS_RELEU_BOOT, $(U_BOOT_BOOT_SCRIPT_TXT))
+$(call ptx/cfghash-file, U_BOOT, $(U_BOOT_BOOT_SCRIPT_TXT))
 endif
 
 ifdef PTXCONF_U_BOOT_ENV_IMAGE_CUSTOM
 U_BOOT_ENV_IMAGE_CUSTOM_SRC := $(call ptx/in-platformconfigdir, \
 	$(call remove_quotes, $(PTXCONF_U_BOOT_ENV_IMAGE_CUSTOM_SOURCE)))
-$(call ptx/cfghash-file, OS_RELEU_BOOT, $(U_BOOT_ENV_IMAGE_CUSTOM_SRC))
+$(call ptx/cfghash-file, U_BOOT, $(U_BOOT_ENV_IMAGE_CUSTOM_SRC))
 endif
 
 U_BOOT_WRAPPER_BLACKLIST := \