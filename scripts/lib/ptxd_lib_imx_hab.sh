3e150564474af6cd16ef804d7226ef5237c74c11
Bastian Krause <bst@pengutronix.de>
2020-05-15 16:26:33 +0200

    ptxd_lib_imx_hab/template-barebox-imx-habv4: make number of SRKs configurable
    Signed-off-by: Bastian Krause <bst@pengutronix.de>
    Message-Id: <20200515142641.812-8-bst@pengutronix.de>
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/lib/ptxd_lib_imx_hab.sh
+++ scripts/lib/ptxd_lib_imx_hab.sh
@@ -9,10 +9,12 @@
 #
 # ptxd_make_imx_habv4_gen_table - generate the srk fuse file and srk table for i.MX HABv4
 #
-# usage: ptxd_make_imx_habv4_gen_table <template>
+# usage: ptxd_make_imx_habv4_gen_table <template> [<srk_count>]
 #
 # template: the role template to access the keys. Must contain a "%d" which is
 #           used as index
+# srk_count: the number of keys (keys with index 1..srk_count will be used),
+#            defaults to 4
 #
 # The output files are generated in the package build dir:
 #
@@ -24,13 +26,22 @@
 #
 ptxd_make_imx_habv4_gen_table_impl() {
     local template="${1}"
+    local srk_count="${2}"
     local table_bin="${pkg_build_dir}/imx-srk-table.bin"
     local srk_fuse_bin="${pkg_build_dir}/imx-srk-fuse.bin"
     local -a certs
 
+    if [ -z "${srk_count}" ]; then
+	srk_count=4
+    fi
+
+    if [ "${srk_count}" -gt 4 ]; then
+	ptxd_bailout "HABv4 allows only 4 certificates"
+    fi
+
     echo -e "generating $(basename ${table_bin}) and $(basename ${srk_fuse_bin})\n"
 
-    for i in 1 2 3 4; do
+    for i in $(seq ${srk_count}); do
 	certs[${#certs[*]}]="$(cs_get_ca "$(printf "${template}" ${i})")"
     done
 
