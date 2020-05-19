LeSpocky/ptxdist

commit 8d02a8a6e3891da78c8c80dd810f2997dc1edac0

host-python-scons: switch to python3
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_world_prepare.sh
@@ -205,7 +205,7 @@ ptxd_make_world_prepare() {
 	    fi
 	    ;;
 	scons)
-	    if ! [[ "${pkg_build_deps}" =~ "host-python-${pkg_conf_tool}" ]]; then
+	    if ! [[ "${pkg_build_deps}" =~ "host-python3-${pkg_conf_tool}" ]]; then
 		ptxd_bailout "'${pkg_label}' uses '${pkg_conf_tool}' but does not select 'host-python-${pkg_conf_tool}'"
 	    fi
 	    ;;

rules/gpsd.in
@@ -1,7 +1,7 @@
 ## SECTION=system_libraries
 menuconfig GPSD
 	tristate
-	select HOST_PYTHON_SCONS
+	select HOST_PYTHON3_SCONS
 	select LIBC_M
 	select LIBC_NSL
 	select LIBC_PTHREAD

rules/host-python-scons.make
@@ -1,33 +1,0 @@
-# -*-makefile-*-
-#
-# Copyright (C) 2019 by Denis Osterland <Denis.Osterland@diehl.com>
-#
-# For further information about the PTXdist project and license conditions
-# see the README file.
-#
-
-#
-# We provide this package
-#
-HOST_PACKAGES-$(PTXCONF_HOST_PYTHON_SCONS) += host-python-scons
-
-#
-# Paths and names
-#
-HOST_PYTHON_SCONS_VERSION	:= 3.1.2
-HOST_PYTHON_SCONS_MD5		:= 77b2f8ac2661b7a4fad51c17cb7f1b25
-HOST_PYTHON_SCONS		:= python-scons-$(HOST_PYTHON_SCONS_VERSION)
-HOST_PYTHON_SCONS_SUFFIX	:= tar.gz
-HOST_PYTHON_SCONS_URL		:= $(call ptx/mirror, SF, scons/scons-$(HOST_PYTHON_SCONS_VERSION).$(HOST_PYTHON_SCONS_SUFFIX))
-HOST_PYTHON_SCONS_SOURCE	:= $(SRCDIR)/$(HOST_PYTHON_SCONS).$(HOST_PYTHON_SCONS_SUFFIX)
-HOST_PYTHON_SCONS_DIR		:= $(HOST_BUILDDIR)/$(HOST_PYTHON_SCONS)
-HOST_PYTHON_SCONS_LICENSE	:= MIT
-HOST_PYTHON_SCONS_LICENSE_FILES	:= file://LICENSE.txt;md5=e14e1b33428df24a40a782ae142785d0
-
-# ----------------------------------------------------------------------------
-# Prepare
-# ----------------------------------------------------------------------------
-
-HOST_PYTHON_SCONS_CONF_TOOL	:= python
-
-# vim: syntax=make

rules/host-python3-scons.in
@@ -1,6 +1,6 @@
 ## SECTION=hosttools_noprompt
 
-config HOST_PYTHON_SCONS
+config HOST_PYTHON3_SCONS
 	tristate
 	default y if ALLYES
-	select HOST_SYSTEM_PYTHON
+	select HOST_SYSTEM_PYTHON3

rules/host-python3-scons.make
@@ -1,0 +1,33 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2019 by Denis Osterland <Denis.Osterland@diehl.com>
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+HOST_PACKAGES-$(PTXCONF_HOST_PYTHON3_SCONS) += host-python3-scons
+
+#
+# Paths and names
+#
+HOST_PYTHON3_SCONS_VERSION		:= 3.1.2
+HOST_PYTHON3_SCONS_MD5			:= 77b2f8ac2661b7a4fad51c17cb7f1b25
+HOST_PYTHON3_SCONS			:= python-scons-$(HOST_PYTHON3_SCONS_VERSION)
+HOST_PYTHON3_SCONS_SUFFIX		:= tar.gz
+HOST_PYTHON3_SCONS_URL			:= $(call ptx/mirror, SF, scons/scons-$(HOST_PYTHON3_SCONS_VERSION).$(HOST_PYTHON3_SCONS_SUFFIX))
+HOST_PYTHON3_SCONS_SOURCE		:= $(SRCDIR)/$(HOST_PYTHON3_SCONS).$(HOST_PYTHON3_SCONS_SUFFIX)
+HOST_PYTHON3_SCONS_DIR			:= $(HOST_BUILDDIR)/$(HOST_PYTHON3_SCONS)
+HOST_PYTHON3_SCONS_LICENSE		:= MIT
+HOST_PYTHON3_SCONS_LICENSE_FILES	:= file://LICENSE.txt;md5=e14e1b33428df24a40a782ae142785d0
+
+# ----------------------------------------------------------------------------
+# Prepare
+# ----------------------------------------------------------------------------
+
+HOST_PYTHON3_SCONS_CONF_TOOL	:= python3
+
+# vim: syntax=make

commit 66419725d2b0839258b7d5420e1be98275e9c5e9

ptxd_make_world_prepare: fix typo in error message
The package name in the line above was moved from host-python to
host-python3, but the same change in the error message was forgotten.

Fixes: 8d02a8a6e3891da78c8c ("host-python-scons: switch to python3")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20200517135513.3257-1-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_world_prepare.sh
@@ -206,7 +206,7 @@ ptxd_make_world_prepare() {
 	    ;;
 	scons)
 	    if ! [[ "${pkg_build_deps}" =~ "host-python3-${pkg_conf_tool}" ]]; then
-		ptxd_bailout "'${pkg_label}' uses '${pkg_conf_tool}' but does not select 'host-python-${pkg_conf_tool}'"
+		ptxd_bailout "'${pkg_label}' uses '${pkg_conf_tool}' but does not select 'host-python3-${pkg_conf_tool}'"
 	    fi
 	    ;;
     esac