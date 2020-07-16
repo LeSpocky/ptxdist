LeSpocky/ptxdist

commit de79ed905942d4362451ea2ec3e16bf2c37aa106

ptxd_make_world_prepare_init: allow keeping the build tree during prepare for oot packages
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_world_common.sh
@@ -372,7 +372,7 @@ ptxd_make_world_init() {
 	    esac
 	fi
 	case "${pkg_build_oot}" in
-	    "YES") pkg_build_dir="${pkg_dir}-build" ;;
+	    "YES"|"KEEP") pkg_build_dir="${pkg_dir}-build" ;;
 	    "NO")  pkg_build_dir="${pkg_conf_dir}" ;;
 	    *)     ptxd_bailout "<PKG>_BUILD_OOT: please set to YES or NO" ;;
 	esac
@@ -381,15 +381,17 @@ ptxd_make_world_init() {
     #
     # out-of-tree
     #
-    unset pkg_build_oot
     if [ "${pkg_build_dir}" = "${pkg_conf_dir}" ]; then
+	unset pkg_build_oot
 	#
 	# some pkgs don't like a full path to their configure
 	# if building in tree
 	#
 	pkg_conf_dir="."
     else
-	pkg_build_oot=true
+	if [ -z "${pkg_build_oot}" ]; then
+	    pkg_build_oot=YES
+	fi
 	pkg_conf_dir="$(ptxd_abs2rel "${pkg_build_dir}" "${pkg_conf_dir}")"
     fi
 

doc/ref_make_variables.inc
@@ -265,6 +265,11 @@ of the corresponding target package if it exists.
   This is especially useful for ``file://`` URLS that point to directories to
   keep the source tree free of build files.
 
+  ``KEEP`` can be used instead of ``YES``. In this case the build tree is
+  not deleted at the beginning of the prepare stage. This make
+  reconfiguration faster. This should only be used for packages that can
+  handle configuration changes correctly and rebuild everything as needed.
+
 ``<PKG>_SUBDIR``
   This is a directory relative to ``<PKG>_DIR``. If set, all build
   operations are executed in this directory instead. By default

scripts/lib/ptxd_make_world_prepare.sh
@@ -158,7 +158,9 @@ export -f ptxd_make_world_prepare_meson
 ptxd_make_world_prepare_init() {
     # delete existing build_dir
     if [ -n "${pkg_build_oot}" ]; then
-	rm -rf   -- "${pkg_build_dir}" &&
+	if [ "${pkg_build_oot}" = "YES" ]; then
+	    rm -rf   -- "${pkg_build_dir}"
+	fi &&
 	mkdir -p -- "${pkg_build_dir}"
     fi &&
     # remove files from sysroot from the last build

commit af80016213136cf127ad8c0cabbfc27a06c3dd26

ptxd_make_world_common: ptxd_make_world_init: pkg_build_oot: add missing option KEEP to error message
Fixes: de79ed905942 ("ptxd_make_world_prepare_init: allow keeping the build tree during prepare for oot packages")
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
Message-Id: <20200715084327.30201-4-mkl@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_world_common.sh
@@ -406,7 +406,7 @@ ptxd_make_world_init() {
 	case "${pkg_build_oot}" in
 	    "YES"|"KEEP") pkg_build_dir="${pkg_dir}-build" ;;
 	    "NO")  pkg_build_dir="${pkg_conf_dir}" ;;
-	    *)     ptxd_bailout "<PKG>_BUILD_OOT: please set to YES or NO" ;;
+	    *)     ptxd_bailout "<PKG>_BUILD_OOT: please set to KEEP, YES or NO" ;;
 	esac
     fi
 