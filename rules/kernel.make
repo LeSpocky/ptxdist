LeSpocky/ptxdist

commit a668fd6120742fe5ef6e5fbcc87148f8135544d7

kernel: handle plugin detection for kernel version >= v5.7
The old hack to disable plugin support no longer works with v5.7.
Instead, add -DGENERATOR_FILE to the HOSTCC/HOSTCXX command.
The plugin headers produce an error if this is defined.

Tested with v4.19, v5.6 and v5.7.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/kernel.make
@@ -61,7 +61,8 @@ endif
 
 ifndef PTXCONF_KERNEL_GCC_PLUGINS
 KERNEL_MAKE_OPT += \
-	HOSTCXX=false
+	HOSTCXX="$(HOSTXX) -DGENERATOR_FILE" \
+	HOSTCC="$(HOSTCC) -DGENERATOR_FILE"
 endif
 
 KERNEL_CONF_OPT := \

commit 80ce928da2c8e0822e91964f0d65afcea5966c1c

kernel: fix typo in HOSTCXX variable
The typo makes the variable expand to nothing. This was probably never
caught because g++ is usually not used when building the kernel.

Fixes: a668fd6120742fe5ef6e (2020-06-11, "kernel: handle plugin detection for kernel version >= v5.7")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20200806164947.4187-1-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/kernel.make
@@ -77,7 +77,7 @@ KERNEL_SHARED_OPT := \
 ifndef PTXCONF_KERNEL_GCC_PLUGINS
 # no gcc plugins; avoid config changes depending on the host compiler
 KERNEL_SHARED_OPT += \
-	HOSTCXX="$(HOSTXX) -DGENERATOR_FILE" \
+	HOSTCXX="$(HOSTCXX) -DGENERATOR_FILE" \
 	HOSTCC="$(HOSTCC) -DGENERATOR_FILE"
 endif
 

rules/templates/template-kernel-make
@@ -43,7 +43,7 @@ KERNEL_@PACKAGE@_SHARED_OPT	:= \
 
 # no gcc plugins; avoid config changes depending on the host compiler
 KERNEL_@PACKAGE@_SHARED_OPT	+= \
-	HOSTCXX="$(HOSTXX) -DGENERATOR_FILE" \
+	HOSTCXX="$(HOSTCXX) -DGENERATOR_FILE" \
 	HOSTCC="$(HOSTCC) -DGENERATOR_FILE"
 
 KERNEL_@PACKAGE@_CONF_OPT	:= \