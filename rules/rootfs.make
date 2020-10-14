LeSpocky/ptxdist

commit 63e2d8696124083fcb1955d3bc4c46e2b0807037

rootfs: don't create empty /media subdirs
If one of PTXCONF_ROOTFS_MEDIA* is "", then all later ones are not defined
at all. As a result '/media'. Is created multiple times with mode '0777'.
If PTXCONF_ROOTFS_MEDIA1 is not empty, then this implicitly creates
'/media' with mode '0755'. The result are conflicting permissions.

Don't create directories for empty PTXCONF_ROOTFS_MEDIA* to fix this.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/rootfs.make
@@ -63,34 +63,34 @@ ifdef PTXCONF_ROOTFS_HOME_ROOT
 	@$(call install_copy, rootfs, 0, 0, 0700, /root)
 endif
 ifdef PTXCONF_ROOTFS_MEDIA
-ifneq ($(PTXCONF_ROOTFS_MEDIA1),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA1)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA1))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA2),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA2)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA2))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA3),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA3)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA3))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA4),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA4)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA4))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA5),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA5)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA5))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA6),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA6)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA6))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA7),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA7)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA7))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA8),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA8)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA8))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA9),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA9)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA9))
 endif
-ifneq ($(PTXCONF_ROOTFS_MEDIA10),"")
+ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA10)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA10))
 endif
 endif

commit 70ddbd1504778cbf9b5980765cb5f11bbf2f3eb2

rootfs: Fix creation of the /media directory
Ensure that with PTXCONF_ROOTFS_MEDIA set directory '/media' is created
even if none of PTXCONF_ROOTFS_MEDIA[1-6] is set.

On our read-only system we mount a tmpfs on directory '/media' in order
to dynamically create additional mount points there.

Fixes: 63e2d8696 (rootfs: don't create empty /media subdirs)
Signed-off-by: Gavin Schenk <g.schenk@eckelmann.de>
Message-Id: <20201008073253.20055-1-g.schenk@eckelmann.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/rootfs.make
@@ -63,6 +63,7 @@ ifdef PTXCONF_ROOTFS_HOME_ROOT
 	@$(call install_copy, rootfs, 0, 0, 0700, /root)
 endif
 ifdef PTXCONF_ROOTFS_MEDIA
+	@$(call install_copy, rootfs, 0, 0, 0755, /media)
 ifneq ($(call remove_quotes,$(PTXCONF_ROOTFS_MEDIA1)),)
 	@$(call install_copy, rootfs, 0, 0, 0777, /media/$(PTXCONF_ROOTFS_MEDIA1))
 endif