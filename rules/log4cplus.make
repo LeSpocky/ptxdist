LeSpocky/ptxdist

commit 0ac556f6b55da4c9d41c5e191432ba51276a40eb

log4cplus: Add new package
log4cplus is an excellent port of the popular log4j logging library
to C++.

Signed-off-by: Bernhard Sessler <bernhard.sessler@corscience.de>
[mol: fix dependency]
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/log4cplus.make
@@ -1,0 +1,67 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2012 by Bernhard Sessler <bernhard.sessler@corscience.de>
+#                       Corscience GmbH & Co. KG <info@corscience.de>, Germany
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_LOG4CPLUS) += log4cplus
+
+#
+# Paths and names
+#
+LOG4CPLUS_VERSION	:= 1.1.0
+LOG4CPLUS_MD5		:= 8f04a7b2db55384440b0ab83b6362d5d
+LOG4CPLUS		:= log4cplus-$(LOG4CPLUS_VERSION)
+LOG4CPLUS_SUFFIX	:= tar.xz
+LOG4CPLUS_URL		:= $(call ptx/mirror, SF, log4cplus/$(LOG4CPLUS).$(LOG4CPLUS_SUFFIX))
+LOG4CPLUS_SOURCE	:= $(SRCDIR)/$(LOG4CPLUS).$(LOG4CPLUS_SUFFIX)
+LOG4CPLUS_DIR		:= $(BUILDDIR)/$(LOG4CPLUS)
+LOG4CPLUS_LICENSE	:= APLv2
+
+# ----------------------------------------------------------------------------
+# Prepare
+# ----------------------------------------------------------------------------
+
+LOG4CPLUS_CONF_TOOL	:= cmake
+
+LOG4CPLUS_CONF_OPT	:= $(CROSS_CMAKE_USR)
+LOG4CPLUS_CONF_OPT	+= -DLOG4CPLUS_BUILD_TESTING=OFF
+
+ifdef PTXCONF_LOG4CPLUS_QT4
+LOG4CPLUS_CONF_OPT	+= -DLOG4CPLUS_QT4=ON
+endif
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/log4cplus.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init, log4cplus)
+	@$(call install_fixup, log4cplus, PRIORITY, optional)
+	@$(call install_fixup, log4cplus, SECTION, base)
+	@$(call install_fixup, log4cplus, AUTHOR, \
+		"Bernhard Sessler <bernhard.sessler@corscience.de>")
+	@$(call install_fixup, log4cplus,DESCRIPTION,missing)
+
+	@$(call install_lib, log4cplus, 0, 0, 0644, liblog4cplus)
+	@$(call install_copy, log4cplus, 0, 0, 0755, -, /usr/bin/loggingserver)
+
+ifdef PTXCONF_LOG4CPLUS_QT4
+	@$(call install_lib, log4cplus, 0, 0, 0644, liblog4cplusqt4debugappender)
+endif
+
+	@$(call install_finish, log4cplus)
+
+	@$(call touch)
+
+# vim: syntax=make

rules/log4cplus.in
@@ -1,0 +1,24 @@
+## SECTION=system_libraries
+
+menuconfig LOG4CPLUS
+	tristate
+	prompt "log4cplus                     "
+	select HOST_CMAKE
+	select GCCLIBS_GCC_S
+	select GCCLIBS_CXX
+	select GLIBC_PTHREAD
+	select QT4 if LOG4CPLUS_QT4
+	help
+	  The purpose of this project is to port the
+	  excellent Log for Java (log4j) logging
+	  library to C++.
+
+if LOG4CPLUS
+
+config LOG4CPLUS_QT4
+	bool
+	prompt "Qt4 debug appender"
+	help
+	  Build the log4cplus Qt4 debug appender
+
+endif

commit b0010a57f9acaab6196371865a0cb22e6cf18d3a

log4cplus: Fix licenses and add license file hash
When introduced as package to ptxdist, log4cplus 1.1.0 README already
stated:

> This library is licensed under the Apache Public License 2.0 and two
> clause BSD license.  Please read the included LICENSE file for
> details.

Actually some files are under Apache-2.0 and other files are under
BSD-2-Clause, so both apply.

Fixes: 0ac556f6b55d ("log4cplus: Add new package")
Cc: Bernhard Sessler <bernhard.sessler@corscience.de>
Signed-off-by: Alexander Dahl <ada@thorsis.com>

rules/log4cplus.make
@@ -22,7 +22,8 @@ LOG4CPLUS_SUFFIX	:= tar.xz
 LOG4CPLUS_URL		:= $(call ptx/mirror, SF, log4cplus/$(LOG4CPLUS).$(LOG4CPLUS_SUFFIX))
 LOG4CPLUS_SOURCE	:= $(SRCDIR)/$(LOG4CPLUS).$(LOG4CPLUS_SUFFIX)
 LOG4CPLUS_DIR		:= $(BUILDDIR)/$(LOG4CPLUS)
-LOG4CPLUS_LICENSE	:= Apache-2.0
+LOG4CPLUS_LICENSE	:= Apache-2.0 AND BSD-2-Clause
+LOG4CPLUS_LICENSE_FILES	:= file://LICENSE;md5=65053a46f707ee2b93f09ad22f692170
 
 # ----------------------------------------------------------------------------
 # Prepare