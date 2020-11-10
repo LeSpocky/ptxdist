LeSpocky/ptxdist

commit 1c799fcdc126d9e81da7944809e9da2a6d96466d

flex: add license info
Signed-off-by: Juergen Borleis <jbe@pengutronix.de>

rules/flex.make
@@ -23,7 +23,7 @@ FLEX_SUFFIX	:= tar.bz2
 FLEX_URL	:= $(call ptx/mirror, SF, flex/$(FLEX).$(FLEX_SUFFIX))
 FLEX_SOURCE	:= $(SRCDIR)/$(FLEX).$(FLEX_SUFFIX)
 FLEX_DIR	:= $(BUILDDIR)/$(FLEX)
-FLEX_LICENSE	:= unknown
+FLEX_LICENSE	:= BSD-2-Clause
 
 # ----------------------------------------------------------------------------
 # Prepare

commit 1661e5ca85000e08a2536f573a8171c90579a57b

flex: Fix license and add license file hash
The license text neither matches BSD-2-Clause nor BSD-3-Clause exactly.
However it actually contains the third clause, but without leading
numbering.

Fixes: 1c799fcdc126 ("flex: add license info")
Cc: Juergen Borleis <jbe@pengutronix.de>
Signed-off-by: Alexander Dahl <ada@thorsis.com>

rules/flex.make
@@ -21,7 +21,8 @@ FLEX_SUFFIX	:= tar.gz
 FLEX_URL	:= https://github.com/westes/flex/releases/download/v$(FLEX_VERSION)/$(FLEX).$(FLEX_SUFFIX)
 FLEX_SOURCE	:= $(SRCDIR)/$(FLEX).$(FLEX_SUFFIX)
 FLEX_DIR	:= $(BUILDDIR)/$(FLEX)
-FLEX_LICENSE	:= BSD-2-Clause
+FLEX_LICENSE	:= BSD-3-Clause
+FLEX_LICENSE_FILES := file://COPYING;md5=e4742cf92e89040b39486a6219b68067
 
 # ----------------------------------------------------------------------------
 # Prepare