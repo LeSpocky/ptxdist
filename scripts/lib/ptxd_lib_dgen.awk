LeSpocky/ptxdist

commit 7484e039302c1369042c7436f6e97d18fff2d938

ptxd_lib_dgen: let .get depend on .srchash
If the srchash changes, then the source md5 must be checked again. This is
handed by the 'get' stage. Before 8353fcf8ecd35da5db1d8db4e28a97bbcd0566e6
("rules: drop source archive stamp handling") the 'stamp' dependency would
trigger the 'get' stage. This was replaces by the expanded srchash, so the
depenency must be changed to handle this.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_dgen.awk
@@ -451,7 +451,7 @@ function write_deps_pkg_active(this_PKG, this_pkg, prefix) {
 		print "ifeq ($(" this_PKG "_EXTRACT_CFGHASH),)"							> DGEN_DEPS_POST;
 		print this_PKG "_EXTRACT_CFGHASH := 00000000000000000000000000000000"				> DGEN_DEPS_POST;
 		print "endif"											> DGEN_DEPS_POST;
-		print "$(STATEDIR)/" this_pkg ".extract: " \
+		print "$(STATEDIR)/" this_pkg ".get: " \
 					"$(STATEDIR)/" this_pkg ".$(" this_PKG "_EXTRACT_CFGHASH).srchash"	> DGEN_DEPS_POST;
 	}
 	print "$(STATEDIR)/" this_pkg ".extract: "                    "$(STATEDIR)/" this_pkg ".get"		> DGEN_DEPS_POST;

commit b6da30651adb631ddc63e4cce1c5c07e81ae2ced

ptxd_lib_dgen: fix --dirty handling with source changes
Since 7484e039302c1369042c7436f6e97d18fff2d938 ("ptxd_lib_dgen: let .get
depend on .srchash") 'get' depends on the src-hash and not 'extract'. This
breaks the --dirty handling:
'extract' fails because the source exists and suggests to use '--dirty'.
However, at that point, 'get' is already newer than 'extract' so it is
still rebuild.
Fix this by making the 'get -> extract' dependency existence only when
'--dirty' is used.

Fixes: 7484e039302c ("ptxd_lib_dgen: let .get depend on .srchash")
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_dgen.awk
@@ -453,8 +453,10 @@ function write_deps_pkg_active(this_PKG, this_pkg, prefix) {
 		print "endif"											> DGEN_DEPS_POST;
 		print "$(STATEDIR)/" this_pkg ".get: " \
 					"$(STATEDIR)/" this_pkg ".$(" this_PKG "_EXTRACT_CFGHASH).srchash"	> DGEN_DEPS_POST;
-	}
-	print "$(STATEDIR)/" this_pkg ".extract: "                    "$(STATEDIR)/" this_pkg ".get"		> DGEN_DEPS_POST;
+		print "$(STATEDIR)/" this_pkg ".extract: "            "$(STATEDIR)/" this_pkg ".get"		> DGEN_DEPS_POST;
+	} else
+		print "$(STATEDIR)/" this_pkg ".extract: | "          "$(STATEDIR)/" this_pkg ".get"		> DGEN_DEPS_POST;
+
 	print "$(STATEDIR)/" this_pkg ".extract.post: "               "$(STATEDIR)/" this_pkg ".extract"	> DGEN_DEPS_POST;
 	print "$(STATEDIR)/" this_pkg ".prepare: "                    "$(STATEDIR)/" this_pkg ".extract.post"	> DGEN_DEPS_POST;
 	if (DIRTY != "true") {