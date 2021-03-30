LeSpocky/ptxdist

commit 9e22636e4ffdc5e7670cb1bc55110b080950d5e0

ModemManager: version bump 1.4.14 -> 1.6.0
Signed-off-by: Ladislav Michl <ladis@linux-mips.org>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/modemmanager.make
@@ -16,8 +16,8 @@ PACKAGES-$(PTXCONF_MODEMMANAGER) += modemmanager
 #
 # Paths and names
 #
-MODEMMANAGER_VERSION	:= 1.4.14
-MODEMMANAGER_MD5	:= cabb72e7c2ddf6af96eca2c9f3d168a3
+MODEMMANAGER_VERSION	:= 1.6.0
+MODEMMANAGER_MD5	:= d9d93d2961ee35b4cd8a75a6a8631cb4
 MODEMMANAGER		:= ModemManager-$(MODEMMANAGER_VERSION)
 MODEMMANAGER_SUFFIX	:= tar.xz
 MODEMMANAGER_URL	:= http://www.freedesktop.org/software/ModemManager/$(MODEMMANAGER).$(MODEMMANAGER_SUFFIX)
@@ -51,8 +51,7 @@ MODEMMANAGER_CONF_OPT	:= \
 	--with-systemdsystemunitdir=/lib/systemd/system \
 	--without-polkit \
 	--with-mbim \
-	--with-qmi \
-	--with-newest-qmi-commands
+	--with-qmi
 
 # ----------------------------------------------------------------------------
 # Target-Install

commit 7146341f77878da02f4500e9768d0c0b0198fe0c

modemmanager: Revise applicable licenses
No source files under GPLv3+ could be found in mm source tree. mmcli was
falsely attributed by upstream as GPLv3+ up to 1.4, that was corrected
with 1.6.

Fixes: 9e22636e4ffd ("ModemManager: version bump 1.4.14 -> 1.6.0")
Suggested-by: Roland Hieber <rhi@pengutronix.de>
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Message-Id: <20210329054904.24342-4-post@lespocky.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/modemmanager.make
@@ -21,7 +21,7 @@ MODEMMANAGER_SUFFIX	:= tar.xz
 MODEMMANAGER_URL	:= https://www.freedesktop.org/software/ModemManager/$(MODEMMANAGER).$(MODEMMANAGER_SUFFIX)
 MODEMMANAGER_SOURCE	:= $(SRCDIR)/$(MODEMMANAGER).$(MODEMMANAGER_SUFFIX)
 MODEMMANAGER_DIR	:= $(BUILDDIR)/$(MODEMMANAGER)
-MODEMMANAGER_LICENSE	:= GPL-2.0-or-later AND GPL-3.0-or-later AND LGPL-2.1-or-later
+MODEMMANAGER_LICENSE	:= GPL-2.0-or-later AND LGPL-2.1-or-later
 
 # ----------------------------------------------------------------------------
 # Prepare