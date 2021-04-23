LeSpocky/ptxdist

commit 97981367fe855c1a2a03a910917ab8929cde882e

mesalib: version bump 20.3.3 -> 21.0.1
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
Message-Id: <20210401084612.17279-1-p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/mesalib.make
@@ -14,8 +14,8 @@ PACKAGES-$(PTXCONF_MESALIB) += mesalib
 #
 # Paths and names
 #
-MESALIB_VERSION	:= 20.3.3
-MESALIB_MD5	:= c4c81cbd8a93f87104d3f1209948b238
+MESALIB_VERSION	:= 21.0.1
+MESALIB_MD5	:= b7376187ef42cb40c9f914c48dfc07b4
 MESALIB		:= mesa-$(MESALIB_VERSION)
 MESALIB_SUFFIX	:= tar.xz
 MESALIB_URL	:= \
@@ -134,6 +134,7 @@ MESALIB_CONF_OPT	:= \
 	-Dgles-lib-suffix= \
 	-Dgles1=$(call ptx/endis, PTXCONF_MESALIB_GLES1)d \
 	-Dgles2=$(call ptx/endis, PTXCONF_MESALIB_GLES2)d \
+	-Dglvnd-name= \
 	-Dglvnd=false \
 	-Dglx=$(call ptx/ifdef, PTXCONF_MESALIB_GLX, dri, disabled) \
 	-Dglx-direct=true \
@@ -142,10 +143,12 @@ MESALIB_CONF_OPT	:= \
 	-Dlibunwind=disabled \
 	-Dllvm=disabled \
 	-Dlmsensors=$(call ptx/endis, PTXCONF_MESALIB_LMSENSORS)d \
+	-Dmicrosoft-clc=disabled \
 	-Domx-libs-path=/usr/lib/dri \
+	-Dopencl-native=false \
 	-Dopencl-spirv=false \
 	-Dopengl=$(call ptx/truefalse, PTXCONF_MESALIB_OPENGL) \
-	-Dosmesa=none \
+	-Dosmesa=false \
 	-Dosmesa-bits=8 \
 	-Dplatform-sdk-version=25 \
 	-Dplatforms=$(subst $(space),$(comma),$(MESALIBS_EGL_PLATFORMS-y)) \
@@ -154,9 +157,12 @@ MESALIB_CONF_OPT	:= \
 	-Dselinux=false \
 	-Dshader-cache-default=true \
 	-Dshader-cache=$(call ptx/endis, PTXCONF_MESALIB_VULKAN_AMD)d \
+	-Dshader-max-cache-size=1G \
 	-Dshared-glapi=enabled \
 	-Dshared-llvm=disabled \
 	-Dshared-swr=true \
+	-Dspirv-to-dxil=false \
+	-Dsse2=true \
 	-Dstatic-libclc=[] \
 	-Dswr-arches=[] \
 	-Dtools=[] \
@@ -167,8 +173,9 @@ MESALIB_CONF_OPT	:= \
 	-Dvulkan-drivers=$(subst $(space),$(comma),$(MESALIB_VULKAN_DRIVERS-y)) \
 	-Dvulkan-icd-dir=/etc/vulkan/icd.d \
 	-Dvulkan-overlay-layer=false \
-	-Dxlib-lease=disabled \
+	-Dxlib-lease=$(call ptx/endis, PTXCONF_MESALIB_EGL_X11)d \
 	-Dxvmc-libs-path=/usr/lib \
+	-Dzlib=enabled \
 	-Dzstd=$(call ptx/endis, PTXCONF_MESALIB_ZSTD)d
 
 # ----------------------------------------------------------------------------

rules/host-mesalib.make
@@ -42,6 +42,7 @@ HOST_MESALIB_CONF_OPT	:= \
 	-Dgles-lib-suffix= \
 	-Dgles1=disabled \
 	-Dgles2=disabled \
+	-Dglvnd-name= \
 	-Dglvnd=false \
 	-Dglx=disabled \
 	-Dglx-direct=false \
@@ -50,10 +51,12 @@ HOST_MESALIB_CONF_OPT	:= \
 	-Dlibunwind=disabled \
 	-Dllvm=disabled \
 	-Dlmsensors=disabled \
+	-Dmicrosoft-clc=disabled \
 	-Domx-libs-path=/usr/lib/dri \
+	-Dopencl-native=false \
 	-Dopencl-spirv=false \
 	-Dopengl=true \
-	-Dosmesa=none \
+	-Dosmesa=false \
 	-Dosmesa-bits=8 \
 	-Dplatform-sdk-version=25 \
 	-Dplatforms= \
@@ -62,9 +65,12 @@ HOST_MESALIB_CONF_OPT	:= \
 	-Dselinux=false \
 	-Dshader-cache-default=true \
 	-Dshader-cache=disabled \
+	-Dshader-max-cache-size=1G \
 	-Dshared-glapi=enabled \
 	-Dshared-llvm=disabled \
 	-Dshared-swr=true \
+	-Dspirv-to-dxil=false \
+	-Dsse2=true \
 	-Dstatic-libclc=[] \
 	-Dswr-arches=[] \
 	-Dtools=glsl \
@@ -77,6 +83,7 @@ HOST_MESALIB_CONF_OPT	:= \
 	-Dvulkan-overlay-layer=false \
 	-Dxlib-lease=disabled \
 	-Dxvmc-libs-path=/usr/lib \
+	-Dzlib=enabled \
 	-Dzstd=disabled
 
 HOST_MESALIB_MAKE_OPT	:= \

rules/mesalib.in
@@ -27,6 +27,7 @@ menuconfig MESALIB
 	select XORG_LIB_XXF86VM		if MESALIB_GLX
 	select XORG_LIB_XDAMAGE		if MESALIB_GLX
 	select XORG_LIB_XFIXES		if MESALIB_GLX
+	select XORG_LIB_XRANDR		if MESALIB_EGL_X11
 	select XORG_LIB_XSHMFENCE	if MESALIB_DRI3
 	select EXPAT
 	select ZLIB

commit bbeab38ad09c26dd1295fcff8eadd280229799fa

mesalib: fix glvnd-vendor-name and shader-cache-max-size options
These have been renamed in 21.0 from glvnd-name and
shader-max-cache-size, respectively.

Fixes: 97981367fe85 ("mesalib: version bump 20.3.3 -> 21.0.1")
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
Message-Id: <20210422075719.20905-1-p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/mesalib.make
@@ -134,8 +134,8 @@ MESALIB_CONF_OPT	:= \
 	-Dgles-lib-suffix= \
 	-Dgles1=$(call ptx/endis, PTXCONF_MESALIB_GLES1)d \
 	-Dgles2=$(call ptx/endis, PTXCONF_MESALIB_GLES2)d \
-	-Dglvnd-name= \
 	-Dglvnd=false \
+	-Dglvnd-vendor-name= \
 	-Dglx=$(call ptx/ifdef, PTXCONF_MESALIB_GLX, dri, disabled) \
 	-Dglx-direct=true \
 	-Dglx-read-only-text=false \
@@ -155,9 +155,9 @@ MESALIB_CONF_OPT	:= \
 	-Dpower8=disabled \
 	-Dprefer-iris=true \
 	-Dselinux=false \
-	-Dshader-cache-default=true \
 	-Dshader-cache=$(call ptx/endis, PTXCONF_MESALIB_VULKAN_AMD)d \
-	-Dshader-max-cache-size=1G \
+	-Dshader-cache-default=true \
+	-Dshader-cache-max-size=1G \
 	-Dshared-glapi=enabled \
 	-Dshared-llvm=disabled \
 	-Dshared-swr=true \