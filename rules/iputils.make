LeSpocky/ptxdist

commit 928752d3a0bc3bf74724af961375934e687dfe2a

iputils: version bump s20180629 -> s20200821
Switch to meson. This fixes building with gcc 10.
The crypto dependency is replaced by a local md5 implementation.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/iputils.make
@@ -14,8 +14,8 @@ PACKAGES-$(PTXCONF_IPUTILS) += iputils
 #
 # Paths and names
 #
-IPUTILS_VERSION	:= s20180629
-IPUTILS_MD5	:= 866547f2ffb17b67049472c770703c83
+IPUTILS_VERSION	:= s20200821
+IPUTILS_MD5	:= 85a5ce27f92d8fa2770dd290acd4c1e3
 IPUTILS		:= iputils-$(IPUTILS_VERSION)
 IPUTILS_SUFFIX	:= tar.gz
 IPUTILS_URL	:= http://codeload.github.com/iputils/iputils/$(IPUTILS_SUFFIX)/$(IPUTILS_VERSION)
@@ -28,35 +28,31 @@ IPUTILS_LICENSE_FILES := file://ninfod/COPYING;md5=5e9a325527978995c41e6d9a83f6e
 # Prepare
 # ----------------------------------------------------------------------------
 
-IPUTILS_TOOLS-y					:=
-IPUTILS_TOOLS-$(PTXCONF_IPUTILS_ARPING)		+= arping
-IPUTILS_TOOLS-$(PTXCONF_IPUTILS_CLOCKDIFF)	+= clockdiff
-IPUTILS_TOOLS-$(PTXCONF_IPUTILS_PING)		+= ping
-IPUTILS_TOOLS-$(PTXCONF_IPUTILS_RARPD)		+= rarpd
-IPUTILS_TOOLS-$(PTXCONF_IPUTILS_RDISC)		+= rdisc
-IPUTILS_TOOLS-$(PTXCONF_IPUTILS_TFTPD)		+= tftpd
-IPUTILS_TOOLS-$(PTXCONF_IPUTILS_TRACEPATH)	+= tracepath
-IPUTILS_TOOLS-$(PTXCONF_IPUTILS_TRACEROUTE6)	+= traceroute6
-
-IPUTILS_CONF_TOOL	:= NO
-IPUTILS_MAKEVARS	:= \
-	$(CROSS_ENV) \
-	USE_IDN=no \
-	USE_GCRYPT=$(call ptx/yesno, PTXCONF_IPUTILS_GCRYPT) \
-	USE_NETTLE=$(call ptx/yesno, PTXCONF_IPUTILS_NETTLE) \
-	USE_CRYPTO=$(call ptx/ifdef, PTXCONF_IPUTILS_OPENSSL, shared, no) \
-	TARGETS="$(IPUTILS_TOOLS-y)"
-
-# ----------------------------------------------------------------------------
-# Install
-# ----------------------------------------------------------------------------
-
-$(STATEDIR)/iputils.install:
-	@$(call targetinfo)
-	@$(foreach tool,$(IPUTILS_TOOLS-y), \
-		install -D -m755 $(IPUTILS_DIR)/$(tool) \
-			$(IPUTILS_PKGDIR)/usr/bin/$(tool);)
-	@$(call touch)
+IPUTILS_CONF_TOOL	:= meson
+IPUTILS_CONF_OPT	:= \
+	$(CROSS_MESON_USR) \
+	-DBUILD_ARPING=$(call ptx/truefalse, PTXCONF_IPUTILS_ARPING) \
+	-DBUILD_CLOCKDIFF=$(call ptx/truefalse, PTXCONF_IPUTILS_CLOCKDIFF) \
+	-DBUILD_HTML_MANS=false \
+	-DBUILD_MANS=false \
+	-DBUILD_NINFOD=false \
+	-DBUILD_PING=$(call ptx/truefalse, PTXCONF_IPUTILS_PING) \
+	-DBUILD_RARPD=$(call ptx/truefalse, PTXCONF_IPUTILS_RARPD) \
+	-DBUILD_RDISC=$(call ptx/truefalse, PTXCONF_IPUTILS_RDISC) \
+	-DBUILD_TFTPD=$(call ptx/truefalse, PTXCONF_IPUTILS_TFTPD) \
+	-DBUILD_TRACEPATH=$(call ptx/truefalse, PTXCONF_IPUTILS_TRACEPATH) \
+	-DBUILD_TRACEROUTE6=$(call ptx/truefalse, PTXCONF_IPUTILS_TRACEROUTE6) \
+	-DENABLE_RDISC_SERVER=true \
+	-DNINFOD_MESSAGES=true \
+	-DNO_SETCAP_OR_SUID=true \
+	-DSETCAP_OR_SUID_ARPING=false \
+	-DSETCAP_OR_SUID_CLOCKDIFF=false \
+	-DSETCAP_OR_SUID_PING=false \
+	-DSETCAP_OR_SUID_TRACEROUTE6=false \
+	-DUSE_CAP=$(call ptx/truefalse, PTXCONF_IPUTILS_LIBCAP) \
+	-DUSE_GETTEXT=false \
+	-DUSE_IDN=false \
+	-Dsystemdunitdir=/usr/lib/systemd/system
 
 # ----------------------------------------------------------------------------
 # Target-Install
@@ -71,8 +67,8 @@ $(STATEDIR)/iputils.targetinstall:
 	@$(call install_fixup, iputils,AUTHOR,"Alexander Aring <aar@pengutronix.de>")
 	@$(call install_fixup, iputils,DESCRIPTION,missing)
 
-	@$(foreach tool,$(IPUTILS_TOOLS-y), \
-		$(call install_copy, iputils, 0, 0, 0755, -, /usr/bin/$(tool));)
+	@$(call install_tree, iputils, 0, 0, -, /usr/bin)
+	@$(call install_tree, iputils, 0, 0, -, /usr/sbin)
 
 	@$(call install_finish, iputils)
 

rules/iputils.in
@@ -3,19 +3,21 @@
 menuconfig IPUTILS
 	tristate
 	prompt "iputils                       "
+	select HOST_MESON
 	select LIBC_RESOLV	if IPUTILS_PING
-	select LIBCAP		if IPUTILS_ARPING || IPUTILS_PING || IPUTILS_TRACEROUTE6 || IPUTILS_CLOCKDIFF
-	select LIBGCRYPT	if IPUTILS_GCRYPT
-	select NETTLE		if IPUTILS_NETTLE
-	select OPENSSL		if IPUTILS_OPENSSL
+	select LIBCAP		if IPUTILS_LIBCAP
 	help
 	  The iputils package is set of small useful utilities for Linux
 	  networking.
 
 if IPUTILS
 
+config IPUTILS_LIBCAP
+	bool
+
 config IPUTILS_ARPING
 	bool
+	select IPUTILS_LIBCAP
 	depends on !BUSYBOX_ARPING || ALLYES
 	prompt "arping"
 	help
@@ -27,6 +29,7 @@ comment "BusyBox' arping is selected!"
 
 config IPUTILS_CLOCKDIFF
 	bool
+	select IPUTILS_LIBCAP
 	prompt "clockdiff"
 	help
 	  Measures clock difference between us and destination with 1 msec
@@ -34,6 +37,7 @@ config IPUTILS_CLOCKDIFF
 
 config IPUTILS_PING
 	bool
+	select IPUTILS_LIBCAP
 	depends on (!BUSYBOX_PING && !INETUTILS_PING) || ALLYES
 	prompt "ping"
 	help
@@ -47,39 +51,6 @@ comment "inetutils' ping is selected!"
 
 comment "BusyBox' ping is selected!"
         depends on BUSYBOX_PING
-
-if GLOBAL_IPV6
-
-choice
-	prompt "crypto library to be used for ping -N"
-	default IPUTILS_NOCRYPTO
-	depends on IPUTILS_PING
-	help
-	  Selecting a crypto library is necessary for ping to support
-	  sending IPv6 Node Information Queries (RFC 4620).
-
-	config IPUTILS_NOCRYPTO
-		bool
-		prompt "none"
-		help
-		  No crypto library is used. This means that ping -N
-		  won't work.
-
-	config IPUTILS_GCRYPT
-		bool
-		prompt "libgcrypt"
-
-	config IPUTILS_NETTLE
-		bool
-		prompt "nettle"
-
-	config IPUTILS_OPENSSL
-		bool
-		prompt "openssl"
-
-endchoice
-
-endif
 
 config IPUTILS_RARPD
 	bool
@@ -120,6 +91,7 @@ config IPUTILS_TRACEPATH
 
 config IPUTILS_TRACEROUTE6
 	bool
+	select IPUTILS_LIBCAP
 	depends on !BUSYBOX_TRACEROUTE6 || ALLYES
 	prompt "traceroute6"
 	depends on GLOBAL_IPV6

commit 98bce3e1496006303fcea400e0daacd8988c7a39

iputils: Fix targetinstall
The tools are all optional.  If no tool to be installed in /usr/bin is
selected, targetinstall fails.  Same for tools going to /usr/sbin. See
for example following log of failed targetinstall, if only arping and
ping are selected, but none of the others:

    install file:
      src=rsu/platform-v7a/packages/iputils-s20200821/usr/bin/arping
      dst=/usr/bin/arping
      owner=0
      group=0
      permissions=0755
      debug file: /usr/lib/debug/.build-id/51/e139e0e79232c85ddadeb45ae7ecc901d4073c.debug

    install file:
      src=rsu/platform-v7a/packages/iputils-s20200821/usr/bin/ping
      dst=/usr/bin/ping
      owner=0
      group=0
      permissions=0755
      debug file: /usr/lib/debug/.build-id/93/03312f626e591bc167b0e7e2a9b38c71d4bc7b.debug

    No suitable file '/usr/sbin' could be found in any of these locations:
    /home/adahl/[…]/rsu/platform-v7a/packages/iputils-s20200821/usr/sbin.v7a
    /home/adahl/[…]/rsu/platform-v7a/packages/iputils-s20200821/usr/sbin

    Error: install_tree failed!

Fixes: 928752d3a0bc ("iputils: version bump s20180629 -> s20200821")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Message-Id: <20210525163015.59080-1-ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/iputils.make
@@ -67,8 +67,30 @@ $(STATEDIR)/iputils.targetinstall:
 	@$(call install_fixup, iputils,AUTHOR,"Alexander Aring <aar@pengutronix.de>")
 	@$(call install_fixup, iputils,DESCRIPTION,missing)
 
-	@$(call install_tree, iputils, 0, 0, -, /usr/bin)
-	@$(call install_tree, iputils, 0, 0, -, /usr/sbin)
+ifdef PTXCONF_IPUTILS_ARPING
+	@$(call install_copy, iputils, 0, 0, 0755, -, /usr/bin/arping)
+endif
+ifdef PTXCONF_IPUTILS_CLOCKDIFF
+	@$(call install_copy, iputils, 0, 0, 0755, -, /usr/bin/clockdiff)
+endif
+ifdef PTXCONF_IPUTILS_PING
+	@$(call install_copy, iputils, 0, 0, 0755, -, /usr/bin/ping)
+endif
+ifdef PTXCONF_IPUTILS_RARPD
+	@$(call install_copy, iputils, 0, 0, 0755, -, /usr/sbin/rarpd)
+endif
+ifdef PTXCONF_IPUTILS_RDISC
+	@$(call install_copy, iputils, 0, 0, 0755, -, /usr/sbin/rdisc)
+endif
+ifdef PTXCONF_IPUTILS_TFTPD
+	@$(call install_copy, iputils, 0, 0, 0755, -, /usr/bin/tftpd)
+endif
+ifdef PTXCONF_IPUTILS_TRACEPATH
+	@$(call install_copy, iputils, 0, 0, 0755, -, /usr/bin/tracepath)
+endif
+ifdef PTXCONF_IPUTILS_TRACEROUTE6
+	@$(call install_copy, iputils, 0, 0, 0755, -, /usr/bin/traceroute6)
+endif
 
 	@$(call install_finish, iputils)
 