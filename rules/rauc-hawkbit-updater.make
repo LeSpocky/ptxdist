LeSpocky/ptxdist

commit 399ce56f9106b1c671fbab3dc98db6c0db670109

rauc-hawkbit-updater: new package
Signed-off-by: Bastian Krause <bst@pengutronix.de>
Message-Id: <20200924165621.24177-2-bst@pengutronix.de>
[mol: use menuconfig instead of config]
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/rauc-hawkbit-updater.make
@@ -1,0 +1,72 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2020 by Bastian Krause <bst@pengutronix.de>
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_RAUC_HAWKBIT_UPDATER) += rauc-hawkbit-updater
+
+#
+# Paths and names
+#
+RAUC_HAWKBIT_UPDATER_VERSION		:= 2020-09-09-gb38f5a5
+RAUC_HAWKBIT_UPDATER_MD5		:= c2accd9bdcab813dbf9850e6ed63085e
+RAUC_HAWKBIT_UPDATER			:= rauc-hawkbit-updater-$(RAUC_HAWKBIT_UPDATER_VERSION)
+RAUC_HAWKBIT_UPDATER_SUFFIX		:= tar.gz
+RAUC_HAWKBIT_UPDATER_URL		:= https://github.com/rauc/rauc-hawkbit-updater/archive/$(RAUC_HAWKBIT_UPDATER).$(RAUC_HAWKBIT_UPDATER_SUFFIX)
+RAUC_HAWKBIT_UPDATER_SOURCE		:= $(SRCDIR)/$(RAUC_HAWKBIT_UPDATER).$(RAUC_HAWKBIT_UPDATER_SUFFIX)
+RAUC_HAWKBIT_UPDATER_DIR		:= $(BUILDDIR)/$(RAUC_HAWKBIT_UPDATER)
+RAUC_HAWKBIT_UPDATER_LICENSE		:= LGPL-2.1-or-later
+RAUC_HAWKBIT_UPDATER_LICENSE_FILES	:= file://LICENSE;md5=1a6d268fd218675ffea8be556788b780
+
+# ----------------------------------------------------------------------------
+# Prepare
+# ----------------------------------------------------------------------------
+
+#
+# cmake
+#
+RAUC_HAWKBIT_UPDATER_CONF_TOOL	:= cmake
+RAUC_HAWKBIT_UPDATER_CONF_OPT	:= \
+	$(CROSS_CMAKE_USR) \
+	-DWITH_SYSTEMD=$(call ptx/onoff,PTXCONF_RAUC_HAWKBIT_UPDATER_SYSTEMD_UNIT) \
+	-DBUILD_DOC=OFF \
+	-DQA_BUILD=ON \
+	--with-systemdsystemunitdir=/usr/lib/systemd/system
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/rauc-hawkbit-updater.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init, rauc-hawkbit-updater)
+	@$(call install_fixup, rauc-hawkbit-updater,PRIORITY,optional)
+	@$(call install_fixup, rauc-hawkbit-updater,SECTION,base)
+	@$(call install_fixup, rauc-hawkbit-updater,AUTHOR,"Bastian Krause <bst@pengutronix.de>")
+	@$(call install_fixup, rauc-hawkbit-updater,DESCRIPTION,missing)
+
+	@$(call install_copy, rauc-hawkbit-updater, 0, 0, 0755, -, \
+		/usr/bin/rauc-hawkbit-updater)
+
+	@$(call install_alternative, rauc-hawkbit-updater, 0, 0, 0644, \
+		/etc/rauc-hawkbit-updater/config.conf)
+
+ifdef PTXCONF_RAUC_HAWKBIT_UPDATER_SYSTEMD_UNIT
+	@$(call install_alternative, rauc-hawkbit-updater, 0, 0, 0644, \
+		/usr/lib/systemd/system/rauc-hawkbit-updater.service)
+	@$(call install_link, rauc-hawkbit-updater, ../rauc-hawkbit-updater.service, \
+		/usr/lib/systemd/system/multi-user.target.wants/rauc-hawkbit-updater.service)
+endif
+
+	@$(call install_finish, rauc-hawkbit-updater)
+
+	@$(call touch)
+
+# vim: syntax=make

projectroot/etc/rauc-hawkbit-updater/config.conf
@@ -1,0 +1,43 @@
+[client]
+# host or IP and optional port
+hawkbit_server            = 10.10.0.254:8080
+
+# true = HTTPS, false = HTTP
+ssl                       = false
+
+# validate ssl certificate (only use if ssl is true)
+ssl_verify                = false
+
+# Tenant id
+tenant_id                 = DEFAULT
+
+# Target name (controller id)
+target_name               = test-target
+
+# Security token
+auth_token                = cb115a721af28f781b493fa467819ef5
+
+# Or gateway_token can be used instead of auth_token
+#gateway_token             = cb115a721af28f781b493fa467819ef5
+
+# Temporay file RAUC bundle should be downloaded to
+bundle_download_location  = /tmp/bundle.raucb
+
+# time in seconds to wait before retrying
+retry_wait                = 60
+
+# connection timeout in seconds
+connect_timeout           = 20
+
+# request timeout in seconds
+timeout                   = 60
+
+# debug, info, message, critical, error, fatal
+log_level                 = message
+
+# Every key / value under [device] is sent to HawkBit (target attributes),
+# and can be used in target filter.
+[device]
+mac_address               = ff:ff:ff:ff:ff:ff
+hw_revision               = 2
+model                     = T1

rules/rauc-hawkbit-updater.in
@@ -1,0 +1,26 @@
+## SECTION=applications
+
+menuconfig RAUC_HAWKBIT_UPDATER
+	tristate
+	prompt "rauc-hawkbit-updater          "
+	select HOST_CMAKE
+	select DBUS
+	select JSON_GLIB
+	select LIBCURL
+	select LIBCURL_HTTP
+	select LIBCURL_SSL
+	select RAUC
+	select RAUC_SERVICE
+	select SYSTEMD		if RAUC_HAWKBIT_UPDATER_SYSTEMD_UNIT
+	help
+	  Daemon operating as an interface between the RAUC D-Bus API and the hawkBit DDI API.
+
+if RAUC_HAWKBIT_UPDATER
+
+config RAUC_HAWKBIT_UPDATER_SYSTEMD_UNIT
+	bool
+	default y
+	depends on INITMETHOD_SYSTEMD
+	prompt "install and enable systemd service file"
+
+endif

commit 7fb63c5e9473904c83ee51238776fb694decc745

rauc-hawkbit-updater: fix license
rauc-hawkbit-updater's license is actually LGPL-2.1-only, not
LGPL-2.1-or-later. See the SPDX-License-Identifier in any source file.

Fixes: 399ce56f9 ("rauc-hawkbit-updater: new package")
Signed-off-by: Bastian Krause <bst@pengutronix.de>
Message-Id: <20210607095535.17091-1-bst@pengutronix.de>
[rhi: add src/rauc-hawkbit-updater.c license file as source]
Reviewed-by: Roland Hieber <rhi@pengutronix.de>
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/rauc-hawkbit-updater.make
@@ -21,8 +21,10 @@ RAUC_HAWKBIT_UPDATER_SUFFIX		:= tar.gz
 RAUC_HAWKBIT_UPDATER_URL		:= https://github.com/rauc/rauc-hawkbit-updater/archive/$(RAUC_HAWKBIT_UPDATER).$(RAUC_HAWKBIT_UPDATER_SUFFIX)
 RAUC_HAWKBIT_UPDATER_SOURCE		:= $(SRCDIR)/$(RAUC_HAWKBIT_UPDATER).$(RAUC_HAWKBIT_UPDATER_SUFFIX)
 RAUC_HAWKBIT_UPDATER_DIR		:= $(BUILDDIR)/$(RAUC_HAWKBIT_UPDATER)
-RAUC_HAWKBIT_UPDATER_LICENSE		:= LGPL-2.1-or-later
-RAUC_HAWKBIT_UPDATER_LICENSE_FILES	:= file://LICENSE;md5=1a6d268fd218675ffea8be556788b780
+RAUC_HAWKBIT_UPDATER_LICENSE		:= LGPL-2.1-only
+RAUC_HAWKBIT_UPDATER_LICENSE_FILES	:= \
+	file://LICENSE;md5=1a6d268fd218675ffea8be556788b780 \
+	file://src/rauc-hawkbit-updater.c;startline=2;endline=18;md5=77f866ecbd1a16747812cee95cdfcf44
 
 # ----------------------------------------------------------------------------
 # Prepare