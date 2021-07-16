LeSpocky/ptxdist

commit b8958ad388ba104527e2ce27f158b0da3d39c977

python3-pycryptodomex: new package
Signed-off-by: Lars Pedersen <lapeddk@gmail.com>
Message-Id: <20210702121931.497226-1-lapeddk@gmail.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/python3-pycryptodomex.make
@@ -1,0 +1,54 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2021 by Lars Pedersen <lapeddk@gmail.com>
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_PYTHON3_PYCRYPTODOMEX) += python3-pycryptodomex
+
+#
+# Paths and names
+#
+PYTHON3_PYCRYPTODOMEX_VERSION	:= 3.10.1
+PYTHON3_PYCRYPTODOMEX_MD5	:= 91b86ca7fa80d41179d7735067c7347a
+PYTHON3_PYCRYPTODOMEX		:= pycryptodomex-$(PYTHON3_PYCRYPTODOMEX_VERSION)
+PYTHON3_PYCRYPTODOMEX_SUFFIX	:= tar.gz
+PYTHON3_PYCRYPTODOMEX_URL	:= $(call ptx/mirror-pypi, pycryptodomex, $(PYTHON3_PYCRYPTODOMEX).$(PYTHON3_PYCRYPTODOMEX_SUFFIX))
+PYTHON3_PYCRYPTODOMEX_SOURCE	:= $(SRCDIR)/$(PYTHON3_PYCRYPTODOMEX).$(PYTHON3_PYCRYPTODOMEX_SUFFIX)
+PYTHON3_PYCRYPTODOMEX_DIR	:= $(BUILDDIR)/$(PYTHON3_PYCRYPTODOMEX)
+PYTHON3_PYCRYPTODOMEX_LICENSE	:= BSD-2
+PYTHON3_PYCRYPTODOMEX_LICENSE_FILES := \
+	file://LICENSE.rst;md5=accfa6aeaceb3ba96676edf18e78302c
+
+# ----------------------------------------------------------------------------
+# Prepare
+# ----------------------------------------------------------------------------
+
+PYTHON3_PYCRYPTODOMEX_CONF_TOOL	:= python3
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/python3-pycryptodomex.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init, python3-pycryptodomex)
+	@$(call install_fixup, python3-pycryptodomex,PRIORITY,optional)
+	@$(call install_fixup, python3-pycryptodomex,SECTION,base)
+	@$(call install_fixup, python3-pycryptodomex,AUTHOR,"Lars Pedersen <lapeddk@gmail.com>")
+	@$(call install_fixup, python3-pycryptodomex,DESCRIPTION,missing)
+
+	@$(call install_glob, python3-pycryptodomex, 0, 0, -, \
+		$(PYTHON3_SITEPACKAGES),, *.py *SelfTest*)
+
+	@$(call install_finish, python3-pycryptodomex)
+
+	@$(call touch)
+
+# vim: syntax=make

rules/python3-pycryptodomex.in
@@ -1,0 +1,9 @@
+## SECTION=python3
+
+config PYTHON3_PYCRYPTODOMEX
+	tristate
+	select PYTHON3
+	select HOST_PYTHON3_SETUPTOOLS
+	prompt "pycryptodomex"
+	help
+	  PyCryptodome is a self-contained Python package of low-level cryptographic primitives

commit ff53b37b44d3cb8218e82b7a5fd33b05ef601f1e

python3-pycryptodomex: fix license identifier
LICENSE.rst includes an Unlicense and a BSD-2-Clause, and refers to
additional licenses for the OCB code, which contains a restriction for
"military purposes", and is not compatible with the Open Source
Definition.

Fixes: b8958ad388ba104527e2 (2021-07-02, "python3-pycryptodomex: new package")
Cc: Lars Pedersen <lapeddk@gmail.com>
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20210708185255.3303-2-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/python3-pycryptodomex.make
@@ -21,9 +21,12 @@ PYTHON3_PYCRYPTODOMEX_SUFFIX	:= tar.gz
 PYTHON3_PYCRYPTODOMEX_URL	:= $(call ptx/mirror-pypi, pycryptodomex, $(PYTHON3_PYCRYPTODOMEX).$(PYTHON3_PYCRYPTODOMEX_SUFFIX))
 PYTHON3_PYCRYPTODOMEX_SOURCE	:= $(SRCDIR)/$(PYTHON3_PYCRYPTODOMEX).$(PYTHON3_PYCRYPTODOMEX_SUFFIX)
 PYTHON3_PYCRYPTODOMEX_DIR	:= $(BUILDDIR)/$(PYTHON3_PYCRYPTODOMEX)
-PYTHON3_PYCRYPTODOMEX_LICENSE	:= BSD-2
+PYTHON3_PYCRYPTODOMEX_LICENSE	:= Unlicense AND BSD-2-Clause and custom
 PYTHON3_PYCRYPTODOMEX_LICENSE_FILES := \
-	file://LICENSE.rst;md5=accfa6aeaceb3ba96676edf18e78302c
+	file://LICENSE.rst;md5=accfa6aeaceb3ba96676edf18e78302c \
+	file://Doc/ocb/license1.pdf;md5=e33fa8dcf4d10e5c0d9a9e9291714d3d \
+	file://Doc/ocb/license2.pdf;md5=68cfc4690fbbaa0d89d3ea3abcc60b0f \
+	file://Doc/ocb/license3.pdf;md5=2124679d48d559681738985e040ae388
 
 # ----------------------------------------------------------------------------
 # Prepare