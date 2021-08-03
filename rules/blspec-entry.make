LeSpocky/ptxdist

commit f50cd1449e9347309b1050b0b738882dac1954e9

rework device-tree handling
It's not really possible to build a dtb by itself. A lot of included dtsi
and header files are needed. So build the device-trees with the package
that provides these files. Currently that's the kernel package.

This also means, that other kernel packages can have their own device-trees
as well.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/blspec-entry.make
@@ -19,7 +19,7 @@ BLSPEC_ENTRY_VERSION	:= $(KERNEL_VERSION)
 
 BLSPEC_ENTRY_TITLE	:= PTXdist - $(call remove_quotes,$(PTXCONF_PROJECT_VENDOR)-$(PTXCONF_PROJECT))
 ifdef PTXCONF_BLSPEC_ENTRY_DEVICETREE
-BLSPEC_ENTRY_NAMES	= $(basename $(notdir $(DTC_DTB)))
+BLSPEC_ENTRY_NAMES	= $(basename $(notdir $(KERNEL_DTB)))
 blspec/title		= $(BLSPEC_ENTRY_TITLE) $(strip $(1))
 blspec/devicetree	= devicetree\t/boot/$(strip $(1)).dtb
 else

platforms/blspec-entry.in
@@ -4,14 +4,13 @@ menuconfig BLSPEC_ENTRY
 	tristate
 	select KERNEL
 	select KERNEL_INSTALL if !KERNEL_FIT
-	select DTC_INSTALL_OFTREE if DTC
 	prompt "default bootloader spec entry "
 
 if BLSPEC_ENTRY
 
 config BLSPEC_ENTRY_DEVICETREE
 	bool
-	default DTC && !KERNEL_FIT
+	default KERNEL_DTB && !KERNEL_FIT
 
 config BLSPEC_ENTRY_CMDLINE
 	string

platforms/dtc.in
@@ -1,40 +1,0 @@
-## SECTION=devicetree
-
-menuconfig DTC
-	bool
-	select HOST_DTC
-	select KERNEL
-	prompt "Build device tree             "
-	help
-	  Select this to build a device tree for your board.
-	  You will find the device tree binary blob under
-	  platform-nnn/images/ in your workspace.
-
-if DTC
-
-config DTC_INSTALL_OFTREE
-	bool "install oftree to /boot"
-	help
-	  Creates a package to install the 'oftree' file to /boot
-	  of your target system.
-
-config DTC_OFTREE_DTS_PATH
-	string "path to source dts file"
-	default "${PTXDIST_PLATFORMCONFIG_SUBDIR}/dts:${KERNEL_DIR}/arch/${GENERIC_KERNEL_ARCH}/boot/dts"
-	help
-	  Define path to the dts source file. Multiple directories can be
-	  specified separated by ':'.
-
-config DTC_OFTREE_DTS
-	string "source dts file"
-	default "<yourboard>.dts"
-	help
-	  Select the dts file to use for the device tree binary
-	  blob generation. For relative file names DTC_OFTREE_DTS_PATH
-	  is used as a search path for the device tree files specified
-	  here. Multiple dts files can be specified, separated by spaces.
-
-config DTC_EXTRA_ARGS
-	string "extra options passed to dtc"
-
-endif

platforms/kernel-fit.in
@@ -5,7 +5,6 @@ menuconfig KERNEL_FIT
 	select KERNEL
 	select HOST_DTC
 	select HOST_U_BOOT_TOOLS
-	select DTC
 	select CODE_SIGNING	if KERNEL_FIT_SIGNED
 	select IMAGE_ROOT_CPIO	if KERNEL_FIT_INITRAMFS
 	prompt "Generate images/linuximage.fit"

platforms/kernel.in
@@ -158,6 +158,30 @@ config KERNEL_IMAGE
 	default "Image"		if KERNEL_IMAGE_RAW
 	default "simpleImage.${PTXCONF_KERNEL_IMAGE_SIMPLE_TARGET}.elf" if KERNEL_IMAGE_SIMPLE
 
+menuconfig KERNEL_DTB
+	bool
+	prompt "Build device trees            "
+
+if KERNEL_DTB
+
+config KERNEL_DTS_PATH
+	string "path to source dts file"
+	default "${PTXDIST_PLATFORMCONFIG_SUBDIR}/dts:${KERNEL_DIR}/arch/${GENERIC_KERNEL_ARCH}/boot/dts"
+	help
+	  Define path to the dts source file. Multiple directories can be
+	  specified separated by ':'.
+
+config KERNEL_DTS
+	string "source dts file"
+	default "<yourboard>.dts"
+	help
+	  Select the dts file to use for the device tree binary
+	  blob generation. For relative file names DTC_OFTREE_DTS_PATH
+	  is used as a search path for the device tree files specified
+	  here. Multiple dts files can be specified, separated by spaces.
+
+endif
+
 config KERNEL_ZSTD
 	prompt "build zstd hosttool"
 	bool

rules/dtc.make
@@ -1,72 +1,0 @@
-# -*-makefile-*-
-#
-# Copyright (C) 2007 by Sascha Hauer
-#           (C) 2010 by Carsten Schlote
-#           (C) 2010 by Michael Olbrich <m.olbrich@pengutronix.de>
-#
-# For further information about the PTXdist project and license conditions
-# see the README file.
-#
-
-#
-# We provide this package
-#
-ifdef PTXCONF_DTC_INSTALL_OFTREE
-PACKAGES-$(PTXCONF_DTC) += dtc
-else
-EXTRA_PACKAGES-$(PTXCONF_DTC) += dtc
-endif
-
-DTC_VERSION := 1.0.0
-DTC_LICENSE := ignore
-
-ifneq ($(subst PTXCONF_KERNEL_ARCH_STRING,,$(value PTXCONF_DTC_OFTREE_DTS_PATH)),$(value PTXCONF_DTC_OFTREE_DTS_PATH))
-$(call ptx/error, invalid value for PTXCONF_DTC_OFTREE_DTS_PATH:)
-$(call ptx/error, PTXCONF_KERNEL_ARCH_STRING is no longer defined.)
-$(call ptx/error, Use GENERIC_KERNEL_ARCH instead)
-endif
-
-# ----------------------------------------------------------------------------
-# Target-Install
-# ----------------------------------------------------------------------------
-
-ptx/dtb = $(notdir $(basename $(strip $(1)))).dtb
-
-dts/env = \
-	$(call ptx/env) \
-	dts_path=$(PTXCONF_DTC_OFTREE_DTS_PATH) \
-	dts_dtb="$(strip $(1))" \
-	dts_dts="$(strip $(2))" \
-	dts_kernel_dir="$(KERNEL_DIR)" \
-	dts_kernel_arch="$(GENERIC_KERNEL_ARCH)"
-
-%.dtb: $(STATEDIR)/dtc.install
-	@$(call targetinfo)
-	@$(call dts/env, $@, $(DTB_DTS)) ptxd_make_dts_dtb
-	@$(call finish)
-
-DTC_DTB = $(foreach dts, $(call remove_quotes,$(PTXCONF_DTC_OFTREE_DTS)), $(IMAGEDIR)/$(call ptx/dtb, $(dts)))
-
-# make sure "ptxdist targetinstall kernel" generates a new device trees
-$(STATEDIR)/kernel.targetinstall.post: $(DTC_DTB)
-
-$(STATEDIR)/dtc.targetinstall: $(DTC_DTB)
-	@$(call targetinfo)
-
-ifdef PTXCONF_DTC_INSTALL_OFTREE
-	@$(call install_init,  dtc)
-	@$(call install_fixup, dtc, PRIORITY,optional)
-	@$(call install_fixup, dtc, SECTION,base)
-	@$(call install_fixup, dtc, AUTHOR,"Carsten Schlote <c.schlote@konzeptpark.de>")
-	@$(call install_fixup, dtc, DESCRIPTION, "oftree description for machine $(PTXCONF_PLATFORM)")
-
-	@$(call install_copy, dtc, 0, 0, 0755, /boot);
-	@$(foreach dtb, $(DTC_DTB), \
-		$(call install_copy, dtc, 0, 0, 0644, \
-		"$(dtb)", "/boot/$(notdir $(dtb))")$(ptx/nl))
-
-	@$(call install_finish, dtc)
-endif
-	@$(call touch)
-
-# vim: syntax=make

rules/kernel-fit.make
@@ -28,7 +28,7 @@ KERNEL_FIT_KERNEL		 = $(KERNEL_IMAGE_PATH_y)
 ifdef PTXCONF_KERNEL_FIT_INITRAMFS
 KERNEL_FIT_INITRAMFS		:= $(IMAGEDIR)/root.cpio
 endif
-KERNEL_FIT_DTB			 = $(DTC_DTB)
+KERNEL_FIT_DTB			 = $(KERNEL_DTB)
 
 # ----------------------------------------------------------------------------
 # Target-Install

rules/kernel.make
@@ -27,10 +27,23 @@ endif
 KERNEL_DIR		:= $(BUILDDIR)/$(KERNEL)
 KERNEL_BUILD_DIR	:= $(KERNEL_DIR)-build
 KERNEL_CONFIG		:= $(call ptx/in-platformconfigdir, $(call remove_quotes, $(PTXCONF_KERNEL_CONFIG)))
+KERNEL_DTS_PATH		:= $(call remove_quotes,$(PTXCONF_KERNEL_DTS_PATH))
+KERNEL_DTS		:= $(call remove_quotes,$(PTXCONF_KERNEL_DTS))
+KERNEL_DTB_FILES	:= $(addsuffix .dtb,$(basename $(KERNEL_DTS)))
 KERNEL_LICENSE		:= GPL-2.0-only
 KERNEL_SOURCE		:= $(SRCDIR)/$(KERNEL).$(KERNEL_SUFFIX)
 KERNEL_DEVPKG		:= NO
 KERNEL_BUILD_OOT	:= KEEP
+
+# track changes to devices-trees in the BSP
+$(call world/dts-cfghash-file, KERNEL)
+
+# in case we migrate some old syntax
+ifneq ($(KERNEL_DTS),$(notdir $(KERNEL_DTS)))
+$(call ptx/error, the device trees in PTXCONF_KERNEL_DTS must be specified without)
+$(call ptx/error, directory. Use PTXCONF_KERNEL_DTS_PATH to provide a list of direcories)
+$(call ptx/error, that will be searched.)
+endif
 
 # ----------------------------------------------------------------------------
 # Prepare
@@ -240,6 +253,7 @@ $(STATEDIR)/kernel.install:
 ifdef PTXCONF_KERNEL_MODULES_INSTALL
 	@$(call world/install, KERNEL)
 endif
+	@$(call world/dtb, KERNEL)
 	@$(call touch)
 
 # ----------------------------------------------------------------------------
@@ -248,6 +262,11 @@ endif
 
 $(STATEDIR)/kernel.targetinstall:
 	@$(call targetinfo)
+
+	@$(foreach dtb, $(KERNEL_DTB_FILES), \
+		echo -e "Installing $(dtb) ...\n"$(ptx/nl) \
+		install -D -m0644 $(KERNEL_PKGDIR)/boot/$(dtb) \
+			$(IMAGEDIR)/$(dtb)$(ptx/nl))
 
 ifdef PTXCONF_KERNEL_XPKG
 	@$(call install_init,  kernel)
@@ -260,6 +279,10 @@ ifdef PTXCONF_KERNEL_XPKG
 
 ifdef PTXCONF_KERNEL_INSTALL
 	@$(call install_copy, kernel, 0, 0, 0644, $(KERNEL_IMAGE_PATH_y), /boot/$(KERNEL_IMAGE), n)
+
+	@$(foreach dtb, $(KERNEL_DTB_FILES), \
+		$(call install_copy, kernel, 0, 0, 0644, -, \
+			/boot/$(dtb), n)$(ptx/nl))
 endif
 
 # install the ELF kernel image for debugging purpose
@@ -315,6 +338,16 @@ ifdef PTXCONF_KERNEL_MODULES_INSTALL
 endif
 
 	@$(call touch)
+
+# ----------------------------------------------------------------------------
+# Clean
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/kernel.clean:
+	@$(call targetinfo)
+	@$(call clean_pkg, KERNEL)
+	@$(foreach dtb,$(KERNEL_DTB_FILES), \
+		rm -vf $(IMAGEDIR)/$(dtb)$(ptx/nl))
 
 # ----------------------------------------------------------------------------
 # oldconfig / menuconfig

commit 0c72a4ecd06d5f0d7990efce045ba5bb5ee68f24

blspec-entry: fix device tree based generation
KERNEL_DTB is just the option to enable the device tree generation. The
actual dts files are stored in KERNEL_DTS.

Fixes: f50cd144 ("rework device-tree handling")
Signed-off-by: Michael Tretter <m.tretter@pengutronix.de>
Message-Id: <20210803091728.45703-1-m.tretter@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/blspec-entry.make
@@ -19,7 +19,7 @@ BLSPEC_ENTRY_VERSION	:= $(KERNEL_VERSION)
 
 BLSPEC_ENTRY_TITLE	:= PTXdist - $(call remove_quotes,$(PTXCONF_PROJECT_VENDOR)-$(PTXCONF_PROJECT))
 ifdef PTXCONF_BLSPEC_ENTRY_DEVICETREE
-BLSPEC_ENTRY_NAMES	= $(basename $(notdir $(KERNEL_DTB)))
+BLSPEC_ENTRY_NAMES	= $(basename $(notdir $(KERNEL_DTS)))
 blspec/title		= $(BLSPEC_ENTRY_TITLE) $(strip $(1))
 blspec/devicetree	= devicetree\t/boot/$(strip $(1)).dtb
 else