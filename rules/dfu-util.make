LeSpocky/ptxdist

commit 01006cfdfedab01290db859e77146dabefafccdc

[dfu-utils] version bump to first release 0.1
Stefan has recently released dfu-utils-0.1, so we can switch to that one
instead to the version in temporary-src.

Signed-off-by: Robert Schwebel <r.schwebel@pengutronix.de>

rules/dfu-util.make
@@ -16,12 +16,13 @@ PACKAGES-$(PTXCONF_DFU_UTIL) += dfu-util
 #
 # Paths and names
 #
-DFU_UTIL_VERSION	:= 5118
+DFU_UTIL_VERSION	:= 0.1
 DFU_UTIL		:= dfu-util-$(DFU_UTIL_VERSION)
 DFU_UTIL_SUFFIX		:= tar.gz
-DFU_UTIL_URL		:= http://www.pengutronix.de/software/ptxdist/temporary-src/$(DFU_UTIL).$(DFU_UTIL_SUFFIX)
+DFU_UTIL_URL		:= http://dfu-util.gnumonks.org/releases/$(DFU_UTIL).$(DFU_UTIL_SUFFIX)
 DFU_UTIL_SOURCE		:= $(SRCDIR)/$(DFU_UTIL).$(DFU_UTIL_SUFFIX)
 DFU_UTIL_DIR		:= $(BUILDDIR)/$(DFU_UTIL)
+DFU_UTIL_LICENSE	:= GPLv2
 
 # ----------------------------------------------------------------------------
 # Get
@@ -50,7 +51,7 @@ DFU_UTIL_AUTOCONF := $(CROSS_AUTOCONF_USR)
 $(STATEDIR)/dfu-util.targetinstall:
 	@$(call targetinfo)
 
-	@$(call install_init, dfu-util)
+	@$(call install_init,  dfu-util)
 	@$(call install_fixup, dfu-util,PACKAGE,dfu-util)
 	@$(call install_fixup, dfu-util,PRIORITY,optional)
 	@$(call install_fixup, dfu-util,VERSION,$(DFU_UTIL_VERSION))

commit 3c6c618533a30e262170ae1977cf8b5c6207d309

dfu-util: Fix license information
dfu-util started with the following headers stating GPL-2.0-or-later in
2007 already, and never changed that:

 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation; either version 2 of the License, or
 * (at your option) any later version.

buildroot and Debian come to the same conclusion.  That was probably
attributed wrong in ptxdist back in 2010 (and converted twice later).

Fixes: 01006cfdfeda ("[dfu-utils] version bump to first release 0.1")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Message-Id: <20211011075112.28107-2-ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/dfu-util.make
@@ -21,7 +21,9 @@ DFU_UTIL_SUFFIX		:= tar.gz
 DFU_UTIL_URL		:= http://dfu-util.sourceforge.net/releases/$(DFU_UTIL).$(DFU_UTIL_SUFFIX)
 DFU_UTIL_SOURCE		:= $(SRCDIR)/$(DFU_UTIL).$(DFU_UTIL_SUFFIX)
 DFU_UTIL_DIR		:= $(BUILDDIR)/$(DFU_UTIL)
-DFU_UTIL_LICENSE	:= GPL-2.0-only
+DFU_UTIL_LICENSE	:= GPL-2.0-or-later
+DFU_UTIL_LICENSE_FILES	:= file://COPYING;md5=94d55d512a9ba36caa9b7df079bae19f
+# The file 'dfuse-pack.py' has different license, but is not copied to target
 
 # ----------------------------------------------------------------------------
 # Prepare