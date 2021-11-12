LeSpocky/ptxdist

commit 0e0f5d31e0e004717320d732154d700405a13b31

[dropbear] use rc-once to create server keys
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/dropbear.make
@@ -280,6 +280,7 @@ endif
 ifdef PTXCONF_INITMETHOD_BBINIT
 ifdef PTXCONF_DROPBEAR_STARTSCRIPT
 	@$(call install_alternative, dropbear, 0, 0, 0755, /etc/init.d/dropbear, n)
+	@$(call install_alternative, dropbear, 0, 0, 0755, /etc/rc.once.d/dropbear, n)
 endif
 endif
 

generic/etc/init.d/dropbear
@@ -16,41 +16,10 @@ DROPBEAR_DSSKEY_DEFAULT="/etc/dropbear/dropbear_dss_host_key"
 
 test -z "$DROPBEAR_BANNER" || \
   DROPBEAR_EXTRA_ARGS="$DROPBEAR_EXTRA_ARGS -b $DROPBEAR_BANNER"
-test -n "$DROPBEAR_RSAKEY" || \
-  DROPBEAR_RSAKEY=$DROPBEAR_RSAKEY_DEFAULT
-test -n "$DROPBEAR_DSSKEY" || \
-  DROPBEAR_DSSKEY=$DROPBEAR_DSSKEY_DEFAULT
 test -n "$DROPBEAR_KEYTYPES" || \
   DROPBEAR_KEYTYPES="rsa dss"
 
-gen_key() {
-
-    key_type=$1
-    key_file=$2
-
-    mount_root_rw || exit
-    rm -f $key_file > /dev/null 2>&1
-
-    echo -n "generating $key_type key..."
-    dropbearkey -t $key_type -f $key_file > /dev/null 2>&1
-
-    if [ "$?" = "0" ]; then
-        echo "done"
-    else
-        echo "failed"
-        mount_root_restore
-        exit 1
-    fi
-
-    mount_root_restore || exit
-
-}
-
 dropbear_start() {
-
-    # create keys if necessary
-    [ \! -f "$DROPBEAR_RSAKEY" ] && gen_key rsa "$DROPBEAR_RSAKEY"
-    [ \! -f "$DROPBEAR_DSSKEY" ] && gen_key dss "$DROPBEAR_DSSKEY"
 
     KEY_ARGS=""
     test -f $DROPBEAR_DSSKEY && KEY_ARGS="$KEY_ARGS -d $DROPBEAR_DSSKEY"

generic/etc/rc.once.d/dropbear
@@ -1,0 +1,33 @@
+#!/bin/sh
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+DROPBEAR_RSAKEY_DEFAULT="/etc/dropbear/dropbear_rsa_host_key"
+DROPBEAR_DSSKEY_DEFAULT="/etc/dropbear/dropbear_dss_host_key"
+
+test -n "$DROPBEAR_RSAKEY" || \
+  DROPBEAR_RSAKEY=$DROPBEAR_RSAKEY_DEFAULT
+test -n "$DROPBEAR_DSSKEY" || \
+  DROPBEAR_DSSKEY=$DROPBEAR_DSSKEY_DEFAULT
+
+gen_key() {
+
+	key_type=$1
+	key_file=$2
+
+	rm -f $key_file > /dev/null 2>&1
+
+	echo -n "generating $key_type key..."
+	dropbearkey -t $key_type -f $key_file > /dev/null 2>&1
+
+	if [ "$?" = "0" ]; then
+		echo "done"
+	else
+		echo "failed"
+		exit 1
+	fi
+}
+
+gen_key rsa "$DROPBEAR_RSAKEY"
+gen_key dss "$DROPBEAR_DSSKEY"
+

rules/dropbear.in
@@ -6,6 +6,7 @@ menuconfig DROPBEAR
 	select LIBC_UTIL
 	select LIBC_CRYPT
 	select INITMETHOD
+	select RC_ONCE
 	select BUSYBOX_START_STOP_DAEMON			if DROPBEAR_STARTSCRIPT
 	select BUSYBOX_FEATURE_START_STOP_DAEMON_FANCY		if DROPBEAR_STARTSCRIPT
 	select BUSYBOX_FEATURE_START_STOP_DAEMON_LONG_OPTIONS	if DROPBEAR_STARTSCRIPT

commit 5f4f0cfc400120e7e59fa350781f136a1743e898

dropbear: Move targetinstall of rc-once script
Key generation is still needed if someone decides not to install the
init script or uses a different init system.

Fixes: 0e0f5d31e0e0 ("[dropbear] use rc-once to create server keys")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Message-Id: <20211105154734.19983-3-ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/dropbear.make
@@ -281,6 +281,10 @@ endif
 ifdef PTXCONF_DROPBEAR_DROPBEAR_KEY
 	@$(call install_copy, dropbear, 0, 0, 0755, -, \
 		/usr/bin/dropbearkey)
+
+	@$(call install_alternative, dropbear, 0, 0, 0755, /etc/rc.once.d/dropbear, n)
+	@$(call install_replace, dropbear, /etc/rc.once.d/dropbear, \
+		@KEYDIR@, $(PTXCONF_DROPBEAR_KEY_DIR))
 endif
 
 ifdef PTXCONF_DROPBEAR_DROPBEAR_CONVERT
@@ -302,9 +306,6 @@ endif
 ifdef PTXCONF_DROPBEAR_STARTSCRIPT
 	@$(call install_alternative, dropbear, 0, 0, 0755, /etc/init.d/dropbear, n)
 	@$(call install_replace, dropbear, /etc/init.d/dropbear, \
-		@KEYDIR@, $(PTXCONF_DROPBEAR_KEY_DIR))
-	@$(call install_alternative, dropbear, 0, 0, 0755, /etc/rc.once.d/dropbear, n)
-	@$(call install_replace, dropbear, /etc/rc.once.d/dropbear, \
 		@KEYDIR@, $(PTXCONF_DROPBEAR_KEY_DIR))
 
 ifneq ($(call remove_quotes,$(PTXCONF_DROPBEAR_BBINIT_LINK)),)