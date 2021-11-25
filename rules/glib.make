LeSpocky/ptxdist

commit 20d3f539a7f8a9f1ceb85718d734189891cf3d36

glib: Version bump. 2.68.3 -> 2.70.0
glib < 2.68.4 fails to build on glibc 2.34 due to improper usage of close_range().
https://gitlab.gnome.org/GNOME/glib/-/merge_requests/2180

Bump to next stable release, 2.70.0.

Signed-off-by: Christian Melki <christian.melki@t2data.com>
Message-Id: <20211018072151.3769846-1-christian.melki@t2data.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/glib.make
@@ -15,8 +15,8 @@ PACKAGES-$(PTXCONF_GLIB) += glib
 #
 # Paths and names
 #
-GLIB_VERSION	:= 2.68.3
-GLIB_MD5	:= a3968c7510447f055fd2204ba947f80f
+GLIB_VERSION	:= 2.70.0
+GLIB_MD5	:= 4b08228707d28a9f23f7369902f4b166
 GLIB		:= glib-$(GLIB_VERSION)
 GLIB_SUFFIX	:= tar.xz
 GLIB_URL	:= $(call ptx/mirror, GNOME, glib/$(basename $(GLIB_VERSION))/$(GLIB).$(GLIB_SUFFIX))

commit c2a935cceaf44dc325038b0118e5a7348a0951e0

glib: drop internal_pcre option
This option was removed in commit 9fbd7f3dc1f0 ("build: Drop the
internal_pcre option in favour of the subproject"), contained in
version 2.69.0.

Fixes: 20d3f539a7f8 ("glib: Version bump. 2.68.3 -> 2.70.0")
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
Message-Id: <20211122133630.1771606-1-p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/glib.make
@@ -47,7 +47,6 @@ GLIB_CONF_OPT	:= \
 	-Dgtk_doc=false \
 	-Diconv=libc \
 	-Dinstalled_tests=false \
-	-Dinternal_pcre=false \
 	-Dlibelf=disabled \
 	-Dlibmount=$(call ptx/endis, PTXCONF_GLIB_LIBMOUNT)d \
 	-Dman=false \

rules/host-glib.make
@@ -36,7 +36,6 @@ HOST_GLIB_CONF_OPT	:= \
 	-Dgtk_doc=false \
 	-Diconv=libc \
 	-Dinstalled_tests=false \
-	-Dinternal_pcre=false \
 	-Dlibelf=disabled \
 	-Dlibmount=disabled \
 	-Dman=false \