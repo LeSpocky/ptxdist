LeSpocky/ptxdist

commit 49254b41f397b66c2540c35f376e4679aba5695d

gst-plugins-base1: version bump 1.16.2 -> 1.18.0
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
Message-Id: <20200910095034.655-5-p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/gst-plugins-base1.make
@@ -14,8 +14,8 @@ PACKAGES-$(PTXCONF_GST_PLUGINS_BASE1) += gst-plugins-base1
 #
 # Paths and names
 #
-GST_PLUGINS_BASE1_VERSION	:= 1.16.2
-GST_PLUGINS_BASE1_MD5		:= 3fdb32823535799a748c1fc14f978e2c
+GST_PLUGINS_BASE1_VERSION	:= 1.18.0
+GST_PLUGINS_BASE1_MD5		:= 5ea9dc2692335e03170c3639901c2d83
 GST_PLUGINS_BASE1		:= gst-plugins-base-$(GST_PLUGINS_BASE1_VERSION)
 GST_PLUGINS_BASE1_SUFFIX	:= tar.xz
 GST_PLUGINS_BASE1_URL		:= http://gstreamer.freedesktop.org/src/gst-plugins-base/$(GST_PLUGINS_BASE1).$(GST_PLUGINS_BASE1_SUFFIX)
@@ -39,6 +39,7 @@ GST_PLUGINS_BASE1_ENABLE-$(PTXCONF_GST_PLUGINS_BASE1_CDPARANOIA)	+= cdparanoia
 GST_PLUGINS_BASE1_ENABLE-$(PTXCONF_GST_PLUGINS_BASE1_COMPOSITOR)	+= compositor
 GST_PLUGINS_BASE1_ENABLE-$(PTXCONF_GST_PLUGINS_BASE1_ENCODING)		+= encoding
 GST_PLUGINS_BASE1_ENABLE-$(PTXCONF_GST_PLUGINS_BASE1_GIO)		+= gio
+GST_PLUGINS_BASE1_ENABLEC-$(PTXCONF_GST_PLUGINS_BASE1_GIO)		+= gio-typefinder
 GST_PLUGINS_BASE1_ENABLEC-$(PTXCONF_GST_PLUGINS_BASE1_GL)		+= gl
 GST_PLUGINS_BASE1_ENABLEP-$(PTXCONF_GST_PLUGINS_BASE1_GL)		+= opengl
 GST_PLUGINS_BASE1_ENABLE-$(PTXCONF_GST_PLUGINS_BASE1_LIBVISUAL)		+= libvisual
@@ -89,7 +90,7 @@ GST_PLUGINS_BASE1_GL_PLATFORM := $(subst $(space),$(comma),$(strip $(GST_PLUGINS
 
 GST_PLUGINS_BASE1_GL_WINSYS :=
 ifdef PTXCONF_GST_PLUGINS_BASE1_EGL
-GST_PLUGINS_BASE1_GL_WINSYS += gbm
+GST_PLUGINS_BASE1_GL_WINSYS := egl gbm
 endif
 ifdef PTXCONF_GST_PLUGINS_BASE1_EGL_WAYLAND
 GST_PLUGINS_BASE1_GL_WINSYS += wayland
@@ -114,9 +115,10 @@ GST_PLUGINS_BASE1_CONF_OPT	= \
 	-Dgl_api=$(GST_PLUGINS_BASE1_GL_API) \
 	-Dgl_platform=$(GST_PLUGINS_BASE1_GL_PLATFORM) \
 	-Dgl_winsys=$(GST_PLUGINS_BASE1_GL_WINSYS) \
-	-Dgtk_doc=disabled \
+	-Dgles_module_name=libGLESv2 \
 	-Dintrospection=$(call ptx/endis,PTXCONF_GSTREAMER1_INTROSPECTION)d \
 	-Diso-codes=disabled \
+	-Dopengl_module_name=libGL \
 	-Dorc=enabled \
 	-Dtools=$(call ptx/endis, PTXCONF_GST_PLUGINS_BASE1_INSTALL_TOOLS)d \
 	-Dx11=$(call ptx/endis, PTXCONF_GST_PLUGINS_BASE1_X11)d

patches/gst-plugins-base-1.16.2/0001-meson-build-gir-even-when-cross-compiling-if-introsp.patch
@@ -1,28 +1,0 @@
-From: =?UTF-8?q?Tim-Philipp=20M=C3=BCller?= <tim@centricular.com>
-Date: Thu, 17 Oct 2019 20:06:24 +0100
-Subject: [PATCH] meson: build gir even when cross-compiling if introspection
- was enabled explicitly
-
-This can be made to work in certain circumstances when
-cross-compiling, so default to not building g-i stuff
-when cross-compiling, but allow it if introspection was
-enabled explicitly via -Dintrospection=enabled.
-
-See gstreamer/gstreamer#454 and gstreamer/gstreamer#381.
----
- meson.build | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/meson.build b/meson.build
-index 767f98f114f4..d99fe50725d0 100644
---- a/meson.build
-+++ b/meson.build
-@@ -371,7 +371,7 @@ endif
- 
- gir = find_program('g-ir-scanner', required : get_option('introspection'))
- gnome = import('gnome')
--build_gir = gir.found() and not meson.is_cross_build()
-+build_gir = gir.found() and (not meson.is_cross_build() or get_option('introspection').enabled())
- gir_init_section = [ '--add-init-section=extern void gst_init(gint*,gchar**);' + \
-     'g_setenv("GST_REGISTRY_DISABLE", "yes", TRUE);' + \
-     'g_setenv("GST_REGISTRY_1.0", "@0@", TRUE);'.format(meson.current_build_dir() + '/gir_empty_registry.reg') + \

patches/gst-plugins-base-1.16.2/series
@@ -1,4 +1,0 @@
-# generated by git-ptx-patches
-#tag:base --start-number 1
-0001-meson-build-gir-even-when-cross-compiling-if-introsp.patch
-# c50daa57270463847718e2263e04d262  - git-ptx-patches magic

commit bb19d3e5a74d7f2ee4e21d3083002c6c34a16081

gst-plugins-base1: fix gles2_module_name option
Fixes: 49254b41f397 ("gst-plugins-base1: version bump 1.16.2 -> 1.18.0")
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
Message-Id: <20211122133644.1777883-1-p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/gst-plugins-base1.make
@@ -115,7 +115,7 @@ GST_PLUGINS_BASE1_CONF_OPT	= \
 	-Dgl_api=$(GST_PLUGINS_BASE1_GL_API) \
 	-Dgl_platform=$(GST_PLUGINS_BASE1_GL_PLATFORM) \
 	-Dgl_winsys=$(GST_PLUGINS_BASE1_GL_WINSYS) \
-	-Dgles_module_name=libGLESv2 \
+	-Dgles2_module_name=libGLESv2 \
 	-Dintrospection=$(call ptx/endis,PTXCONF_GSTREAMER1_INTROSPECTION)d \
 	-Diso-codes=disabled \
 	-Dopengl_module_name=libGL \