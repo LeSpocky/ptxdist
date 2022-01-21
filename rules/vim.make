LeSpocky/ptxdist

commit c538cff7e42b27000f9b8ae7ab9bc508460fb2b3

vim: add install options
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/vim.make
@@ -108,13 +108,18 @@ $(STATEDIR)/vim.targetinstall:
 	@$(call install_fixup, vim,AUTHOR,"Michael Olbrich <m.olbrich@pengutronix.de>")
 	@$(call install_fixup, vim,DESCRIPTION,missing)
 
+ifdef PTXCONF_VIM_VIM
 	@$(call install_copy, vim, 0, 0, 0755, -, /usr/bin/vim)
 
 	@$(foreach link, $(VIM_LINKS), \
 		$(call install_link, vim, vim, /usr/bin/$(link));)
 
 	@$(call install_tree, vim, 0, 0, -, /usr/share/vim)
+endif
 
+ifdef PTXCONF_VIM_XXD
+	@$(call install_copy, vim, 0, 0, 0755, $(VIM_DIR)/$(VIM_SUBDIR)/xxd/xxd, /usr/bin/vim)
+endif
 	@$(call install_finish, vim)
 
 	@$(call touch)

rules/vim.in
@@ -1,13 +1,23 @@
 ## SECTION=editors
 
-config VIM
+menuconfig VIM
 	tristate
 	select LIBC_DL
 	select LIBC_M
 	select GCCLIBS_GCC_S
 	select NCURSES
-	prompt "vim"
+	prompt "vim                           "
 	help
 	  Vim is an advanced text editor that seeks to provide the
 	  power of the de-facto Unix editor 'Vi', with a more complete
 	  feature set.
+
+if VIM
+
+config VIM_VIM
+	bool "Vim Editor"
+
+config VIM_XXD
+	bool "XXD"
+
+endif

commit d67b82df99ffce2a36870a3805af2d4873a63aed

vim: fix xxd install location
Installing xxd to /usr/bin/vim is obviously wrong. In case
PTXCONF_VIM_VIM and PTXCONF_VIM_XXD are enabled, vim is even
overwritten. Fix that by installing xxd to /usr/bin/xxd.

Fixes: c538cff7e ("vim: add install options")
Signed-off-by: Bastian Krause <bst@pengutronix.de>
Message-Id: <20220119155544.3364301-1-bst@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/vim.make
@@ -118,7 +118,7 @@ ifdef PTXCONF_VIM_VIM
 endif
 
 ifdef PTXCONF_VIM_XXD
-	@$(call install_copy, vim, 0, 0, 0755, $(VIM_DIR)/$(VIM_SUBDIR)/xxd/xxd, /usr/bin/vim)
+	@$(call install_copy, vim, 0, 0, 0755, $(VIM_DIR)/$(VIM_SUBDIR)/xxd/xxd, /usr/bin/xxd)
 endif
 	@$(call install_finish, vim)
 