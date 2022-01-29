3ca92677df5bbde0fb01c396acb0ffc64f26fc2b
Michael Riesch <michael.riesch@wolfvision.net>
2022-01-29 08:03:28 +0100

    scripts: add helper to inject files into a source directory
    Some packages may require certain files that are maintained
    or generated outside of their source repository. For example,
    binary firmware blobs could be excluded from the sources due
    to licensing issues. Add a helper that allows to inject certain
    files into the source directory (usually in the prepare stage).
    
    Signed-off-by: Michael Riesch <michael.riesch@wolfvision.net>
    Message-Id: <20220129070330.2601433-4-michael.riesch@wolfvision.net>
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

---
+++ rules/post/ptxd_make_world_inject.make
@@ -0,0 +1,19 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2021 by Michael Riesch <michael.riesch@wolfvision.net>
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+world/inject/env = \
+	$(call world/env, $(1)) \
+	pkg_inject_path="$($(1)_INJECT_PATH)" \
+	pkg_inject_files="$($(1)_INJECT_FILES)" \
+	pkg_source="$($(1)_DIR)"
+
+world/inject = \
+	$(call world/inject/env,$(strip $(1))) \
+	ptxd_make_world_inject
+
+# vim: syntax=make
---
+++ scripts/lib/ptxd_make_world_inject.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (C) 2021 by Michael Riesch <michael.riesch@wolfvision.net>
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+ptxd_make_inject() {
+    local source target
+
+    source="$(echo ${inject_file} | cut -d ":" -f 1)"
+    target="${pkg_source}/$(echo ${inject_file} | cut -d ":" -f 2)"
+    if [ -z "${target}" ]; then
+	target="${source}"
+    fi
+
+    if [[ "${source}" =~ ^/.* ]]; then
+	ptxd_bailout "'${source}' must not be an absolute path!" \
+	    "Use <PKG>_INJECT_PATH to specify the search path."
+    fi
+
+    if ! ptxd_in_path pkg_inject_path "${source}"; then
+	ptxd_bailout "Blob '${source}' not found in '${pkg_inject_path}'."
+    fi
+    source="${ptxd_reply}"
+
+    echo -e "\nInject file $(ptxd_print_path ${source}) into" \
+	 "$(ptxd_print_path ${target})..."
+    cp ${source} ${target}
+}
+export -f ptxd_make_inject
+
+
+ptxd_make_world_inject() {
+    ptxd_make_world_init || return
+
+    for inject_file in ${pkg_inject_files}; do
+	ptxd_make_inject || return
+    done
+}
+export -f ptxd_make_world_inject
