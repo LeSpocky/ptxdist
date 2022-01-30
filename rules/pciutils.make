LeSpocky/ptxdist

commit e61ccbcad6537c02ae00477507ef2c6655f22907

pciutils: Update to accomodate for hwdata package.
Make pciutils depend on hwdata-pci files.

Remove compression as all utils may not be able to read
compressed .ids files.

Remove update-pciids script. This is done by updating
hwdata now.

Remove installation of pci.ids, provided by hwdata.

Signed-off-by: Christian Melki <christian.melki@t2data.com>
Message-Id: <20220108184518.3774572-4-christian.melki@t2data.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/pciutils.make
@@ -39,7 +39,6 @@ PCIUTILS_MAKE_OPT := \
 	SBINDIR=/usr/bin \
 	HOST=$(PTXCONF_ARCH_STRING)-linux \
 	RELEASE=$(KERNEL_HEADER_VERSION) \
-	ZLIB=$(call ptx/yesno, PTXCONF_PCIUTILS_COMPRESS) \
 	LIBKMOD=$(call ptx/yesno, PTXCONF_PCIUTILS_LIBKMOD) \
 	SHARED=$(call ptx/yesno, PTXCONF_PCIUTILS_LIBPCI) \
 	STRIP= \
@@ -67,20 +66,12 @@ $(STATEDIR)/pciutils.targetinstall:
 ifdef PTXCONF_PCIUTILS_TOOLS
 	@$(call install_copy, pciutils, 0, 0, 0755, -, /usr/bin/lspci)
 	@$(call install_copy, pciutils, 0, 0, 0755, -, /usr/bin/setpci)
-	@$(call install_copy, pciutils, 0, 0, 0755, -, /usr/bin/update-pciids)
 endif
 
 ifdef PTXCONF_PCIUTILS_LIBPCI
 	@$(call install_lib, pciutils, 0, 0, 0644, libpci)
 endif
 
-ifdef PTXCONF_PCIUTILS_COMPRESS
-	@$(call install_copy, pciutils, 0, 0, 0644, -, \
-		/usr/share/pci.ids.gz, n)
-else
-	@$(call install_copy, pciutils, 0, 0, 0644, -, \
-		/usr/share/pci.ids, n)
-endif
 	@$(call install_finish, pciutils)
 
 	@$(call touch)

rules/pciutils.in
@@ -1,7 +1,8 @@
 ## SECTION=shell_and_console
 menuconfig PCIUTILS
-	select ZLIB	if PCIUTILS_COMPRESS
-	select LIBKMOD	if PCIUTILS_LIBKMOD
+	select LIBKMOD		if PCIUTILS_LIBKMOD
+	select HWDATA		if !SYSTEMD_HWDB
+	select HWDATA_PCI	if !SYSTEMD_HWDB	
 	tristate
 	prompt "pciutils                      "
 	help
@@ -35,14 +36,6 @@ config PCIUTILS_LIBPCI
 	prompt "libpci support"
 	help
 	  Generate and install the shared libpci library.
-
-config PCIUTILS_COMPRESS
-	bool
-	default y
-	prompt "compress pci_ids"
-	help
-	  Save storage space by compressing the pci-id database. Access will
-	  be a little bit slower then.
 
 endif
 

commit d53596c450eb1d3ef9dbb5e4ddaf5eebe1423f76

pciutils: explicitly disable zlib
Otherwise it is automatically enabled if /usr/include/zlib.h exists.

Fixes: e61ccbcad653 ("pciutils: Update to accomodate for hwdata package.")
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/pciutils.make
@@ -39,6 +39,7 @@ PCIUTILS_MAKE_OPT := \
 	SBINDIR=/usr/bin \
 	HOST=$(PTXCONF_ARCH_STRING)-linux \
 	RELEASE=$(KERNEL_HEADER_VERSION) \
+	ZLIB=no \
 	LIBKMOD=$(call ptx/yesno, PTXCONF_PCIUTILS_LIBKMOD) \
 	SHARED=$(call ptx/yesno, PTXCONF_PCIUTILS_LIBPCI) \
 	STRIP= \