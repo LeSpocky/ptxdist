LeSpocky/ptxdist

commit 010b50b484c0367d4be62eddabedfd2afccca4b7

libcurl: add license information
Signed-off-by: Robert Schwebel <r.schwebel@pengutronix.de>
Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libcurl.make
@@ -24,6 +24,7 @@ LIBCURL_SUFFIX	:= tar.gz
 LIBCURL_URL	:= http://curl.haxx.se/download/$(LIBCURL).$(LIBCURL_SUFFIX)
 LIBCURL_SOURCE	:= $(SRCDIR)/$(LIBCURL).$(LIBCURL_SUFFIX)
 LIBCURL_DIR	:= $(BUILDDIR)/$(LIBCURL)
+LIBCURL_LICENSE	:= MIT
 
 # ----------------------------------------------------------------------------
 # Prepare

commit 9e993317c22cdfc473796d3c9a977bcb54a01d77

libcurl: Fix license
curl has its own entry in the SPDX list of licenses.  The license is
inspired by MIT / X, but only that.

Link: https://curl.se/docs/copyright.html
Link: https://spdx.org/licenses/curl.html
Fixes: 010b50b484c0 ("libcurl: add license information")
Signed-off-by: Alexander Dahl <ada@thorsis.com>

rules/libcurl.make
@@ -22,7 +22,7 @@ LIBCURL_SUFFIX	:= tar.xz
 LIBCURL_URL	:= https://curl.haxx.se/download/$(LIBCURL).$(LIBCURL_SUFFIX)
 LIBCURL_SOURCE	:= $(SRCDIR)/$(LIBCURL).$(LIBCURL_SUFFIX)
 LIBCURL_DIR	:= $(BUILDDIR)/$(LIBCURL)
-LIBCURL_LICENSE	:= MIT
+LIBCURL_LICENSE	:= curl
 
 # ----------------------------------------------------------------------------
 # Prepare