45b027a5cda0cf42bc7878ba94e701548460f960
Michael Olbrich <m.olbrich@pengutronix.de>
2022-05-18 10:41:14 +0200

    liboil: sync autogen.sh with generic version
    Specifically GTKDOCIZE=true is needed here.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- patches/liboil-0.3.16/autogen.sh
+++ patches/liboil-0.3.16/autogen.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ -n "${pkg_stamp}" ] && ! [[ " ${pkg_build_deps} " =~ " host-gettext " ]]; then
+	echo "No host-gettext dependency. Faking autopoint: AUTOPOINT=true."
+	export AUTOPOINT=true
+fi
+export GTKDOCIZE=true
+
 aclocal $ACLOCAL_FLAGS
 
 am_macro_dir=m4 \
