LeSpocky/ptxdist

commit 34db9dd470bdec673a15f4b2038303ff053d0427

libcurl: Version bump. 7.84.0 -> 7.85.0
https://curl.se/changes.html#7_85_0
Bunch of bugfixes as usual.
Fixes CVE-2022-35252

* Remove old patchset, included in the new release

Signed-off-by: Christian Melki <christian.melki@t2data.com>
Message-Id: <20220909201628.853195-1-christian.melki@t2data.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libcurl.make
@@ -15,8 +15,8 @@ PACKAGES-$(PTXCONF_LIBCURL) += libcurl
 #
 # Paths and names
 #
-LIBCURL_VERSION	:= 7.84.0
-LIBCURL_MD5	:= 6ce66afa416bb11b8f39cc9e059afd5b
+LIBCURL_VERSION	:= 7.85.0
+LIBCURL_MD5	:= 131f76c84016c45806b902330a74164f
 LIBCURL		:= curl-$(LIBCURL_VERSION)
 LIBCURL_SUFFIX	:= tar.xz
 LIBCURL_URL	:= https://curl.haxx.se/download/$(LIBCURL).$(LIBCURL_SUFFIX)

patches/curl-7.84.0/0001-easy_lock.h-include-sched.h-if-available-to-fix-buil.patch
@@ -1,25 +1,0 @@
-From: Daniel Stenberg <daniel@haxx.se>
-Date: Mon, 27 Jun 2022 08:46:21 +0200
-Subject: [PATCH] easy_lock.h: include sched.h if available to fix build
-
-Patched-by: Harry Sintonen
-
-Closes #9054
----
- lib/easy_lock.h | 3 +++
- 1 file changed, 3 insertions(+)
-
-diff --git a/lib/easy_lock.h b/lib/easy_lock.h
-index 819f50ce815b..1f54289ceb2d 100644
---- a/lib/easy_lock.h
-+++ b/lib/easy_lock.h
-@@ -36,6 +36,9 @@
- 
- #elif defined (HAVE_ATOMIC)
- #include <stdatomic.h>
-+#if defined(HAVE_SCHED_YIELD)
-+#include <sched.h>
-+#endif
- 
- #define curl_simple_lock atomic_bool
- #define CURL_SIMPLE_LOCK_INIT false

patches/curl-7.84.0/0002-easy_lock.h-use-__asm__-instead-of-asm-to-fix-build.patch
@@ -1,22 +1,0 @@
-From: vvb2060 <vvb2060@gmail.com>
-Date: Tue, 28 Jun 2022 03:48:43 +0800
-Subject: [PATCH] easy_lock.h: use __asm__ instead of asm to fix build
-
-Closes #9056
----
- lib/easy_lock.h | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/lib/easy_lock.h b/lib/easy_lock.h
-index 1f54289ceb2d..07c85c5ffdd1 100644
---- a/lib/easy_lock.h
-+++ b/lib/easy_lock.h
-@@ -54,7 +54,7 @@ static inline void curl_simple_lock_lock(curl_simple_lock *lock)
- #if defined(__i386__) || defined(__x86_64__)
-       __builtin_ia32_pause();
- #elif defined(__aarch64__)
--      asm volatile("yield" ::: "memory");
-+      __asm__ volatile("yield" ::: "memory");
- #elif defined(HAVE_SCHED_YIELD)
-       sched_yield();
- #endif

patches/curl-7.84.0/series
@@ -1,5 +1,0 @@
-# generated by git-ptx-patches
-#tag:base --start-number 1
-0001-easy_lock.h-include-sched.h-if-available-to-fix-buil.patch
-0002-easy_lock.h-use-__asm__-instead-of-asm-to-fix-build.patch
-# 3ace341f1349f3d078579bbef0b35324  - git-ptx-patches magic

commit 4ff9656bf44f4350c303e9d4f2c2af82865fec18

libcurl: fix configure if no SSL backend is selected
If no SSL backend is selected configure will fail with the following
error:

| configure: error: TLS not detected, you will not be able to use HTTPS, FTPS, NTLM and more.
| Use --with-openssl, --with-gnutls, --with-wolfssl, --with-mbedtls,
| --with-nss, --with-schannel, --with-secure-transport, --with-amissl,
| --with-bearssl or --with-rustls to address this.

Fixes: 34db9dd470bd ("libcurl: Version bump. 7.84.0 -> 7.85.0")
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
Message-Id: <20220928121609.1209927-2-mkl@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libcurl.make
@@ -93,6 +93,7 @@ LIBCURL_CONF_OPT	:= \
 	--without-schannel \
 	--without-secure-transport \
 	--without-amissl \
+	--$(call ptx/wwo,PTXCONF_LIBCURL_SSL)-ssl \
 	--with-openssl=$(call ptx/ifdef, PTXCONF_LIBCURL_SSL_OPENSSL,$(SYSROOT)/usr,no) \
 	--with-gnutls=$(call ptx/ifdef, PTXCONF_LIBCURL_SSL_GNUTLS,$(SYSROOT)/usr,no) \
 	--without-mbedtls \