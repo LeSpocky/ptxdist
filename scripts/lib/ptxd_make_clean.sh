LeSpocky/ptxdist

commit c98e2ba9594cd07257fa6dc648b3ab8e5b4df1cb

detect patch changes and reextract when needed
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_clean.sh
@@ -12,10 +12,20 @@ ptxd_make_clean() {
     local directory="${1}"
 
     if [ -e "${directory}" ]; then
-	if [ -h "${directory}" ! -d "${directory}" ]; then
+	if [ -h "${directory}" -o ! -d "${directory}" ]; then
 	    rm -f "${directory}"
 	elif [ -n "${PTXDIST_FORCE}" ]; then
 	    : # always delete with --force
+	elif [[ -e "${ptx_make_target}" && "${ptx_make_target}" =~ .extract$ ]]; then
+	    if [ -z "${PTXCONF_SETUP_AUTO_EXTRACT}" ]; then
+		ptxd_bailout "Refusing to delete existing source" \
+		    "$(ptxd_print_path "${directory}")" \
+		    "Delete anyways with --force or  skip extract with --dirty."
+	    elif [ -z "${PTXDIST_FORCE}" -a -d "${directory}/.git" ]; then
+		ptxd_bailout "Refusing to delete existing source with git" \
+		    "$(ptxd_print_path "${directory}")" \
+		    "Delete anyways with --force or skip extract with --dirty."
+	    fi
 	elif [ -d "${directory}/.git" ]; then
 	    ptxd_bailout "Refusing to delete git repository" \
 		"$(ptxd_print_path "${directory}")" \

config/setup/Kconfig
@@ -348,6 +348,17 @@ config SETUP_DIRECT_CLEAN
 	help
 	  I'm a developer. I know what I'm doing. Just clean the BSP.
 
+config SETUP_AUTO_EXTRACT
+	bool "automatically reextract non-git sources"
+	help
+	  By default ptxdist will refuse to delete the existing package
+	  source tree if the extract stage is triggered again due to
+	  dependencies (e.g. new patches). This happens to ensure that
+	  local changes are not accidentally lost.
+	  If this option is enabled, then the sources are automatically
+	  deleted unless they contain a .git directory. This is useful if
+	  local modifications are only done in combination with --git.
+
 config SETUP_HOST_CPP
 	prompt "host CPP"
 	string

config/setup/ptxdistrc.default
@@ -1,6 +1,6 @@
 #
 # Automatically generated file; DO NOT EDIT.
-# PTXdist 2019.03.0
+# PTXdist 2019.06.0
 #
 
 #
@@ -66,6 +66,7 @@ PTXCONF_SETUP_ENV_WHITELIST=""
 # PTXCONF_SETUP_PATCHIN_GIT is not set
 # PTXCONF_SETUP_NFS_REL_SYMLINK is not set
 # PTXCONF_SETUP_DIRECT_CLEAN is not set
+# PTXCONF_SETUP_AUTO_EXTRACT is not set
 PTXCONF_SETUP_HOST_CPP="cpp"
 PTXCONF_SETUP_HOST_CC="gcc"
 PTXCONF_SETUP_HOST_CXX="g++"

rules/post/ptxd_make_world_common.make
@@ -86,6 +86,7 @@ world/env/impl = \
 	pkg_md5="$(call ptx/escape,$($(1)_MD5))"				\
 	pkg_url="$(call ptx/escape,$($(1)_URL))"				\
 	pkg_cfghash="$(call ptx/escape,$($(1)_CFGHASH))"			\
+	pkg_srchash="$(call ptx/escape,$($(1)_EXTRACT_CFGHASH))"		\
 										\
 	pkg_dir="$(call ptx/escape,$($(1)_DIR))"				\
 	pkg_subdir="$(call ptx/escape,$($(1)_SUBDIR))"				\

rules/post/ptxd_make_world_extract.make
@@ -8,6 +8,15 @@
 # see the README file.
 #
 
+world/srchash = \
+	+$(call world/env, $1) \
+	ptxd_make_world_hash srchash
+
+$(STATEDIR)/%.srchash:
+	@$(call targetinfo)
+	@$(call world/srchash, $(PTX_MAP_TO_PACKAGE_$(basename $(*))))
+	@$(call touch)
+
 #
 # extract
 #

rules/post/ptxd_make_world_prepare.make
@@ -10,7 +10,7 @@
 
 world/cfghash = \
 	+$(call world/env, $1) \
-	ptxd_make_world_hash srchash
+	ptxd_make_world_hash cfghash
 
 $(STATEDIR)/%.cfghash:
 	@$(call targetinfo)

scripts/lib/ptxd_lib_dgen.awk
@@ -342,6 +342,15 @@ function write_deps_pkg_all(this_PKG, this_pkg) {
 }
 
 function write_deps_pkg_active_cfghash(this_PKG, this_pkg) {
+	print "ifneq ($(" this_PKG "),)"									> DGEN_DEPS_POST;
+	print this_PKG "_PATCHES := $(call ptx/in-path,PTXDIST_PATH_PATCHES,$(" this_PKG "))"			> DGEN_DEPS_POST;
+	print "ifneq ($(wildcard $(" this_PKG "_PATCHES)),)"							> DGEN_DEPS_POST;
+	print "ifeq ($(wildcard " PTXDIST_TEMPDIR "/pkghash-" this_PKG ".done),)"				> DGEN_DEPS_POST;
+	print "$(call ptx/force-sh, find '$(" this_PKG "_PATCHES)' -type f ! -name '.*' | sort | xargs cat | tee " \
+		PTXDIST_TEMPDIR "/pkghash-" this_PKG "_EXTRACT >> " PTXDIST_TEMPDIR "/pkghash-" this_PKG ")"	> DGEN_DEPS_POST;
+	print "endif"												> DGEN_DEPS_POST;
+	print "endif"												> DGEN_DEPS_POST;
+	print "endif"												> DGEN_DEPS_POST;
 	print "ifneq ($(filter /%,$(" this_PKG "_CONFIG)),)"							> DGEN_DEPS_POST;
 	print "ifneq ($(wildcard $(" this_PKG "_CONFIG)),)"							> DGEN_DEPS_POST;
 	print "ifeq ($(wildcard " PTXDIST_TEMPDIR "/pkghash-" this_PKG ".done),)"				> DGEN_DEPS_POST;
@@ -372,6 +381,12 @@ function write_deps_pkg_active(this_PKG, this_pkg, prefix) {
 		print "endif"											> DGEN_DEPS_POST;
 	print "$(foreach src,$(" this_PKG "_SOURCES)," \
 		"$(eval $(STATEDIR)/" this_pkg ".get:"      "$(STATEDIR)/" this_pkg ".$(notdir $(src)).stamp))"	> DGEN_DEPS_POST;
+	if (DIRTY != "true") {
+		print "ifneq ($(" this_PKG "_EXTRACT_CFGHASH),)"						> DGEN_DEPS_POST;
+		print "$(STATEDIR)/" this_pkg ".extract: " \
+					"$(STATEDIR)/" this_pkg ".$(" this_PKG "_EXTRACT_CFGHASH).srchash"	> DGEN_DEPS_POST;
+		print "endif"											> DGEN_DEPS_POST;
+	}
 	print "$(STATEDIR)/" this_pkg ".extract: "                    "$(STATEDIR)/" this_pkg ".get"		> DGEN_DEPS_POST;
 	print "$(STATEDIR)/" this_pkg ".extract.post: "               "$(STATEDIR)/" this_pkg ".extract"	> DGEN_DEPS_POST;
 	print "$(STATEDIR)/" this_pkg ".prepare: "                    "$(STATEDIR)/" this_pkg ".extract.post"	> DGEN_DEPS_POST;

scripts/lib/ptxd_make_world_prepare.sh
@@ -31,6 +31,8 @@ ptxd_make_world_hash()
 	if [ "${h}" != "${hash}" ]; then
 	    if [ "${target}" = "pkghash" ]; then
 		echo -e "Configuration changed! Reconfiguring...\n"
+	    else
+		echo -e "Patches changed! Reextracting...\n"
 	    fi
 	fi
 	rm "${h}" || break

commit 8f8dbdd3530d7b63727013ad2e395e5d3e5b3c5b

clean: Fix a double whitespace
Fixes: c98e2ba9594c ("detect patch changes and reextract when needed")
Message-Id: <20221108084030.113565-1-u.kleine-koenig@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_clean.sh
@@ -18,7 +18,7 @@ ptxd_make_clean() {
 	    if [ -z "${PTXCONF_SETUP_AUTO_EXTRACT}" ]; then
 		ptxd_bailout "Refusing to delete existing source" \
 		    "$(ptxd_print_path "${directory}")" \
-		    "Delete anyways with --force or  skip extract with --dirty."
+		    "Delete anyways with --force or skip extract with --dirty."
 	    elif [ -z "${PTXDIST_FORCE}" -a -d "${directory}/.git" ]; then
 		ptxd_bailout "Refusing to delete existing source with git" \
 		    "$(ptxd_print_path "${directory}")" \