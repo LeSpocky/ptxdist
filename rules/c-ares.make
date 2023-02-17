LeSpocky/ptxdist

commit d3e729ff2c2a12a834c8f65ad8d2d76a38c57899

c-ares: version bump 1.10.0 -> 1.14.0
Signed-off-by: Roland Hieber <r.hieber@pengutronix.de>
[mol: add some more configure options]
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/c-ares.make
@@ -16,14 +16,15 @@ PACKAGES-$(PTXCONF_C_ARES) += c-ares
 #
 # Paths and names
 #
-C_ARES_VERSION	:= 1.10.0
-C_ARES_MD5	:= 1196067641411a75d3cbebe074fd36d8
+C_ARES_VERSION	:= 1.14.0
+C_ARES_MD5	:= e57b37a7c46283e83c21cde234df10c7
 C_ARES		:= c-ares-$(C_ARES_VERSION)
 C_ARES_SUFFIX	:= tar.gz
 C_ARES_URL	:= http://c-ares.haxx.se/download/$(C_ARES).$(C_ARES_SUFFIX)
 C_ARES_SOURCE	:= $(SRCDIR)/$(C_ARES).$(C_ARES_SUFFIX)
 C_ARES_DIR	:= $(BUILDDIR)/$(C_ARES)
 C_ARES_LICENSE	:= MIT
+C_ARES_LICENSE_MD5	:= file://LICENSE.md;md5=fb997454c8d62aa6a47f07a8cd48b006
 
 
 # ----------------------------------------------------------------------------
@@ -42,8 +43,12 @@ C_ARES_CONF_OPT              := \
 	--disable-werror \
 	--disable-curldebug \
 	--enable-symbol-hiding \
+	--disable-expose-statics \
+	--disable-code-coverage \
 	$(GLOBAL_LARGE_FILE_OPTION) \
+	--disable-libgcc \
 	--enable-nonblocking \
+	--disable-tests \
 	--with-random=/dev/urandom
 
 # ----------------------------------------------------------------------------

commit fd5da21491dd93db75c41222c0ddf89a05c3996b

c-ares: fix license files variable name
The variable name did nothing, but LICENSE was auto-detected anyway. Now
this just pins down the MD5.

Fixes: d3e729ff2c2a12a834c8 (2018-05-30, "c-ares: version bump 1.10.0 -> 1.14.0")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20230213093117.1865063-1-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/c-ares.make
@@ -22,7 +22,7 @@ C_ARES_URL	:= http://c-ares.haxx.se/download/$(C_ARES).$(C_ARES_SUFFIX)
 C_ARES_SOURCE	:= $(SRCDIR)/$(C_ARES).$(C_ARES_SUFFIX)
 C_ARES_DIR	:= $(BUILDDIR)/$(C_ARES)
 C_ARES_LICENSE	:= MIT
-C_ARES_LICENSE_MD5	:= file://LICENSE.md;md5=fb997454c8d62aa6a47f07a8cd48b006
+C_ARES_LICENSE_FILES	:= file://LICENSE.md;md5=fb997454c8d62aa6a47f07a8cd48b006
 
 
 # ----------------------------------------------------------------------------