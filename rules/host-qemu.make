LeSpocky/ptxdist

commit e3060a69c0190dfa7131e5955ab1a444f628c501

qemu: Version bump. 7.1.0 -> 7.2.0
A bunch of traffic in QEMU, as usual.
Notable is the RISC-V work and x86 TCG gains support for AVX, AVX2, F16C, FMA3 and VAES instructions.
Changelog: https://wiki.qemu.org/ChangeLog/7.2
This release plugs CVEs: CVE-2022-3872, CVE-2022-2962, CVE-2022-3165 and CVE-2022-4144.

* Forward patches, with a minor correction, seems to apply cleanly.
* Be more explicit about libvduse, disable it.

Signed-off-by: Christian Melki <christian.melki@t2data.com>
Message-Id: <20221215141131.3689507-1-christian.melki@t2data.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/qemu.make
@@ -14,8 +14,8 @@ PACKAGES-$(PTXCONF_QEMU) += qemu
 #
 # Paths and names
 #
-QEMU_VERSION	:= 7.1.0
-QEMU_MD5	:= 3be5458a9171b4ec5220c65d5d52bdcf
+QEMU_VERSION	:= 7.2.0
+QEMU_MD5	:= 7630d6a9eba7ab2bcb9979d6d24c2697
 QEMU		:= qemu-$(QEMU_VERSION)
 QEMU_SUFFIX	:= tar.xz
 QEMU_URL	:= https://download.qemu.org/$(QEMU).$(QEMU_SUFFIX)
@@ -128,6 +128,7 @@ QEMU_CONF_OPT	:= \
 	--disable-usb-redir \
 	--disable-vde \
 	--disable-vhost-user-blk-server \
+	--disable-libvduse \
 	--disable-virglrenderer \
 	--$(call ptx/endis, PTXCONF_QEMU_SYS)-virtfs \
 	--disable-virtiofsd \

patches/qemu-7.2.0/0101-let-ninja-use-the-jobserver.patch
@@ -11,10 +11,10 @@ Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>
  1 file changed, 1 insertion(+), 1 deletion(-)
 
 diff --git a/Makefile b/Makefile
-index e5fd1ebdf619..23128172d02a 100644
+index a48103cc8a1a..9fbd7b095ca9 100644
 --- a/Makefile
 +++ b/Makefile
-@@ -142,7 +142,7 @@ MAKE.k = $(findstring k,$(firstword $(filter-out --%,$(MAKEFLAGS))))
+@@ -145,7 +145,7 @@ MAKE.k = $(findstring k,$(firstword $(filter-out --%,$(MAKEFLAGS))))
  MAKE.q = $(findstring q,$(firstword $(filter-out --%,$(MAKEFLAGS))))
  MAKE.nq = $(if $(word 2, $(MAKE.n) $(MAKE.q)),nq)
  NINJAFLAGS = $(if $V,-v) $(if $(MAKE.n), -n) $(if $(MAKE.k), -k0) \
@@ -22,4 +22,4 @@ index e5fd1ebdf619..23128172d02a 100644
 +        $(filter-out -j, $(lastword $(filter -l% -j%, $(MAKEFLAGS)))) \
          -d keepdepfile
  ninja-cmd-goals = $(or $(MAKECMDGOALS), all)
- ninja-cmd-goals += $(foreach g, $(MAKECMDGOALS), $(.ninja-goals.$g))))
+ ninja-cmd-goals += $(foreach g, $(MAKECMDGOALS), $(.ninja-goals.$g))

commit 506aa4ff4a97f697fee79c457b928efaae4b8aec

qemu, host-qemu: re-enable user networking via libslirp
In qemu 7.2, the bundled libslirp necessary for user networking was
removed [1], and as such '-netdev user,…' parameters will no longer
work. User networking needs no infrastructure and is therefore nice to
have for ad-hoc virtual machines, so re-enable it by building against
libslirp.

[1]: https://wiki.qemu.org/ChangeLog/7.2#SLIRP_module_(user-mode_networking)

Fixes: e3060a69c0190dfa7131 (2022-12-15, "qemu: Version bump. 7.1.0 -> 7.2.0")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20230312185306.2402677-2-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/host-qemu.in
@@ -6,6 +6,7 @@ menuconfig HOST_QEMU
 	prompt "host-side qemu                "
 	select HOST_GLIB
 	select HOST_LIBCAP_NG
+	select HOST_LIBSLIRP
 	select HOST_ZLIB
 	select HOST_SYSTEM_PYTHON3
 	select HOST_LIBUSB	if HOST_QEMU_SYS

rules/host-qemu.make
@@ -120,6 +120,7 @@ HOST_QEMU_CONF_OPT	:= \
 	--disable-sdl-image \
 	--disable-seccomp \
 	--disable-selinux \
+	--enable-slirp \
 	--disable-smartcard \
 	--disable-snappy \
 	--disable-sparse \

rules/qemu.in
@@ -9,6 +9,7 @@ menuconfig QEMU
 	select GCCLIBS_CXX
 	select GLIB
 	select LIBCAP_NG
+	select LIBSLIRP
 	select ZLIB
 	select SDL2		if QEMU_SDL
 	select GTK		if QEMU_GTK

rules/qemu.make
@@ -118,6 +118,7 @@ QEMU_CONF_OPT	:= \
 	--disable-sdl-image \
 	--disable-seccomp \
 	--disable-selinux \
+	--enable-slirp \
 	--disable-smartcard \
 	--disable-snappy \
 	--disable-sparse \