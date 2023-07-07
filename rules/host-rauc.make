LeSpocky/ptxdist

commit c2ad273712cfb89c292dd5a72e8e1ee87a997ab7

RAUC: leverage --disable-create option to shrink size for target RAUC
We do not need bundle creation capabilities on the target in most cases,
thus make this optional (and disable by default).

Signed-off-by: Enrico Jorns <ejo@pengutronix.de>
Message-Id: <20220615142217.990960-4-ejo@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/host-rauc.make
@@ -27,6 +27,7 @@ HOST_RAUC_CONF_OPT	:= \
 	--disable-Werror \
 	--disable-code-coverage \
 	--disable-valgrind \
+	--enable-create \
 	--disable-service \
 	--enable-create \
 	--disable-network \

rules/rauc.make
@@ -43,6 +43,7 @@ RAUC_CONF_OPT	:= \
 	--disable-Werror \
 	--disable-code-coverage \
 	--disable-valgrind \
+	--$(call ptx/endis,PTXCONF_RAUC_CREATE)-create \
 	--$(call ptx/endis,PTXCONF_RAUC_SERVICE)-service \
 	--disable-create \
 	--$(call ptx/endis,PTXCONF_RAUC_NETWORK)-network \

rules/rauc.in
@@ -27,6 +27,15 @@ menuconfig RAUC
 
 if RAUC
 
+config RAUC_CREATE
+	bool
+	prompt "Enable bundle creation support on target"
+	help
+	  Enables support for creating bundles on target.
+	  Disabling this will remove functionality required for bundle
+	  creation which is not required on the target in most cases and thus
+	  allows to save space.
+
 config RAUC_SERVICE
 	bool
 	prompt "Enable service (D-Bus) support"

commit 13713e62b43478e8aa5e284cd6b739c34253db71

rauc: fix PTXCONF_RAUC_CREATE handling
The existing --disable-create/--disable-create configure options were
not removed when the PTXCONF_RAUC_CREATE kconfig was introduced.

As a result of this, create could not be enabled for the target (since
it was forced to --disable-create afterwards) and the --enable-create
line was duplicated within the host-rauc recipe.

Fixes: c2ad273712cfb89c292dd5a72e8e1ee87a997ab7
("RAUC: leverage --disable-create option to shrink size for target RAUC")

Signed-off-by: Enrico Jorns <ejo@pengutronix.de>
Message-Id: <20230630204927.2700230-1-ejo@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/host-rauc.make
@@ -27,7 +27,6 @@ HOST_RAUC_CONF_OPT	:= \
 	--disable-Werror \
 	--disable-code-coverage \
 	--disable-valgrind \
-	--enable-create \
 	--disable-service \
 	--enable-create \
 	--disable-network \

rules/rauc.make
@@ -45,7 +45,6 @@ RAUC_CONF_OPT	:= \
 	--disable-valgrind \
 	--$(call ptx/endis,PTXCONF_RAUC_CREATE)-create \
 	--$(call ptx/endis,PTXCONF_RAUC_SERVICE)-service \
-	--disable-create \
 	--$(call ptx/endis,PTXCONF_RAUC_NETWORK)-network \
 	--$(call ptx/endis,PTXCONF_RAUC_STREAMING)-streaming \
 	--$(call ptx/endis,PTXCONF_RAUC_JSON)-json \