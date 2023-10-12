LeSpocky/ptxdist

commit 40a39c7a94d1f8821fab5a09a827937e00fe62af

image-hdimg: support using barebox with GPT
Currently, choosing IMAGE_HDIMG_GPT together with IMAGE_HDIMG_BAREBOX
leads to an error in genimage because the full GPT array is larger than
the hole in the barebox image. However, the hole in the barebox images
is large enough to fit the protective MBR and the primary GPT header.
The rest of the GPT array needs to moved after the barebox image, so set
the gpt-location setting to the size of the barebox image.

For the other bootloaders, nothing needs to change, so set the
gpt-location to its default value of 1024.

For MBR, this config setting is ignored.

Link: https://www.barebox.org/doc/latest/boards/imx.html#using-gpt-on-i-mx
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20230316165132.423488-1-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/image-hdimg.make
@@ -30,17 +30,20 @@ IMAGE_HDIMG_BOOTLOADER_ENV := \
 
 ifdef PTXCONF_IMAGE_HDIMG_GRUB
 IMAGE_HDIMG_BOOTLOADER_ENV = \
+	GPT_LOCATION='1024' \
 	GRUB_STAGE_DIR=$(GRUB_STAGE_DIR) \
 	BOOTLOADER_IMAGES='include("grub.config")' \
 	BOOTLOADER_PARTITIONS='include("grub_partitions.config")'
 endif
 ifdef PTXCONF_IMAGE_HDIMG_BAREBOX
 IMAGE_HDIMG_BOOTLOADER_ENV := \
+	GPT_LOCATION='2M' \
 	BOOTLOADER_IMAGES='' \
 	BOOTLOADER_PARTITIONS='include("barebox_partitions.config")'
 endif
 ifdef PTXCONF_IMAGE_HDIMG_VFAT
 IMAGE_HDIMG_BOOTLOADER_ENV := \
+	GPT_LOCATION='1024' \
 	BOOTLOADER_IMAGES='' \
 	BOOTLOADER_PARTITIONS='include("vfat_partitions.config")'
 

config/images/barebox_partitions.config
@@ -1,6 +1,6 @@
 partition barebox {
 	in-partition-table = false
 	image = "barebox-image"
-	holes = {"(440; 512)"}
+	holes = {"(440; 1024)"}
 	size = 2M
 }

config/images/hd.config
@@ -5,6 +5,7 @@ image @IMAGE@ {
 	hdimage {
 		align = 1M
 		partition-table-type = "@PARTITION_TABLE_TYPE@"
+		gpt-location = "@GPT_LOCATION@"
 	}
 	@BOOTLOADER_PARTITIONS@
 	partition root {

commit 0bf8bc3cecbdf085e427374dcea4d760951e7822

image-hdimg: fix building image without bootloader
genimage complains that '@GPT_LOCATION@' is an invalid size suffix
because we replace that template variable in the config file only if
IMAGE_HDIMG_BAREBOX, IMAGE_HDIMG_GRUB, or IMAGE_HDIMG_VFAT are selected.
Fix the use case for IMAGE_HDIMG_NONE too.

Fixes: 40a39c7a94d1f8821fab (2023-03-16, "image-hdimg: support using barebox with GPT")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20231009113710.493504-1-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/image-hdimg.make
@@ -25,6 +25,7 @@ IMAGE_HDIMG_CONFIG	:= hd.config
 
 ifdef PTXCONF_IMAGE_HDIMG
 IMAGE_HDIMG_BOOTLOADER_ENV := \
+	GPT_LOCATION='0' \
 	BOOTLOADER_IMAGES='' \
 	BOOTLOADER_PARTITIONS=''
 