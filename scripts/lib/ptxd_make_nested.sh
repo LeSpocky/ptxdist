LeSpocky/ptxdist

commit 5169ae99daf821ef820f1de635b8e93634ed061f

add support for nested PTXdist execution
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_nested.sh
@@ -1,0 +1,81 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+ptxd_make_nested_ptxdist_impl() {
+    # close all unneeded fds. The nested PTXdist will open its own fds.
+    exec 9>&-
+    exec {PTXDIST_FD_STDOUT}>&-
+    exec {PTXDIST_FD_STDERR}>&-
+    exec {ptxd_make_serialize_get_writefd}>&-
+    exec {ptxd_make_serialize_get_readfd}>&-
+    exec {ptxd_make_serialize_extract_writefd}>&-
+    exec {ptxd_make_serialize_extract_readfd}>&-
+
+    # let shell split by IFS
+    set -- ${PTXCONF_SETUP_ENV_WHITELIST} ${PTXDIST_ENV_WHITELIST} PTXDIST_ENV_WHITELIST
+    whitelist="${*}"
+    whitelist="${whitelist:+|}${whitelist// /|}"
+
+    unset $({
+	export -p  | sed -n 's/^declare -x \([^=]*\).*$/\1/p'
+	export -fp | sed -n 's/^declare -fx \([^=]*\).*$/\1/p'
+	} | grep -E -v "^(args|PTXDIST_PTXRC|PTX_AUTOBUILD_DESTDIR|PTXDIST_PLATFORMDIR|CCACHE_.*|PWD|HOME|USER|PATH|TERM|COLUMNS|LINES|DISPLAY|TMPDIR|http_proxy|https_proxy|ftp_proxy|no_proxy${whitelist})$")
+
+    PATH=$(sed -e 's;[^:]*/sysroot-\(host\|cross\)/[^:]*:;;g' -e "s;${PTXDIST_TOPDIR}/bin:;;" -e "s;${PTXDIST_TOOLCHAIN}:;;" <<< "${PATH}")
+
+    eval "${args[@]}"
+}
+export -f ptxd_make_nested_ptxdist_impl
+
+#
+# Clear the environment and call a nested PTXdist.
+# Can be used to build other platforms.
+#
+ptxd_make_nested_ptxdist() {
+    local -a args
+
+    ptxd_make_world_init || return
+
+    if [ -z "${pkg_workspace}" ]; then
+	pkg_workspace="${PTXDIST_WORKSPACE}"
+    fi
+    args=( "cd" "${pkg_workspace}" "&&" "${PTXDIST_TOPDIR}/bin/ptxdist" )
+
+    if [ "${PTXDIST_DIRTY}" = 1 ]; then
+	args[${#args[*]}]="--dirty"
+    fi
+    if [ "${PTXDIST_FORCE}" = 1 ]; then
+	args[${#args[*]}]="--force"
+    fi
+    if [ "${PTXDIST_PEDANTIC}" = 1 ]; then
+	args[${#args[*]}]="--pedantic"
+    fi
+    if [ "${PTXDIST_QUIET}" = 1 ]; then
+	args[${#args[*]}]="--quiet"
+    fi
+    if [ "${PTXDIST_VERBOSE}" = 1 ]; then
+	args[${#args[*]}]="--verbose"
+    fi
+    if [ -n "${PTXDIST_PARALLEL_FLAGS}" ]; then
+	args[${#args[*]}]="${PTXDIST_PARALLEL_FLAGS}"
+    fi
+    if [[ "${PTXDIST_JOBSERVER_FLAGS}" =~ ^--jobserver-auth ]]; then
+	args[${#args[*]}]="${PTXDIST_JOBSERVER_FLAGS}"
+    fi
+    args+=( "${@}" )
+
+    ptxd_verbose "executing:" "${args[@]}"
+
+    # run ptxdist but don't log it. It has it's on logfile
+    (
+	exec 1> >(sed "s/^/${pkg_label}: /" >&${PTXDIST_FD_STDOUT})
+	exec 2> >(sed "s/^/${pkg_label}: /" >&${PTXDIST_FD_STDERR})
+	ptxd_make_nested_ptxdist_impl
+    )
+}
+export -f ptxd_make_nested_ptxdist

bin/ptxdist
@@ -2211,10 +2211,11 @@ setup_env() {
 	whitelist="${*}"
 	whitelist="${whitelist:+|}${whitelist// /|}"
 
+	# Any change here should be done in ptxd_make_nested_ptxdist() as well
 	unset $({
 		export -p  | sed -n 's/^declare -x \([^=]*\).*$/\1/p'
 		export -fp | sed -n 's/^declare -fx \([^=]*\).*$/\1/p'
-		} | grep -E -v "^(PTXDIST_PTXRC|PTX_AUTOBUILD_DESTDIR|CCACHE_.*|PWD|HOME|USER|PATH|TERM|COLUMNS|LINES|DISPLAY|TMPDIR|KCONFIG_ALLCONFIG|KCONFIG_SEED|http_proxy|https_proxy|ftp_proxy|no_proxy${whitelist})$")
+		} | grep -E -v "^(PTXDIST_PTXRC|PTX_AUTOBUILD_DESTDIR|PTXDIST_PLATFORMDIR|CCACHE_.*|PWD|HOME|USER|PATH|TERM|COLUMNS|LINES|DISPLAY|TMPDIR|KCONFIG_ALLCONFIG|KCONFIG_SEED|http_proxy|https_proxy|ftp_proxy|no_proxy${whitelist})$")
 
 	######## the environment is clean now ########
 
@@ -2496,7 +2497,7 @@ setup_platform_continue_with_broken_config()
 #      "*DIR"	correct directory definitions
 #
 setup_platform() {
-	local platform platform_version project project_version
+	local platform platform_version project project_version old_platformdir
 
 	# ptxd_get_ptxconf's return value is
 	platform="$(ptxd_get_ptxconf PTXCONF_PLATFORM)"				|| [ ${?} -eq 1 ] &&
@@ -2506,12 +2507,17 @@ setup_platform() {
 	# remove space and "(" ")"
 	project_version="${project_version//[ \(\)]/_}"
 
+	old_platformdir="${PTXDIST_PLATFORMDIR}"
 	if [ -n "${platform}" ]; then
 		PTXDIST_PLATFORMDIR="${PTXDIST_WORKSPACE}/platform-${platform}${platform_version}"
 		PTXDIST_PLATFORMSUFFIX=".${platform}"
 	else
 		PTXDIST_PLATFORMDIR="${PTXDIST_WORKSPACE}/platform"
 		PTXDIST_PLATFORMSUFFIX=""
+	fi
+
+	if [ "${old_platformdir}" = "${PTXDIST_PLATFORMDIR}" ]; then
+		ptxd_bailout "Nested PTXdist execution for the same platform is not allowed!"
 	fi
 
 	# reread vars with correct PTXDIST_PLATFORMDIR

rules/post/ptxd_make_world_nested.make
@@ -1,0 +1,15 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2023 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+world/nested = \
+	+$(call world/env, $(1)) \
+	pkg_workspace="$(call ptx/escape,$($(strip $(1))_WORKSPACE))" \
+	ptxd_make_nested_ptxdist $(2)
+
+
+# vim: syntax=make:

commit 98bc55e65d3b5c64ea15c86d2ac6627fdb13fbae

ptxd_make_nested: Fix parameter passing for --dirty, --force and --pedantic
Some variables corresponding to ptxdist parameters use "true" as value
when given, others use "1". PTXDIST_DIRTY, PTXDIST_FORCE and
PTXDIST_PEDANTIC are in the first group according to

	git grep -E 'PTXDIST_(DIRTY|FORCE|FEDANTIC)='

. Adapt ptxd_make_nested_ptxdist() accordingly.

Fixes: 5169ae99daf8 ("add support for nested PTXdist execution")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Message-Id: <20231110093705.45879-2-u.kleine-koenig@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_nested.sh
@@ -46,13 +46,13 @@ ptxd_make_nested_ptxdist() {
     fi
     args=( "cd" "${pkg_workspace}" "&&" "${PTXDIST_TOPDIR}/bin/ptxdist" )
 
-    if [ "${PTXDIST_DIRTY}" = 1 ]; then
+    if [ "${PTXDIST_DIRTY}" = true ]; then
 	args[${#args[*]}]="--dirty"
     fi
-    if [ "${PTXDIST_FORCE}" = 1 ]; then
+    if [ "${PTXDIST_FORCE}" = true ]; then
 	args[${#args[*]}]="--force"
     fi
-    if [ "${PTXDIST_PEDANTIC}" = 1 ]; then
+    if [ "${PTXDIST_PEDANTIC}" = true ]; then
 	args[${#args[*]}]="--pedantic"
     fi
     if [ "${PTXDIST_QUIET}" = 1 ]; then