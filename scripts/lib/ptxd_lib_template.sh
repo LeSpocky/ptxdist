LeSpocky/ptxdist

commit c9659e6c318f1291186316c98bfcf1bcd047cdca

templates: add template for custom FIT images
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20230912165759.3492269-2-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_template.sh
@@ -598,6 +598,31 @@ export -f ptxd_template_new_image_tgz
 ptxd_template_help_list[${#ptxd_template_help_list[@]}]="image-tgz"
 ptxd_template_help_list[${#ptxd_template_help_list[@]}]="create package for a tgz image"
 
+ptxd_template_new_image_fit() {
+    export class="image-"
+    ptxd_template_read_name &&
+    ptxd_template_read_author &&
+    ptxd_template_read "sign FIT image? (y/N)" SIGN
+    case "$SIGN" in
+	y*|Y*)
+	    export select_CODE_SIGNING="select CODE_SIGNING
+	"
+	    export CODE_SIGNING_VARS="
+IMAGE_@PACKAGE@_SIGN_ROLE	:= # TODO: role name of the code signing provider, passed to cs_get_uri
+IMAGE_@PACKAGE@_KEY_NAME_HINT	:= # TODO: key-name-hint property in the signature node of the FIT image
+"
+	;;
+	*)
+	    export select_CODE_SIGNING=""
+	    export CODE_SIGNING_VARS=""
+	;;
+    esac
+    ptxd_template_write_platform_rules
+}
+export -f ptxd_template_new_image_fit
+ptxd_template_help_list[${#ptxd_template_help_list[@]}]="image-fit"
+ptxd_template_help_list[${#ptxd_template_help_list[@]}]="create package for a FIT image"
+
 ptxd_template_new_image_genimage() {
     export class="image-"
     ptxd_template_read_name &&

rules/templates/template-image-fit-in
@@ -1,0 +1,10 @@
+## SECTION=image
+
+config IMAGE_@PACKAGE@
+	tristate
+	select HOST_U_BOOT_TOOLS
+	@select_CODE_SIGNING@select IMAGE_KERNEL
+	select IMAGE_ROOT_CPIO
+	prompt "Generate images/@package@.fit"
+	help
+	  FIXME

rules/templates/template-image-fit-make
@@ -1,0 +1,34 @@
+# -*-makefile-*-
+#
+# Copyright (C) @YEAR@ by @AUTHOR@
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+IMAGE_PACKAGES-$(PTXCONF_IMAGE_@PACKAGE@) += image-@package@
+
+#
+# Paths and names
+#
+IMAGE_@PACKAGE@			:= image-@package@
+IMAGE_@PACKAGE@_DIR		:= $(BUILDDIR)/$(IMAGE_@PACKAGE@)
+IMAGE_@PACKAGE@_IMAGE		:= $(IMAGEDIR)/@package@.fit
+IMAGE_@PACKAGE@_VERSION		:= ${PTXCONF_PROJECT_VERSION}
+IMAGE_@PACKAGE@_KERNEL		= $(IMAGE_KERNEL_IMAGE)
+IMAGE_@PACKAGE@_DTB		= # TODO: $(IMAGEDIR)/your-board.dtb
+IMAGE_@PACKAGE@_INITRAMFS	= $(IMAGE_ROOT_CPIO_IMAGE)
+@CODE_SIGNING_VARS@
+# ----------------------------------------------------------------------------
+# Image
+# ----------------------------------------------------------------------------
+
+$(IMAGE_@PACKAGE@_IMAGE): $(IMAGE_@PACKAGE@_KERNEL) $(IMAGE_@PACKAGE@_INITRAMFS) $(IMAGE_@PACKAGE@_DTB)
+	@$(call targetinfo)
+	@$(call world/image-fit, IMAGE_@PACKAGE@)
+	@$(call finish)
+
+# vim: syntax=make

commit b67947db5769533df662efaf52fcde3ecbcea8fe

ptxd_lib_template: image-fit: Fix nested replacement
The @PACKAGE@ inside of $CODE_SIGNING_VARS was not replaced before,
leading to output in the make rule like this:

  IMAGE_@PACKAGE@_SIGN_ROLE       := # TODO: role name of the code signing provider, passed to cs_get_uri
  IMAGE_@PACKAGE@_KEY_NAME_HINT   := # TODO: key-name-hint property in the signature node of the FIT image

Using shell variables before assignement, it looks
like this now (for image fit package named 'foo'):

  IMAGE_FOO_SIGN_ROLE     := # TODO: role name of the code signing provider, passed to cs_get_uri
  IMAGE_FOO_KEY_NAME_HINT := # TODO: key-name-hint property in the signature node of the FIT image

Fixes: c9659e6c318f ("templates: add template for custom FIT images")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Message-Id: <20241023060628.8357-1-ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_template.sh
@@ -615,8 +615,8 @@ ptxd_template_new_image_fit() {
 	    export select_CODE_SIGNING="select CODE_SIGNING
 	"
 	    export CODE_SIGNING_VARS="
-IMAGE_@PACKAGE@_SIGN_ROLE	:= # TODO: role name of the code signing provider, passed to cs_get_uri
-IMAGE_@PACKAGE@_KEY_NAME_HINT	:= # TODO: key-name-hint property in the signature node of the FIT image
+IMAGE_${PACKAGE}_SIGN_ROLE	:= # TODO: role name of the code signing provider, passed to cs_get_uri
+IMAGE_${PACKAGE}_KEY_NAME_HINT	:= # TODO: key-name-hint property in the signature node of the FIT image
 "
 	;;
 	*)