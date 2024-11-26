LeSpocky/ptxdist

commit 2531a1942ed664e297bd2c17bcd25551d63fbd4e

chrony: enable support for PPS devices
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20210604180715.4674-2-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/chrony.in
@@ -8,6 +8,7 @@ menuconfig CHRONY
 	select LIBCAP
 	select LIBSECCOMP	if CHRONY_SECCOMP
 	select NETTLE		if CHRONY_USE_NETTLE
+	select PPS_TOOLS	if CHRONY_PPS_REFCLK
 	help
 	  This will install the Chrony NTPD Daemon (chronyd)
 	  Chrony is a pair of programs which are used to maintain
@@ -43,6 +44,13 @@ config CHRONY_ADVANCED_COMMAND
 	help
 	  This allows getting advanced NTP time synchronization
 	  status monitoring from the service.
+
+config CHRONY_PPS_REFCLK
+	bool
+	prompt "Enable support for PPS devices"
+	help
+	  Pulse per second devices signal the start of every second
+	  with high precision.
 
 comment "install options   ---"
 

rules/chrony.make
@@ -52,7 +52,7 @@ CHRONY_CONF_OPT		:= \
 	$(call ptx/ifdef, PTXCONF_CHRONY_ADVANCED_COMMAND,--enable-debug,) \
 	--disable-refclock \
 	--disable-phc \
-	--disable-pps \
+	$(call ptx/ifdef, PTXCONF_CHRONY_PPS_REFCLK,,--disable-pps) \
 	$(call ptx/ifdef, PTXCONF_GLOBAL_IPV6,,--disable-ipv6) \
 	--with-user=chrony \
 	$(call ptx/ifdef, PTXCONF_CHRONY_SECCOMP,--enable-scfilter,) \

commit 7251ffe9266aed35122e5c5fb8eee8ecb5b25c78

chrony: fix use of PHC and PTP reference clocks
Passing --disable-pps to ./configure doesn't matter if we unconditionally
disable all reference clock support anyway[1].

All of refclock, phc and pps are enabled by default, so let's add
options for refclock and phc that allow disabling them and add the
proper dependencies, so both CHRONY_PPS_REFCLK and CHRONY_PHC_REFCLK
can actually work.

[1]: https://gitlab.com/chrony/chrony/-/blob/4.6/configure?ref_type=tags#L773-794

Fixes: 2531a1942ed6 ("chrony: enable support for PPS devices")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Message-Id: <20241125080958.380467-1-a.fatoum@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/chrony.in
@@ -45,11 +45,24 @@ config CHRONY_ADVANCED_COMMAND
 	  This allows getting advanced NTP time synchronization
 	  status monitoring from the service.
 
+config CHRONY_REFCLK
+	bool
+	prompt "Enable reference clock support"
+
 config CHRONY_PPS_REFCLK
 	bool
+	select CHRONY_REFCLK
 	prompt "Enable support for PPS devices"
 	help
 	  Pulse per second devices signal the start of every second
+	  with high precision.
+
+config CHRONY_PHC_REFCLK
+	bool
+	select CHRONY_REFCLK
+	prompt "Enable support for PHC devices"
+	help
+	  PTP hardware clock devices signal the start of every second
 	  with high precision.
 
 comment "install options   ---"

rules/chrony.make
@@ -50,8 +50,8 @@ CHRONY_CONF_OPT		:= \
 	--without-tomcrypt \
 	$(call ptx/ifdef, PTXCONF_CHRONY_ADVANCED_COMMAND,,--disable-cmdmon) \
 	$(call ptx/ifdef, PTXCONF_CHRONY_ADVANCED_COMMAND,--enable-debug,) \
-	--disable-refclock \
-	--disable-phc \
+	$(call ptx/ifdef, PTXCONF_CHRONY_REFCLK,,--disable-refclock) \
+	$(call ptx/ifdef, PTXCONF_CHRONY_PHC_REFCLK,,--disable-phc) \
 	$(call ptx/ifdef, PTXCONF_CHRONY_PPS_REFCLK,,--disable-pps) \
 	$(call ptx/ifdef, PTXCONF_GLOBAL_IPV6,,--disable-ipv6) \
 	--with-user=$(call ptx/ifdef, PTXCONF_INITMETHOD_SYSTEMD,chrony,root) \