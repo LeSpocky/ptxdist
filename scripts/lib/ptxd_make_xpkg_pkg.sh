LeSpocky/ptxdist

commit baf33e7186cb8fe7a1777054e5ae0ddd62b2bf42

ptxd_install_file_extract_debug: pick the debug compression format from the toolchain
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_xpkg_pkg.sh
@@ -384,7 +384,18 @@ ptxd_extract_build_id() {
 }
 export -f ptxd_extract_build_id
 
-export ptxd_install_file_objcopy_args="--only-keep-debug --compress-debug-sections"
+ptxd_install_compression_format() {
+    local libc
+    local comp
+
+    libc="$(ptxd_cross_cc -print-file-name=libc.so.6 2> /dev/null)"
+    if [ -n "${libc}" ]; then
+	comp="=$(readelf -t "${libc}" | sed -n -e '/COMPRESSED/{N;s/.*\(ZLIB\|ZSTD\).*/\1/p;q}' | tr '[:upper:]' '[:lower:]')"
+    fi
+    export ptxd_install_file_objcopy_args="--only-keep-debug --compress-debug-sections${comp}"
+}
+
+ptxd_install_compression_format
 
 ptxd_install_file_extract_debug() {
     local dst="${1}"

commit 21f4c86df87fcf2e5e17c732804d2afbc440eb4e

ptxd_install_compression_format: don't use host readelf to figure out debug compression format
The host readelf might be too old and doesn't understand the
compression formats of new toolchain libraries. This leads to the
following error:

| readelf: Warning: section '.debug_str' has unsupported compress type: 2

Use coss-readelf of toolchain instead.

Fixes: baf33e7186cb ("ptxd_install_file_extract_debug: pick the debug compression format from the toolchain")
Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
Message-Id: <20241218-fix-ptxd_install_compression_format-v1-1-8b4973b0bb55@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_make_xpkg_pkg.sh
@@ -390,7 +390,7 @@ ptxd_install_compression_format() {
 
     libc="$(ptxd_cross_cc -print-file-name=libc.so.6 2> /dev/null)"
     if [ -n "${libc}" ]; then
-	comp="=$(readelf -t "${libc}" | sed -n -e '/COMPRESSED/{N;s/.*\(ZLIB\|ZSTD\).*/\1/p;q}' | tr '[:upper:]' '[:lower:]')"
+	comp="=$($(ptxd_get_ptxconf PTXCONF_COMPILER_PREFIX)readelf -t "${libc}" | sed -n -e '/COMPRESSED/{N;s/.*\(ZLIB\|ZSTD\).*/\1/p;q}' | tr '[:upper:]' '[:lower:]')"
     fi
     export ptxd_install_file_objcopy_args="--only-keep-debug --compress-debug-sections${comp}"
 }