LeSpocky/ptxdist

commit 6d447e0278dfc6121ffaf3c246c0a27af05cc5e7

weston: build color management module
Add an option to build weston color management module.

Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>
Message-Id: <01020191bd736848-cdc7caea-1dd9-4d3d-bee3-88de4984cab9-000000@eu-west-1.amazonses.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/weston.make
@@ -45,7 +45,7 @@ WESTON_CONF_OPT		:= \
 	-Dbackend-vnc=$(call ptx/truefalse,PTXCONF_WESTON_BACKEND_VNC) \
 	-Dbackend-wayland=$(call ptx/truefalse,PTXCONF_WESTON_GL) \
 	-Dbackend-x11=false \
-	-Dcolor-management-lcms=false \
+	-Dcolor-management-lcms=$(call ptx/truefalse,PTXCONF_WESTON_COLOR_MANAGEMENT_LCMS) \
 	-Ddemo-clients=$(call ptx/truefalse,PTXCONF_WESTON_IVISHELL_EXAMPLE) \
 	-Ddeprecated-color-management-colord=false \
 	-Ddeprecated-color-management-static=false \

rules/weston.in
@@ -40,6 +40,7 @@ menuconfig WESTON
 	select PIPEWIRE 		if WESTON_BACKEND_PIPEWIRE
 	select NEATVNC			if WESTON_BACKEND_VNC
 	select PAM			if WESTON_BACKEND_VNC
+	select LCMS			if WESTON_COLOR_MANAGEMENT_LCMS
 	prompt "weston                        "
 	help
 	  Wayland compositor reference implementation
@@ -128,5 +129,9 @@ config WESTON_TOUCH_CALIBRATOR
 	help
 	  Install weston-touch-calibrator tool, interface must be enabled in
 	  weston.ini.
+
+config WESTON_COLOR_MANAGEMENT_LCMS
+	bool
+	prompt "color management module"
 
 endif

commit 1824bcf480401b20612048ae072fdfa790643a1f

weston: install color-lcms lib if needed
if PTXCONF_WESTON_COLOR_MANAGEMENT_LCMS has been set, it is needed
to install color-lcms lib.

Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>
Fixes: 6d447e0278df ("weston: build color management module")
Message-Id: <20250327084023.23392-1-benjamin.gaignard@collabora.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/weston.make
@@ -147,6 +147,9 @@ endif
 ifdef PTXCONF_WESTON_REMOTING
 	@$(call install_lib, weston, 0, 0, 0644, libweston-$(LIBWESTON_MAJOR)/remoting-plugin)
 endif
+ifdef PTXCONF_WESTON_COLOR_MANAGEMENT_LCMS
+	@$(call install_lib, weston, 0, 0, 0644, libweston-$(LIBWESTON_MAJOR)/color-lcms)
+endif
 	@$(call install_lib, weston, 0, 0, 0644, weston/desktop-shell)
 	@$(call install_lib, weston, 0, 0, 0644, weston/fullscreen-shell)
 ifdef PTXCONF_WESTON_IVISHELL