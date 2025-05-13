LeSpocky/ptxdist

commit 47d24525a49dc3f1687bbf00ef602cf3dc5bb31f

ptxd_kconfig_find_config: refactor to define the list of possible configs first
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_kconfig.sh
@@ -250,9 +250,8 @@ ptxd_kconfig_validate_config_check() {
 export -f ptxd_kconfig_validate_config_check
 
 ptxd_kconfig_validate_config_next() {
-	local layer
-	for layer in "${@}"; do
-	    next="${layer}/${relative_config}"
+	local next
+	for next in "${@}"; do
 	    if ! ptxd_kconfig_validate_config_check "${next}"; then
 		continue
 	    fi
@@ -263,6 +262,11 @@ ptxd_kconfig_validate_config_next() {
 }
 export -f ptxd_kconfig_validate_config_next
 
+ptxd_kconfig_setup_layer_configs() {
+    layer_configs=( "${PTXDIST_LAYERS[@]/%//${relative_config}}" )
+}
+export -f ptxd_kconfig_setup_layer_configs
+
 ptxd_kconfig_validate_config() {
     local relative_config="${1}"
     local relative_ref_config="${2}"
@@ -271,16 +275,15 @@ ptxd_kconfig_validate_config() {
     local last next
     local -a layers
 
+    ptxd_kconfig_setup_layer_configs
+
     if [ "${mode}" = update ]; then
-	layers=( "${PTXDIST_LAYERS[@]:1}" )
-    else
-	layers=( "${PTXDIST_LAYERS[@]}" )
+	layer_configs=( "${layer_configs[@]:1}" )
     fi
 
-    set -- "${layers[@]}";
+    set -- "${layer_configss[@]}";
     while [ $# -gt 0 ]; do
-	layer="${1}"
-	last="${layer}/${relative_config}"
+	last="${1}"
 	shift
 	if ! ptxd_kconfig_validate_config_check "${last}"; then
 	    continue
@@ -293,7 +296,7 @@ ptxd_kconfig_validate_config() {
 	    relative_config="${relative_ref_config}"
 	    unset relative_ref_config
 	    ignore_last_diff=y
-	    set -- "${layer}" "${@}"
+	    set -- "${last}" "${@}"
 	    ptxd_kconfig_validate_config_next "${@}"
 	fi
 	if [ ! -e "${next}" ]; then
@@ -330,14 +333,16 @@ ptxd_kconfig_find_config() {
     local mode="${1}"
     local relative_config="${2}"
     local relative_ref_config="${3}"
-    local -a layers
-    local tmp_config
+    local -a layer_configs
+    local tmp_config tmp
 
     if [ "${mode}" = run -o "${mode}" = "update" ]; then
 	ptxd_kconfig_validate_config "${relative_config}" "${relative_ref_config}" || return
     fi
 
-    last_config="${PTXDIST_LAYERS[0]}/${relative_config}"
+    ptxd_kconfig_setup_layer_configs
+
+    last_config="${layer_configs[0]}"
     if [ "$(readlink -f "${last_config}")" = /dev/null ]; then
 	# use the first existing config for 'run'
 	if [ "${mode}" = run ]; then
@@ -349,15 +354,12 @@ ptxd_kconfig_find_config() {
     fi
 
     if [ "${mode}" = update ]; then
-	layers=( "${PTXDIST_LAYERS[@]:1}" )
+	layer_configs=( "${layer_configs[@]:1}" )
     else
 	base_config="${last_config}"
-	layers=( "${PTXDIST_LAYERS[@]}" )
     fi
 
-    for layer in "${layers[@]}"; do
-	local tmp
-	tmp="${layer}/${relative_config}"
+    for tmp in "${layer_configs[@]}"; do
 	if [ "$(readlink -f "${tmp}")" = /dev/null ]; then
 	    continue
 	fi

commit 30afb3fa3753c656107476ed75784302b1c580f0

ptxd_kconfig_validate_config: fix validation
Due to a typo, the config was not actually validated. So changes in the
base layer may not be detected.

There are more things broken that where hidden by the typo and are fixed
here:

Remove an incorect local variable to ensure that 'next' is actually set.

"${@}" are now the configs not the layers, so for the _REF_CONFIG
fallback, the serach path must be modified accordingly.

Fixes: 47d24525a49d ("ptxd_kconfig_find_config: refactor to define the list of possible configs first")
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_kconfig.sh
@@ -251,7 +251,6 @@ ptxd_kconfig_validate_config_check() {
 export -f ptxd_kconfig_validate_config_check
 
 ptxd_kconfig_validate_config_next() {
-	local next
 	for next in "${@}"; do
 	    if ! ptxd_kconfig_validate_config_check "${next}"; then
 		continue
@@ -286,7 +285,7 @@ ptxd_kconfig_validate_config() {
 	layer_configs=( "${layer_configs[@]:1}" )
     fi
 
-    set -- "${layer_configss[@]}";
+    set -- "${layer_configs[@]}";
     while [ $# -gt 0 ]; do
 	last="${1}"
 	shift
@@ -298,10 +297,10 @@ ptxd_kconfig_validate_config() {
 	fi
 	ptxd_kconfig_validate_config_next "${@}"
 	if [ ! -e "${next}" -a -n "${relative_ref_config}" ]; then
-	    relative_config="${relative_ref_config}"
-	    unset relative_ref_config
 	    ignore_last_diff=y
 	    set -- "${last}" "${@}"
+	    set -- "${@/${relative_config}/${relative_ref_config}}"
+	    unset relative_ref_config
 	    ptxd_kconfig_validate_config_next "${@}"
 	fi
 	if [ ! -e "${next}" ]; then