LeSpocky/ptxdist

commit 30afb3fa3753c656107476ed75784302b1c580f0

ptxd_kconfig_validate_config: fix validation
Due to a typo, the config was not actually validated. So changes in the
base layer may not be detected.

There are more things broken that where hidden by the typo and are fixed
here:

Remove an incorect local variable to ensure that 'next' is actually set.

"${@}" are now the configs not the layers, so for the _REF_CONFIG
fallback, the serach path must be modified accordingly.

Fixes: 47d24525a49d ("ptxd_kconfig_find_config: refactor to define the list of possible configs first")
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_kconfig.sh
@@ -251,7 +251,6 @@ ptxd_kconfig_validate_config_check() {
 export -f ptxd_kconfig_validate_config_check
 
 ptxd_kconfig_validate_config_next() {
-	local next
 	for next in "${@}"; do
 	    if ! ptxd_kconfig_validate_config_check "${next}"; then
 		continue
@@ -286,7 +285,7 @@ ptxd_kconfig_validate_config() {
 	layer_configs=( "${layer_configs[@]:1}" )
     fi
 
-    set -- "${layer_configss[@]}";
+    set -- "${layer_configs[@]}";
     while [ $# -gt 0 ]; do
 	last="${1}"
 	shift
@@ -298,10 +297,10 @@ ptxd_kconfig_validate_config() {
 	fi
 	ptxd_kconfig_validate_config_next "${@}"
 	if [ ! -e "${next}" -a -n "${relative_ref_config}" ]; then
-	    relative_config="${relative_ref_config}"
-	    unset relative_ref_config
 	    ignore_last_diff=y
 	    set -- "${last}" "${@}"
+	    set -- "${@/${relative_config}/${relative_ref_config}}"
+	    unset relative_ref_config
 	    ptxd_kconfig_validate_config_next "${@}"
 	fi
 	if [ ! -e "${next}" ]; then

commit d628bd553c266dd4fc5428d8bc483a8010afb2d0

ptxd_kconfig_validate_config: don't break if config exists on all layers
If the config exists on the lowest layer, then
ptxd_kconfig_validate_config_next() is called with out arguments, so
next is not updated at all. and ptxd_kconfig_validate_config() expects
another layer and fails because the diff is missing.
Avoid this by clearing next in this case.

This happens with 'ptxdist localsetup' where the config in the last
layer is ptxdistrc.default.

Fixes: 30afb3fa3753 ("ptxd_kconfig_validate_config: fix validation")
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_kconfig.sh
@@ -252,6 +252,7 @@ ptxd_kconfig_validate_config_check() {
 export -f ptxd_kconfig_validate_config_check
 
 ptxd_kconfig_validate_config_next() {
+	next=""
 	for next in "${@}"; do
 	    if ! ptxd_kconfig_validate_config_check "${next}"; then
 		continue