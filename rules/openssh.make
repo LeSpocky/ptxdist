LeSpocky/ptxdist

commit 76baa24d31305948dc60f0932921074510761590

openssh: Version bump. 9.9p2 -> 10.0p1.
Mostly tightening of security.
DSA is gone, DH-* KEXes are gone too.
https://www.openssh.com/releasenotes.html#10.0p1

Signed-off-by: Christian Melki <christian.melki@t2data.com>
Message-Id: <20250410163802.2803935-1-christian.melki@t2data.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/openssh.make
@@ -15,8 +15,8 @@ PACKAGES-$(PTXCONF_OPENSSH) += openssh
 #
 # Paths and names
 #
-OPENSSH_VERSION	:= 9.9p2
-OPENSSH_MD5	:= f617b95fe278bfea8d004589c7a68a85
+OPENSSH_VERSION	:= 10.0p1
+OPENSSH_MD5	:= 689148621a2eaa734497b12bed1c5202
 OPENSSH		:= openssh-$(OPENSSH_VERSION)
 OPENSSH_SUFFIX	:= tar.gz
 OPENSSH_URL	:= \

commit c240fdd0de4f9b15263a6b8dae2e17a510dafd66

openssh: install missing sshd-auth binary
It was split out from sshd-session and is needed with sshd.

Fixes: 76baa24d3130 ("openssh: Version bump. 9.9p2 -> 10.0p1.")
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/openssh.make
@@ -107,6 +107,8 @@ ifdef PTXCONF_OPENSSH_SSHD
 	@$(call install_copy, openssh, 0, 0, 0755, -, \
 		/usr/sbin/sshd)
 	@$(call install_copy, openssh, 0, 0, 0755, -, \
+		/usr/sbin/sshd-auth)
+	@$(call install_copy, openssh, 0, 0, 0755, -, \
 		/usr/sbin/sshd-session)
 ifdef PTXCONF_OPENSSH_SSHD_GENKEYS
 	@$(call install_alternative, openssh, 0, 0, 0755, /etc/rc.once.d/openssh)