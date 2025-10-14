LeSpocky/ptxdist

commit 4850f7aac30aebf4ef64b1d3f1d324333b5978c2

update all spdx license identifier to the new version
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/evtest.make
@@ -23,7 +23,7 @@ EVTEST_SUFFIX	:= tar.bz2
 EVTEST_URL	:= http://www.pengutronix.de/software/ptxdist/temporary-src/$(EVTEST).$(EVTEST_SUFFIX)
 EVTEST_SOURCE	:= $(SRCDIR)/$(EVTEST).$(EVTEST_SUFFIX)
 EVTEST_DIR	:= $(BUILDDIR)/$(EVTEST)
-EVTEST_LICENSE	:= GPL-2.0
+EVTEST_LICENSE	:= GPL-2.0-only
 
 # ----------------------------------------------------------------------------
 # Prepare

commit 8a8dc3d93312a64bb2f1eb8789f0fbae4d46b2aa

evtest: version bump 1.35 -> 1.36
The evtest.c file always specified GPLv2 or later, so fix it and
specify license md5 sums.

https://gitlab.freedesktop.org/libevdev/evtest/-/releases/evtest-1.36

Fixes: 4850f7aac30a ("update all spdx license identifier to the new version")
Signed-off-by: Sven Püschel <s.pueschel@pengutronix.de>
Message-Id: <20251013153048.2571379-1-s.pueschel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/evtest.make
@@ -14,14 +14,17 @@ PACKAGES-$(PTXCONF_EVTEST) += evtest
 #
 # Paths and names
 #
-EVTEST_VERSION	:= 1.35
-EVTEST_MD5	:= cc63594043419bb8364d1b54bf3c32e7
+EVTEST_VERSION	:= 1.36
+EVTEST_MD5	:= ebd8a804185bfcf19ed20b4789f7e9fc
 EVTEST		:= evtest-$(EVTEST_VERSION)
 EVTEST_SUFFIX	:= tar.bz2
 EVTEST_URL	:= https://gitlab.freedesktop.org/libevdev/evtest/-/archive/evtest-$(EVTEST_VERSION)/evtest-$(EVTEST).$(EVTEST_SUFFIX)
 EVTEST_SOURCE	:= $(SRCDIR)/$(EVTEST).$(EVTEST_SUFFIX)
 EVTEST_DIR	:= $(BUILDDIR)/$(EVTEST)
-EVTEST_LICENSE	:= GPL-2.0-only
+EVTEST_LICENSE	:= GPL-2.0-or-later
+EVTEST_LICENSE_FILES := \
+	file://COPYING;md5=751419260aa954499f7abaabaa882bbe \
+	file://evtest.c;startline=18;endline=36;md5=f38f0b28798dbf9b0cc59de32ef71a28
 
 # ----------------------------------------------------------------------------
 # Prepare