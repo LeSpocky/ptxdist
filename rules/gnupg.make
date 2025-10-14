LeSpocky/ptxdist

commit fa1e4439f3a6627c3f83bafb894994f6bb5c0c1f

gnupg: version bump 2.4.5 -> 2.5.3
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/gnupg.make
@@ -15,8 +15,8 @@ PACKAGES-$(PTXCONF_GNUPG) += gnupg
 #
 # Paths and names
 #
-GNUPG_VERSION	:= 2.4.5
-GNUPG_MD5	:= 49c3534e87744e994250d37c1b43f928
+GNUPG_VERSION	:= 2.5.3
+GNUPG_MD5	:= 0f6c849e3c7ad7cefd35b54647044348
 GNUPG		:= gnupg-$(GNUPG_VERSION)
 GNUPG_SUFFIX	:= tar.bz2
 GNUPG_URL	:= https://www.gnupg.org/ftp/gcrypt/gnupg/$(GNUPG).$(GNUPG_SUFFIX)
@@ -36,7 +36,8 @@ GNUPG_LICENSE_FILES := \
 
 GNUPG_CONF_ENV	:= \
 	$(CROSS_ENV) \
-	ac_cv_path_GPGRT_CONFIG=$(PTXDIST_SYSROOT_CROSS)/usr/bin/gpgrt-config
+	ac_cv_path_GPGRT_CONFIG=$(PTXDIST_SYSROOT_CROSS)/usr/bin/gpgrt-config \
+	ac_cv_path_GPG_ERROR_CONFIG=$(PTXDIST_SYSROOT_CROSS)/usr/bin/gpg-error-config
 
 GNUPG_CONF_TOOL	:= autoconf
 GNUPG_CONF_OPT	:= $(CROSS_AUTOCONF_USR) \
@@ -49,7 +50,6 @@ GNUPG_CONF_OPT	:= $(CROSS_AUTOCONF_USR) \
 	--disable-doc \
 	--$(call ptx/endis, PTXCONF_GNUPG_GPGTAR)-gpgtar \
 	--disable-wks-tools \
-	--disable-gpg-is-gpg2 \
 	--$(call ptx/endis, PTXCONF_GLOBAL_SELINUX)-selinux-support \
 	--disable-large-secmem \
 	--enable-trust-models \
@@ -94,6 +94,7 @@ GNUPG_CONF_OPT	:= $(CROSS_AUTOCONF_USR) \
 	--disable-log-clock \
 	--disable-werror \
 	--disable-all-tests \
+	--disable-tests \
 	--disable-run-gnupg-user-socket \
 	--enable-build-timestamp="$(PTXDIST_BUILD_TIMESTAMP)"
 

rules/gnupg.in
@@ -6,6 +6,7 @@ menuconfig GNUPG
 	select GCCLIBS_GCC_S
 	select LIBASSUAN
 	select LIBGCRYPT
+	select LIBGPG_ERROR
 	select LIBKSBA
 	select NPTH
 	select READLINE

commit dc556e8dd9ad698f8923972b5b827cc852a89d2f

gnupg: use the stable 2.4 version line
The 2.4.x releases of GnuPG represent stable releases, whereas the 2.5.x
releases are marked as devel. While they are also fully supported and
ready for production use, stay at the latest stable version for PTXDist.

Fixes: fa1e4439f3a6 ("gnupg: version bump 2.4.5 -> 2.5.3")
Signed-off-by: Sven Püschel <s.pueschel@pengutronix.de>
Message-Id: <20251013092343.1237782-1-s.pueschel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/gnupg.make
@@ -15,8 +15,8 @@ PACKAGES-$(PTXCONF_GNUPG) += gnupg
 #
 # Paths and names
 #
-GNUPG_VERSION	:= 2.5.3
-GNUPG_MD5	:= 0f6c849e3c7ad7cefd35b54647044348
+GNUPG_VERSION	:= 2.4.8
+GNUPG_MD5	:= a165b60aeaac0bb4d251117a45199c5f
 GNUPG		:= gnupg-$(GNUPG_VERSION)
 GNUPG_SUFFIX	:= tar.bz2
 GNUPG_URL	:= https://www.gnupg.org/ftp/gcrypt/gnupg/$(GNUPG).$(GNUPG_SUFFIX)
@@ -50,6 +50,7 @@ GNUPG_CONF_OPT	:= $(CROSS_AUTOCONF_USR) \
 	--disable-doc \
 	--$(call ptx/endis, PTXCONF_GNUPG_GPGTAR)-gpgtar \
 	--disable-wks-tools \
+	--disable-gpg-is-gpg2 \
 	--$(call ptx/endis, PTXCONF_GLOBAL_SELINUX)-selinux-support \
 	--disable-large-secmem \
 	--enable-trust-models \