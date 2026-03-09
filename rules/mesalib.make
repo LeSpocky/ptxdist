LeSpocky/ptxdist

commit 8a0425b3c2a58e60dfd190285d8a1b956f200828

mesalib: version bump 25.3.5 -> 26.0.1
https://docs.mesa3d.org/relnotes/26.0.0.html
https://docs.mesa3d.org/relnotes/26.0.1.html

Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
Message-Id: <20260226093123.1690070-1-p.zabel@pengutronix.de>
[mol: drop option for kosmickrisp. I makes no sense on Linux]
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/mesalib.make
@@ -14,8 +14,8 @@ PACKAGES-$(PTXCONF_MESALIB) += mesalib
 #
 # Paths and names
 #
-MESALIB_VERSION	:= 25.3.5
-MESALIB_MD5	:= 8e699baecbb0a15b5651cc0db2035a4c
+MESALIB_VERSION	:= 26.0.1
+MESALIB_MD5	:= 20a629e63ffe1474b33993684bf9cb75
 MESALIB		:= mesa-$(MESALIB_VERSION)
 MESALIB_SUFFIX	:= tar.xz
 MESALIB_URL	:= \
@@ -136,6 +136,8 @@ MESALIB_VIDEO_CODECS-$(PTXCONF_MESALIB_VIDEO_H265ENC)	+= h265enc
 MESALIB_VIDEO_CODECS-$(PTXCONF_MESALIB_VIDEO_AV1DEC)	+= av1dec
 MESALIB_VIDEO_CODECS-$(PTXCONF_MESALIB_VIDEO_AV1ENC)	+= av1enc
 MESALIB_VIDEO_CODECS-$(PTXCONF_MESALIB_VIDEO_VP9DEC)	+= vp9dec
+MESALIB_VIDEO_CODECS-$(PTXCONF_MESALIB_VIDEO_MPEG12DEC)	+= mpeg12dec
+MESALIB_VIDEO_CODECS-$(PTXCONF_MESALIB_VIDEO_JPEGDEC)	+= jpegdec
 
 ifdef PTXCONF_ARCH_X86
 MESALIB_VULKAN_DRIVERS-$(PTXCONF_MESALIB_VULKAN_AMD)		+= amd
@@ -168,7 +170,7 @@ MESALIB_VULKAN_LIBS-y = $(subst amd,radeon \
 
 MESALIB_VULKAN_ICDS-y = $(subst gfxstream,gfxstream_vk \
 	,$(MESALIB_VULKAN_LIBS-y) \
-	)
+	))
 
 MESALIB_VULKAN_LAYERS-$(PTXCONF_MESALIB_VULKAN_ANTI_LAG)	+= anti-lag
 MESALIB_VULKAN_LAYERS-$(PTXCONF_MESALIB_VULKAN_DEVICE_SELECT)	+= device-select
@@ -301,6 +303,7 @@ MESALIB_CONF_OPT	:= \
 	-Dvulkan-drivers=$(subst $(space),$(comma),$(MESALIB_VULKAN_DRIVERS-y)) \
 	-Dvulkan-icd-dir=/etc/vulkan/icd.d \
 	-Dvulkan-layers=$(subst $(space),$(comma),$(MESALIB_VULKAN_LAYERS-y)) \
+	-Dvulkan-manifest-per-architecture=true \
 	-Dxlib-lease=$(call ptx/endis, PTXCONF_MESALIB_EGL_X11)d \
 	-Dxmlconfig=$(call ptx/endis, PTXCONF_MESALIB_XMLCONFIG)d \
 	-Dzlib=enabled \

rules/host-mesalib.make
@@ -122,6 +122,7 @@ HOST_MESALIB_CONF_OPT	:= \
 	-Dvulkan-drivers=[] \
 	-Dvulkan-icd-dir=/etc/vulkan/icd.d \
 	-Dvulkan-layers=[] \
+	-Dvulkan-manifest-per-architecture=true \
 	-Dxlib-lease=disabled \
 	-Dxmlconfig=disabled \
 	-Dzlib=enabled \

rules/mesalib.in
@@ -232,6 +232,14 @@ config MESALIB_VIDEO_VP9DEC
 	bool
 	prompt "VP9 decoding"
 
+config MESALIB_VIDEO_MPEG12DEC
+	bool
+	prompt "MPEG1/2 decoding"
+
+config MESALIB_VIDEO_JPEGDEC
+	bool
+	prompt "JPEG decoding"
+
 endmenu
 
 endif

commit 2cb12d7743c83ae0b9ac4459da07e2b717b8749d

mesalib: remove erroneous parenthesis
Remove a superfluous closing parenthesis that shouldn't have been added
in the last update.

Fixes: 8a0425b3c2a5 ("mesalib: version bump 25.3.5 -> 26.0.1")
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
Message-Id: <20260309095406.140826-1-p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/mesalib.make
@@ -170,7 +170,7 @@ MESALIB_VULKAN_LIBS-y = $(subst amd,radeon \
 
 MESALIB_VULKAN_ICDS-y = $(subst gfxstream,gfxstream_vk \
 	,$(MESALIB_VULKAN_LIBS-y) \
-	))
+	)
 
 MESALIB_VULKAN_LAYERS-$(PTXCONF_MESALIB_VULKAN_ANTI_LAG)	+= anti-lag
 MESALIB_VULKAN_LAYERS-$(PTXCONF_MESALIB_VULKAN_DEVICE_SELECT)	+= device-select