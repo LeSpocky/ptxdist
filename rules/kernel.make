LeSpocky/ptxdist

commit 844408110d046a967ec1d588dd038d95a6065514

kernel: fix modules.builtin.modinfo being executable
Since kernel commit a26a6c93edfe ("kbuild: Strip trailing padding bytes from
modules.builtin.modinfo") [1], the resulting modules.builtin.modinfo file gets
copied by objcopy resulting in an executable file.

It seems, that objcopy detects this file as executable even though
the file is not executable at all.

Fix this problem by removing the execution bit afterwards.

I also send a patch upstream to the kernel [2].

[1] https://lore.kernel.org/all/20251105-kbuild-fix-builtin-modinfo-for-kmod-v1-1-b419d8ad4606@kernel.org
[2] https://lore.kernel.org/all/20251209-modinfo-executable-v1-1-ed0c553a4390@pengutronix.de/#t

Signed-off-by: Fabian Pfitzner <f.pfitzner@pengutronix.de>
Message-Id: <20251210-modinfo-executable-v1-1-23c6f3fc999e@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/kernel.make
@@ -274,6 +274,7 @@ $(STATEDIR)/kernel.install:
 	@$(call targetinfo)
 ifdef PTXCONF_KERNEL_MODULES_INSTALL
 	@$(call world/install, KERNEL)
+	@chmod -x $(KERNEL_PKGDIR)/lib/modules/*/modules.builtin.modinfo
 endif
 	@$(call world/dtb, KERNEL)
 	@$(call world/dtbo, KERNEL)

commit c140700eca0af50a8fbe802c5b41668145d9270d

kernel: Call chmod modules.builtin.modinfo for v6.x only
kernel.install stage fails for kernel versions before v5.2 (which
introduced modules.builtin.modinfo in the first place) like this:

      DEPMOD  4.9.327-rt197-tt
    depmod: WARNING: could not open modules.builtin.modinfo at [redacted]/platform-ncl/packages/linux-4.9.327/lib/modules/4.9.327-rt197-tt: No such file or directory
    chmod: cannot access '[redacted]/platform-ncl/packages/linux-4.9.327/lib/modules/*/modules.builtin.modinfo': No such file or directory
    make: *** [/usr/local/lib/ptxdist-2026.01.0/rules/kernel.make:287: [redacted]/platform-ncl/state/kernel.install] Error 1
    ptxdist install kernel  3,75s user 1,63s system 101% cpu 5,308 total

The chmod fix is only necessary for kernel v6.18 which introduced the
faulty executable permissions.  It was fixed for v6.19 with kernel commit
6d60354ea2f9 ("kbuild: Fix permissions of modules.builtin.modinfo").

Suggested-by: Michael Olbrich <m.olbrich@pengutronix.de>
Tested-by: Ruggero Rossi <rugrossi@googlemail.com>
Fixes: 844408110d04 ("kernel: fix modules.builtin.modinfo being executable")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Message-Id: <20260327095642.526947-1-ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/kernel.make
@@ -285,7 +285,9 @@ $(STATEDIR)/kernel.install:
 	@$(call targetinfo)
 ifdef PTXCONF_KERNEL_MODULES_INSTALL
 	@$(call world/install, KERNEL)
+ifeq ($(KERNEL_VERSION_MAJOR),6)
 	@chmod -x $(KERNEL_PKGDIR)/lib/modules/*/modules.builtin.modinfo
+endif
 endif
 	@$(call world/dtb, KERNEL)
 	@$(call world/dtbo, KERNEL)