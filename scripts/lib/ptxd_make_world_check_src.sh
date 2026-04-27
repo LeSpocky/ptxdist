da495a88188efd1990b9c106165606a907c080fa
Philipp Zabel <p.zabel@pengutronix.de>
2026-04-27 16:28:46 +0200

    scripts: make checksum update hash algorithm agnostic
    Rename ptxd_make_world_update_md5() to ptxd_make_world_update_checksum()
    and pass in the checksum tool (md5sum) and the hash suffix (MD5) as
    parameters.
    Rename local variables md5 to checksum and PKG_MD5 to PKG_SUM.
    
    Add back ptxd_make_world_update_md5() as a wrapper that calls
    ptx_make_world_update_checksum md5sum MD5.
    
    No functional change.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Message-Id: <20260427142848.989702-3-p.zabel@pengutronix.de>
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/lib/ptxd_make_world_check_src.sh
+++ scripts/lib/ptxd_make_world_check_src.sh
@@ -8,26 +8,28 @@
 
 
 #
-# try to update the md5sum of the current package
-# this only works if the makefile contains a "<PKG>_MD5 := ..." line.
+# $1: checksum tool, e.g. md5sum
+# $2: hash type suffix appended to <PKG>_, e.g. MD5
 #
-ptxd_make_world_update_md5() {
+ptxd_make_world_update_checksum() {
+    local tool="${1}"
+    local SUFFIX="${2}"
     local config file_dotconfig
-    set -- $(md5sum "${pkg_src}")
-    local md5="${1}"
+    set -- $("${tool}" "${pkg_src}")
+    local checksum="${1}"
 
-    local PKG_MD5="PTXCONF_${pkg_PKG}_MD5"
+    local PKG_SUM="PTXCONF_${pkg_PKG}_${SUFFIX}"
     for config in "${PTXDIST_PLATFORMCONFIG}" "${PTXDIST_PTXCONFIG}"; do
 	file_dotconfig="${config}"
 	ptxd_normalize_config
-	if grep -q "^${PKG_MD5}=\"" "${file_dotconfig}"; then
-	    sed -i "s/^${PKG_MD5}=\".*$/${PKG_MD5}=\"${md5}\"/" "${file_dotconfig}"
-	    ptxd_warning "New checksum for ${pkg_PKG}: ${md5} in $(ptxd_print_path "${file_dotconfig}")"
+	if grep -q "^${PKG_SUM}=\"" "${file_dotconfig}"; then
+	    sed -i "s/^${PKG_SUM}=\".*$/${PKG_SUM}=\"${checksum}\"/" "${file_dotconfig}"
+	    ptxd_warning "New checksum for ${pkg_PKG}: ${checksum} in $(ptxd_print_path "${file_dotconfig}")"
 	    if [ -e "${file_dotconfig}.diff" ]; then
-		if grep -q "^${PKG_MD5}=\"" "${file_dotconfig}.diff"; then
-		    sed -i "s/^${PKG_MD5}=\".*$/${PKG_MD5}=\"${md5}\"/" "${file_dotconfig}.diff"
+		if grep -q "^${PKG_SUM}=\"" "${file_dotconfig}.diff"; then
+		    sed -i "s/^${PKG_SUM}=\".*$/${PKG_SUM}=\"${checksum}\"/" "${file_dotconfig}.diff"
 		else
-		    echo "${PKG_MD5}=\"${md5}\"" >> "${file_dotconfig}.diff"
+		    echo "${PKG_SUM}=\"${checksum}\"" >> "${file_dotconfig}.diff"
 		    if [ "${config}" == "${PTXDIST_PLATFORMCONFIG}" ]; then
 			arg=" platform"
 		    fi
@@ -40,14 +42,21 @@ ptxd_make_world_update_md5() {
     if [ -z "${pkg_makefile}" ]; then
 	ptxd_bailout "Could not update checksum for '${pkg_label}': makefile not found"
     fi
-    local count=$(grep "^${pkg_PKG}_MD5[ 	]*:=" "${pkg_makefile}" 2> /dev/null | wc -l)
+    local count=$(grep "^${pkg_PKG}_${SUFFIX}[ 	]*:=" "${pkg_makefile}" 2> /dev/null | wc -l)
     if [ "${count}" -gt 1 ]; then
-	ptxd_bailout "Could not update checksum for '${pkg_label}': ${pkg_PKG}_MD5 found ${count} times in '$(ptxd_print_path ${pkg_makefile})'."
+	ptxd_bailout "Could not update checksum for '${pkg_label}': ${pkg_PKG}_${SUFFIX} found ${count} times in '$(ptxd_print_path ${pkg_makefile})'."
     fi
-    sed -i "s/^\(\<${pkg_PKG}_MD5[ 	]*:=\) *[a-f0-9]*\$/\1 ${md5}/" "${pkg_makefile}"
-    if ! grep -q "${md5}\$" "${pkg_makefile}"; then
-	ptxd_bailout "Could not update checksum for '${pkg_label}': ${pkg_PKG}_MD5 not found"
+    sed -i "s/^\(\<${pkg_PKG}_${SUFFIX}[ 	]*:=\) *[a-f0-9]*\$/\1 ${checksum}/" "${pkg_makefile}"
+    if ! grep -q "${checksum}\$" "${pkg_makefile}"; then
+	ptxd_bailout "Could not update checksum for '${pkg_label}': ${pkg_PKG}_${SUFFIX} not found"
     fi
-    ptxd_warning "New checksum for ${pkg_PKG}: ${md5} in $(ptxd_print_path "${pkg_makefile}")"
+    ptxd_warning "New checksum for ${pkg_PKG}: ${checksum} in $(ptxd_print_path "${pkg_makefile}")"
+}
+export -f ptxd_make_world_update_checksum
+
+# try to update the md5sum of the current package
+# this only works if the makefile contains a "<PKG>_MD5 := ..." line.
+ptxd_make_world_update_md5() {
+    ptxd_make_world_update_checksum md5sum MD5
 }
 export -f ptxd_make_world_update_md5
