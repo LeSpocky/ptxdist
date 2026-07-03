LeSpocky/ptxdist

commit b6ac44c56bef8756e6c7ff6abd446eca035a7588

json-c: version bump 0.14 -> 0.15
A couple of different changes, some new features
and some removed ones. Nothing big.

Changelog can be found here:
https://github.com/json-c/json-c/blob/master/ChangeLog

Signed-off-by: Linnea Gunnarsson <linnea.gunnarsson@t2data.com>
Message-Id: <20220330124036.2334533-1-linnea.gunnarsson@t2data.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/json-c.make
@@ -14,8 +14,8 @@ PACKAGES-$(PTXCONF_JSON_C) += json-c
 #
 # Paths and names
 #
-JSON_C_VERSION		:= 0.14
-JSON_C_MD5		:= 72cbb065b43376d825cd521d115ae1f6
+JSON_C_VERSION		:= 0.15
+JSON_C_MD5		:= 55f395745ee1cb3a4a39b41636087501
 JSON_C			:= json-c-$(JSON_C_VERSION)
 JSON_C_SUFFIX		:= tar.gz
 JSON_C_URL		:= https://s3.amazonaws.com/json-c_releases/releases/$(JSON_C).$(JSON_C_SUFFIX)

commit 69555c70215c21fe180c4a8ad371d77f8c85c8ed

json-c: Revise and pin down configuration options
removed with 0.15:
- BUILD_DOCUMENTATION

added with 0.15:
- DISABLE_THREAD_LOCAL_STORAGE
- OVERRIDE_GET_RANDOM_SEED

added with 0.16:
- DISABLE_JSON_POINTER

added with 0.17:
- BUILD_APPS
- DISABLE_JSON_PATCH
- NEWLOCALE_NEEDS_FREELOCALE

After changing json-c.make sync the changes to host-json-c.make …

Fixes: b6ac44c56bef ("json-c: version bump 0.14 -> 0.15")
Fixes: ecd364618410 ("json-c: version bump 0.15 -> 0.16")
Fixes: ee8afdbc3ea7 ("json-c: Version bump. 0.16 -> 0.17")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Message-Id: <20260629113457.1664202-2-ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/host-json-c.make
@@ -18,12 +18,19 @@ HOST_PACKAGES-$(PTXCONF_HOST_JSON_C) += host-json-c
 HOST_JSON_C_CONF_TOOL	:= cmake
 HOST_JSON_C_CONF_OPT	:= \
 	$(HOST_CMAKE_OPT) \
-	-DBUILD_DOCUMENTATION:BOOL=OFF \
+	-DBUILD_APPS:BOOL=OFF \
 	-DBUILD_SHARED_LIBS:BOOL=ON \
+	-DBUILD_STATIC_LIBS:BOOL=OFF \
 	-DBUILD_TESTING:BOOL=OFF \
 	-DDISABLE_BSYMBOLIC:BOOL=ON \
+	-DDISABLE_EXTRA_LIBS:BOOL=ON \
+	-DDISABLE_JSON_PATCH=OFF \
+	-DDISABLE_JSON_POINTER=OFF \
+	-DDISABLE_THREAD_LOCAL_STORAGE=OFF \
 	-DDISABLE_WERROR:BOOL=ON \
 	-DENABLE_RDRAND:BOOL=OFF \
-	-DENABLE_THREADING:BOOL=OFF
+	-DENABLE_THREADING:BOOL=OFF \
+	-DNEWLOCALE_NEEDS_FREELOCALE=OFF \
+	-DOVERRIDE_GET_RANDOM_SEED=OFF
 
 # vim: syntax=make

rules/json-c.make
@@ -31,15 +31,20 @@ JSON_C_LICENSE_FILES	:= file://COPYING;md5=de54b60fbbc35123ba193fea8ee216f2
 JSON_C_CONF_TOOL	:= cmake
 JSON_C_CONF_OPT		:= \
 	$(CROSS_CMAKE_USR) \
-	-DBUILD_DOCUMENTATION:BOOL=OFF \
+	-DBUILD_APPS:BOOL=OFF \
 	-DBUILD_SHARED_LIBS:BOOL=ON \
 	-DBUILD_STATIC_LIBS:BOOL=OFF \
 	-DBUILD_TESTING:BOOL=OFF \
 	-DDISABLE_BSYMBOLIC:BOOL=ON \
 	-DDISABLE_EXTRA_LIBS:BOOL=ON \
+	-DDISABLE_JSON_PATCH=OFF \
+	-DDISABLE_JSON_POINTER=OFF \
+	-DDISABLE_THREAD_LOCAL_STORAGE=OFF \
 	-DDISABLE_WERROR:BOOL=ON \
 	-DENABLE_RDRAND:BOOL=OFF \
-	-DENABLE_THREADING:BOOL=OFF
+	-DENABLE_THREADING:BOOL=OFF \
+	-DNEWLOCALE_NEEDS_FREELOCALE=OFF \
+	-DOVERRIDE_GET_RANDOM_SEED=OFF
 
 # ----------------------------------------------------------------------------
 # Install