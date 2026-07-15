LeSpocky/ptxdist

commit 4948d954f0c7be328fbdbd98e89a0326d56aa2cf

open62541: fix inclusion of mdnsd license in license report
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Message-Id: <20240923102506.1100544-1-rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/open62541.make
@@ -22,8 +22,6 @@ OPEN62541_SUFFIX		:= tar.gz
 OPEN62541_URL			:= https://github.com/open62541/open62541/archive/refs/tags/v$(OPEN62541_VERSION).$(OPEN62541_SUFFIX)
 OPEN62541_SOURCE		:= $(SRCDIR)/$(OPEN62541).$(OPEN62541_SUFFIX)
 OPEN62541_DIR			:= $(BUILDDIR)/$(OPEN62541)
-OPEN62541_LICENSE		:= MPL-2.0
-OPEN62541_LICENSE_FILES		:= file://LICENSE;md5=815ca599c9df247a0c7f619bab123dad
 
 # use tarballs instead of git submodules
 # see https://github.com/open62541/open62541/tree/v$(version)/deps
@@ -34,12 +32,17 @@ OPEN62541_MDNSD_SUFFIX		:= tar.gz
 OPEN62541_MDNSD_URL		:= https://github.com/Pro/mdnsd/archive/$(OPEN62541_MDNSD_VERSION).$(OPEN62541_MDNSD_SUFFIX)
 OPEN62541_MDNSD_SOURCE		:= $(SRCDIR)/$(OPEN62541_MDNSD).$(OPEN62541_MDNSD_SUFFIX)
 OPEN62541_MDNSD_DIR		:= $(OPEN62541_DIR)/deps/mdnsd
-OPEN62541_MDNSD_LICENSE		:= BSD-3-Clause
-OPEN62541_MDNSD_LICENSE_FILES	:= file://LICENSE;md5=3bb4047dc4095cd7336de3e2a9be94f0
+OPEN62541_MDNS_LICENSE		:= AND BSD-3-Clause
+OPEN62541_MDNS_LICENSE_FILES	:= file://deps/mdnsd/LICENSE;md5=3bb4047dc4095cd7336de3e2a9be94f0
 
 OPEN62541_DEVPKG		:= NO
 
 OPEN62541_PARTS			+= OPEN62541_MDNSD
+
+OPEN62541_LICENSE		:= $(HOST_OPEN62541_LICENSE) AND $(OPEN62541_MDNS_LICENSE)
+OPEN62541_LICENSE_FILES		:= \
+	$(HOST_OPEN62541_LICENSE_FILES) \
+	$(OPEN62541_MDNS_LICENSE_FILES)
 
 # ----------------------------------------------------------------------------
 # Prepare

rules/host-open62541.make
@@ -12,6 +12,10 @@
 #
 HOST_PACKAGES-$(PTXCONF_HOST_OPEN62541) += host-open62541
 
+# host-open62541 doesn't need the mdnsd dependency like open62541, so its license differs
+HOST_OPEN62541_LICENSE		= MPL-2.0
+HOST_OPEN62541_LICENSE_FILES	= file://LICENSE;md5=815ca599c9df247a0c7f619bab123dad
+
 # ----------------------------------------------------------------------------
 # Prepare
 # ----------------------------------------------------------------------------

commit e89ec87c6f37bdf9ab2410e55ee59d48b1b83e36

open62541: fix cross rule variable use
Avoid using host package rule variables in target package rule.
In this case it breaks target package information as host packages are ignored.

"license": " AND BSD-3-Clause"

Fixes: 4948d954f0c7b ('open62541: fix inclusion of mdnsd license in license report')

Signed-off-by: Bruno Thomsen <bruno.thomsen@gmail.com>
Message-Id: <20260714183720.18755-1-bruno.thomsen@gmail.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/open62541.make
@@ -22,6 +22,8 @@ OPEN62541_SUFFIX		:= tar.gz
 OPEN62541_URL			:= https://github.com/open62541/open62541/archive/refs/tags/v$(OPEN62541_VERSION).$(OPEN62541_SUFFIX)
 OPEN62541_SOURCE		:= $(SRCDIR)/$(OPEN62541).$(OPEN62541_SUFFIX)
 OPEN62541_DIR			:= $(BUILDDIR)/$(OPEN62541)
+OPEN62541_LICENSE		:= MPL-2.0
+OPEN62541_LICENSE_FILES		:= file://LICENSE;md5=815ca599c9df247a0c7f619bab123dad
 
 # use tarballs instead of git submodules
 # see https://github.com/open62541/open62541/tree/v$(version)/deps
@@ -39,9 +41,9 @@ OPEN62541_DEVPKG		:= NO
 
 OPEN62541_PARTS			+= OPEN62541_MDNSD
 
-OPEN62541_LICENSE		:= $(HOST_OPEN62541_LICENSE) AND $(OPEN62541_MDNS_LICENSE)
+OPEN62541_LICENSE		:= $(OPEN62541_LICENSE) AND $(OPEN62541_MDNS_LICENSE)
 OPEN62541_LICENSE_FILES		:= \
-	$(HOST_OPEN62541_LICENSE_FILES) \
+	$(OPEN62541_LICENSE_FILES) \
 	$(OPEN62541_MDNS_LICENSE_FILES)
 
 # ----------------------------------------------------------------------------