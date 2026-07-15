LeSpocky/ptxdist

commit 6e72460fe79714249b8100004886db82f2c2ef78

rauc: migrate to meson
Since different archive paths are used for the autotools and the meson
variant of rauc, we need to put them to distinct SRCDIR locations in
order to differentiate them when picking up. For this, add the -meson
suffix to RAUC_SOURCE.
This might be removed again when switching to the next release.

Also, RAUC_CONF_ENV modification does not seem to be needed since all
installed paths look valid without it.

Signed-off-by: Enrico Jorns <ejo@pengutronix.de>
Message-Id: <20230630210216.2724278-1-ejo@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/host-rauc.make
@@ -18,26 +18,20 @@ HOST_PACKAGES-$(PTXCONF_HOST_RAUC) += host-rauc
 #
 # autoconf
 #
-HOST_RAUC_CONF_TOOL	:= autoconf
+HOST_RAUC_CONF_TOOL	:= meson
 HOST_RAUC_CONF_OPT	:= \
 	$(HOST_AUTOCONF) \
-	--enable-debug=info \
-	--enable-largefile \
-	--enable-compile-warnings=yes \
-	--disable-Werror \
-	--disable-code-coverage \
-	--disable-valgrind \
-	--disable-service \
-	--enable-create \
-	--disable-network \
-	--disable-streaming \
-	--disable-json \
-	--disable-gpt \
-	--with-gcov=gcov \
-	--with-streaming-user=nobody \
-	--with-systemdunitdir=/usr/lib/systemd/system \
-	--with-dbuspolicydir=/usr/share/dbus-1/system.d \
-	--with-dbussystemservicedir=/usr/share/dbus-1/system-services \
-	--with-dbusinterfacesdir=/usr/share/dbus-1/interfaces
+	-Dcreate=true \
+	-Ddbusinterfacesdir=/usr/share/dbus-1/interfaces \
+	-Ddbuspolicydir=/usr/share/dbus-1/system.d \
+	-Ddbussystemservicedir=/usr/share/dbus-1/system-services \
+	-Dgpt=disabled \
+	-Djson=disabled \
+	-Dnetwork=false \
+	-Dservice=false \
+	-Dstreaming=false \
+	-Dstreaming_user=nobody \
+	-Dsystemdunitdir=/usr/lib/systemd/system \
+	-Dtests=false
 
 # vim: syntax=make

rules/host-rauc.in
@@ -4,5 +4,6 @@ config HOST_RAUC
 	tristate
 	default y if ALLYES
 	select HOST_GLIB
+	select HOST_MESON
 	select HOST_OPENSSL
 	select HOST_SQUASHFS_TOOLS

rules/rauc.in
@@ -3,6 +3,7 @@
 menuconfig RAUC
 	tristate
 	prompt "RAUC Update Tool              "
+	select HOST_MESON
 	select OPENSSL
 	select GLIB
 	select GLIB_LIBMOUNT

rules/rauc.make
@@ -1,6 +1,6 @@
 # -*-makefile-*-
 #
-# Copyright (C) 2015 by Enrico Joerns <e.joerns@pengutronix.de>
+# Copyright (C) 2015-2023 by Enrico Joerns <e.joerns@pengutronix.de>
 #
 # For further information about the PTXdist project and license conditions
 # see the README file.
@@ -15,11 +15,11 @@ PACKAGES-$(PTXCONF_RAUC) += rauc
 # Paths and names
 #
 RAUC_VERSION	:= 1.10
-RAUC_MD5	:= 28de8bb641402bb77df5d5233925a97d
+RAUC_MD5	:= ed1ebd5e0c1081528a3a6f26355cbbcc
 RAUC		:= rauc-$(RAUC_VERSION)
 RAUC_SUFFIX	:= tar.xz
-RAUC_URL	:= https://github.com/rauc/rauc/releases/download/v$(RAUC_VERSION)/$(RAUC)-autotools.$(RAUC_SUFFIX)
-RAUC_SOURCE	:= $(SRCDIR)/$(RAUC).$(RAUC_SUFFIX)
+RAUC_URL	:= https://github.com/rauc/rauc/releases/download/v$(RAUC_VERSION)/$(RAUC).$(RAUC_SUFFIX)
+RAUC_SOURCE	:= $(SRCDIR)/$(RAUC)-meson.$(RAUC_SUFFIX)
 RAUC_DIR	:= $(BUILDDIR)/$(RAUC)
 RAUC_LICENSE	:= LGPL-2.1-only
 
@@ -27,34 +27,24 @@ RAUC_LICENSE	:= LGPL-2.1-only
 # Prepare
 # ----------------------------------------------------------------------------
 
-RAUC_CONF_ENV	:= \
-	$(CROSS_ENV) \
-	PTXDIST_PKG_CONFIG_VAR_NO_SYSROOT=interfaces_dir
-
 #
-# autoconf
+# meson
 #
-RAUC_CONF_TOOL	:= autoconf
+RAUC_CONF_TOOL	:= meson
 RAUC_CONF_OPT	:= \
-	$(CROSS_AUTOCONF_USR) \
-	--enable-debug=info \
-	$(GLOBAL_LARGE_FILE_OPTION) \
-	--enable-compile-warnings=yes \
-	--disable-Werror \
-	--disable-code-coverage \
-	--disable-valgrind \
-	--$(call ptx/endis,PTXCONF_RAUC_SERVICE)-service \
-	--$(call ptx/endis,PTXCONF_RAUC_CREATE)-create \
-	--$(call ptx/endis,PTXCONF_RAUC_NETWORK)-network \
-	--$(call ptx/endis,PTXCONF_RAUC_STREAMING)-streaming \
-	--$(call ptx/endis,PTXCONF_RAUC_JSON)-json \
-	--$(call ptx/endis,PTXCONF_RAUC_GPT)-gpt \
-	--with-gcov=gcov \
-	--with-streaming_user=nobody \
-	--with-systemdunitdir=/usr/lib/systemd/system \
-	--with-dbuspolicydir=/usr/share/dbus-1/system.d \
-	--with-dbussystemservicedir=/usr/share/dbus-1/system-services \
-	--with-dbusinterfacesdir=/usr/share/dbus-1/interfaces
+	$(CROSS_MESON_USR) \
+	-Dcreate=$(call ptx/truefalse,PTXCONF_RAUC_CREATE) \
+	-Ddbusinterfacesdir=/usr/share/dbus-1/interfaces \
+	-Ddbuspolicydir=/usr/share/dbus-1/system.d \
+	-Ddbussystemservicedir=/usr/share/dbus-1/system-services \
+	-Dgpt=$(call ptx/endis,PTXCONF_RAUC_GPT)d \
+	-Djson=$(call ptx/endis,PTXCONF_RAUC_JSON)d \
+	-Dnetwork=$(call ptx/truefalse,PTXCONF_RAUC_NETWORK) \
+	-Dservice=$(call ptx/truefalse,PTXCONF_RAUC_SERVICE) \
+	-Dstreaming=$(call ptx/truefalse,PTXCONF_RAUC_STREAMING) \
+	-Dstreaming_user=nobody \
+	-Dsystemdunitdir=/usr/lib/systemd/system \
+	-Dtests=false
 
 $(STATEDIR)/rauc.prepare:
 	@$(call targetinfo)

commit 07cc935e7f26ce4c44ca9a50e3332163c6a9ab62

host-rauc: use the correct build system options
While migrating from autoconf to meson, the host options
variable was missed. Adjust it to not mix build system options.

Fixes: 6e72460fe797 ("rauc: migrate to meson")
Signed-off-by: Sven Püschel <s.pueschel@pengutronix.de>
Message-Id: <20260715073140.2795953-3-s.pueschel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/host-rauc.make
@@ -20,7 +20,7 @@ HOST_PACKAGES-$(PTXCONF_HOST_RAUC) += host-rauc
 #
 HOST_RAUC_CONF_TOOL	:= meson
 HOST_RAUC_CONF_OPT	:= \
-	$(HOST_AUTOCONF) \
+	$(HOST_MESON_OPT) \
 	-Dcreate=true \
 	-Ddbusinterfacesdir=/usr/share/dbus-1/interfaces \
 	-Ddbuspolicydir=/usr/share/dbus-1/system.d \